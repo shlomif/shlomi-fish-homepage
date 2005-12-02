6736c7284e2d3c5e5826011270ef203e9c52324a
shlomif <shlomif@cec68495-dca5-4e2b-845c-11fdaaa4f967>
2005-12-02 07:22:42 +0000

    Added the section of the lectures to the main navigation menu.

---
+++ lib/Rejects.pm
@@ -0,0 +1,26 @@
+my @nav_lectures_rejects =
+(
+                {
+                    'text' => "Freecell Solver",
+                    'url' => "lecture/Freecell-Solver/",
+                },
+                {
+                    'text' => "Lambda Calculus",
+                    'title' => "A presentation about a Turing-complete programming environment with only two primitives",
+                    'url' => "lecture/lc/",
+                },
+                {
+                    'text' => "The Gimp",
+                    'title' => "A Presentation about the GNU Image Manipulation Program",
+                    'url' => "lecture/Gimp/",
+                },
+                {
+                    'text' => "GNU Autotools",
+                    'url' => "lecture/Autotools/",
+                },
+                {
+                    'text' => "Web Meta Lecture",
+                    'title' => "A Presentation about the Web Meta Language",
+                    'url' => "lecture/WebMetaLecture/",
+                },
+);

9533b1a4388677f3e2b4175cc26cccfe781be4be
shlomif <shlomif@cec68495-dca5-4e2b-845c-11fdaaa4f967>
2006-08-26 08:09:44 +0000

    Am now touching the feed's modified files.

--- bin/fetch-shlomif_hsite-feed.pl
+++ bin/fetch-shlomif_hsite-feed.pl
@@ -10,3 +10,5 @@ use Shlomif::Homepage::Rss;
 my $feed = Shlomif::Homepage::Rss->new();
 $feed->run();
 
+# Touch the files so they'll be recompiled.
+utime(undef, undef, "t2/index.html.wml", "t2/old-news.html.wml");

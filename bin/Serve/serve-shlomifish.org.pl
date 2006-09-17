1c87478c726f7904ca6759b1dce9391c61aa2611
shlomif <shlomif@cec68495-dca5-4e2b-845c-11fdaaa4f967>
2006-09-17 15:58:51 +0000

    Added a CGI script to the serve the page. Useful with an Apache ScriptAlias or its equivalents in other web servers.

---
+++ bin/Serve/serve-shlomifish.org.pl
@@ -0,0 +1,13 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use File::Spec;
+
+use ShlomifServe;
+
+ShlomifServe::serve(
+     'dir_to_serve' => 
+         File::Spec->rel2abs("../../dest/t2-homepage/"),
+    );

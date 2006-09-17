0a1eb6f836a068ed896dc23a76d73068060d1aa8
shlomif <shlomif@cec68495-dca5-4e2b-845c-11fdaaa4f967>
2006-09-17 17:15:19 +0000

    Removed some redundant files.

--- find-wmls-vipe.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-perl -MFile::Find -e 'sub mypush { push @f, $File::Find::name if (/\.wml$/) } find(\&mypush, "vipe"); print join(" ", @f);'
--- find-wmls.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-perl -MFile::Find -e 'sub mypush { push @f, $File::Find::name if (/\.html\.wml$/) } find(\&mypush, $ARGV[0]); print join(" ", @f);' "$1"

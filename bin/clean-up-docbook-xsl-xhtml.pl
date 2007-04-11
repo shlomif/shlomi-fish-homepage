502abf80f0125db715c825d0db676ba9916c9a71
shlomif <shlomif@cec68495-dca5-4e2b-845c-11fdaaa4f967>
2007-04-11 12:04:35 +0000

    Added the Introductory Language article.

---
+++ bin/clean-up-docbook-xsl-xhtml.pl
@@ -0,0 +1,58 @@
+#!/usr/bin/perl 
+
+use strict;
+use warnings;
+
+use XML::LibXML;
+use XML::LibXML::XPathContext;
+
+# Input the filename
+my $filename = shift(@ARGV)
+    or die "Give me a filename as a command argument: myscript FILENAME";
+
+# Prepare the objects.
+my $xml = XML::LibXML->new;
+my $root_node = $xml->parse_file($filename);
+my $xpc = XML::LibXML::XPathContext->new($root_node);
+$xpc->registerNs("xhtml", "http://www.w3.org/1999/xhtml");
+
+
+# Find the empty a id="..." elements.
+{
+    my @nodes = $xpc->findnodes('//xhtml:a[@id and not(@href)]');
+
+    foreach my $anchor (@nodes)
+    {
+        my $container = $anchor->parentNode();
+        $container->setAttribute("id", $anchor->getAttribute("id"));
+        $container->removeChild($anchor);
+    }
+}
+
+# Remove the style attribute
+{
+    my @nodes = $xpc->findnodes('//*');
+    foreach my $node (@nodes)
+    {
+        $node->removeAttribute("style");
+    }
+}
+
+{
+    my ($body_node) = $xpc->findnodes('//xhtml:body');
+
+    my $s = $body_node->toString();
+
+    $s =~ s{\A<body[^>]*>}{}sm;
+    $s =~ s{</body>\z}{};
+
+    # It's a kludge
+    $s =~ s{ lang="en"}{};
+    $s =~ s{ xml:lang="en"}{};
+
+    # Fixed in Perl 6...
+    $s =~ s{<(/?)h(\d)}{"<".$1."h".($2+2)}ge;
+    binmode STDOUT, ":utf8";
+    print $s;
+}
+

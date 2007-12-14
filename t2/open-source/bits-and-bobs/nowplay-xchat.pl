5bb2a2a0c9dcc4e893b51ba25f68677879bcc44f
shlomif <shlomif@cec68495-dca5-4e2b-845c-11fdaaa4f967>
2007-12-14 14:24:15 +0000

    r4592@telaviv1:  shlomi | 2007-12-07 18:32:20 +0200  Added the current nowplay-xchat.pl script with support for Amarok.

--- t2/open-source/bits-and-bobs/nowplay-xchat.pl
+++ t2/open-source/bits-and-bobs/nowplay-xchat.pl
@@ -9,10 +9,32 @@ IRC::add_command_handler("nowplay", "display_now_playing_song");
 
 my $remote = Xmms::Remote->new();
 
+sub get_amarok_title
+{
+    my $t = `dcop amarok player title`;
+    my $a = `dcop amarok player artist`;
+    chomp($t);
+    chomp($a);
+    if (length($t) + length($a) > 0)
+    {
+        return "$a - $t";
+    }
+    else
+    {
+        return undef;
+    }
+}
+
+sub get_xmms_title
+{
+    # Check for amaroK
+    my $pos = $remote->get_playlist_pos();
+    return $remote->get_playlist_title($pos);
+}
+
 sub display_now_playing_song
 {
-    my $pos = $remote->get_playlist_pos();
-    my $title = $remote->get_playlist_title($pos);
+    my $title = get_amarok_title() || get_xmms_title();
     IRC::command("/me is listening to $title");
 }
 

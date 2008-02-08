b07a8899e0d107903e1926444cd478f22adf5eb0
shlomif <shlomif@cec68495-dca5-4e2b-845c-11fdaaa4f967>
2008-02-08 18:14:18 +0000

    r4692@telaviv1:  shlomi | 2008-02-05 12:02:12 +0200  More tweaks to get the new Fortunes build system working better.

--- gen-helpers.pl
+++ gen-helpers.pl
@@ -5,6 +5,8 @@ use warnings;
 
 use HTML::Latemp::GenMakeHelpers;
 
+use Cwd (qw(getcwd));
+
 my $generator = 
     HTML::Latemp::GenMakeHelpers->new(
         'hosts' =>
@@ -22,6 +24,17 @@ use IO::All;
 my $text = io("include.mak")->slurp();
 $text =~ s!^(T2_DOCS = .*)humour/fortunes/index.html!$1!m;
 io("include.mak")->print($text);
+
+# This is to in order to generate the t2/humour/fortunes/arcs-list.mak
+# file, which is inclduded by the makefile.
+{
+    my $orig_dir = getcwd();
+
+    chdir("t2/humour/fortunes");
+    system("make", "dist");
+
+    chdir($orig_dir);
+}
 
 1;
 

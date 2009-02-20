5955b2ad27f736f97eb1daf07cea23b1cf8dbf46
shlomif <shlomif@cec68495-dca5-4e2b-845c-11fdaaa4f967>
2009-02-20 23:43:38 +0000

    r5732@telaviv1:  shlomi | 2008-06-18 08:41:17 +0300  Added the ":utf8" binmode to output in UTF-8 encoding.

--- lib/prod-synd/music/gen-prod-synd.pl
+++ lib/prod-synd/music/gen-prod-synd.pl
@@ -28,6 +28,7 @@ my $xc = XML::LibXML::XPathContext->new($xml);
 $xc->registerNs('html' => "http://www.w3.org/1999/xhtml");
 
 open my $out, ">", "$lib_dir/include-me.html";
+binmode $out, ":utf8";
 print {$out} $xc->findnodes('/html:html/html:body/html:div')->[0]->toString(0);
 close ($out);
 

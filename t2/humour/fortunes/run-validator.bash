dd2aa279a0d779cfa4433524fd9d7c72c80f9225
shlomif <shlomif@cec68495-dca5-4e2b-845c-11fdaaa4f967>
2009-02-21 02:52:05 +0000

    r6354@telaviv1:  shlomi | 2008-12-26 12:02:31 +0200  Added a fortune.

--- t2/humour/fortunes/run-validator.bash
+++ t2/humour/fortunes/run-validator.bash
@@ -4,7 +4,7 @@ filename="$1"
 shift
 
 (
-    ~/apps/perl/perl-5.8.x-latest/bin/perl \
+    perl \
     -MXML::Grammar::Fortune \
     -e 'exit(XML::Grammar::Fortune->new(
         {mode => "validate"})->run({input => shift(@ARGV)}));' \

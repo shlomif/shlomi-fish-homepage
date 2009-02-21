f1a3854b877d3355dd78498518b69d3900110004
shlomif <shlomif@cec68495-dca5-4e2b-845c-11fdaaa4f967>
2009-02-21 01:43:18 +0000

    r6125@telaviv1:  shlomi | 2008-08-21 18:10:22 +0300  Converted the scripts to use standard "use" statements.

--- t2/humour/fortunes/convert-to-plaintext.pl
+++ t2/humour/fortunes/convert-to-plaintext.pl
@@ -1,23 +1,23 @@
 use strict;
 use warnings;
 
-use Cwd;
+use XML::Grammar::Fortune::ToText;
 use File::Spec;
-use String::ShellQuote;
 
 my $xml_fn = shift(@ARGV);
 my $out_fn = shift(@ARGV);
 
 my $html_fn = $xml_fn;
 
-
 my $abs_xml_fn = File::Spec->rel2abs($xml_fn);
 
 my $abs_out_fn = File::Spec->rel2abs($out_fn);
 
-my $xml_data_gen_cmd = 
-    q#perl -MXML::Grammar::Fortune::ToText -e 'my ($in_fn, $out_fn) = @ARGV; open my $out, ">", $out_fn; binmode($out, ":utf8"); XML::Grammar::Fortune::ToText->new({input => shift(@ARGV), output => $out})->run();close($out)' # .  shell_quote($abs_xml_fn, $abs_out_fn);
-
-# print STDERR $xml_data_gen_cmd; exit(0);
-system($xml_data_gen_cmd);
-
+{
+    open my $out, ">", $abs_out_fn; 
+    binmode($out, ":utf8"); 
+    XML::Grammar::Fortune::ToText
+        ->new({input => $abs_xml_fn, output => $out})
+        ->run();
+    close($out);
+}
--- t2/humour/fortunes/convert-to-xhtml.pl
+++ t2/humour/fortunes/convert-to-xhtml.pl
@@ -1,9 +1,8 @@
 use strict;
 use warnings;
 
-use Cwd;
 use File::Spec;
-use String::ShellQuote;
+use XML::Grammar::Fortune;
 
 my $xml_fn = shift(@ARGV);
 my $out_fn = shift(@ARGV);
@@ -14,11 +13,10 @@ my $abs_xml_fn = File::Spec->rel2abs($xml_fn);
 
 my $abs_out_fn = File::Spec->rel2abs($out_fn);
 
-my $xml_data_gen_cmd = 
-    q#perl -MXML::Grammar::Fortune -e 'XML::Grammar::Fortune->new({mode => "convert_to_html", output_mode => "filename"})->run({input => shift(@ARGV), output => shift(@ARGV)})' # .  shell_quote($abs_xml_fn, $abs_out_fn);
-
-# print STDERR $xml_data_gen_cmd; exit(0);
-system($xml_data_gen_cmd);
+XML::Grammar::Fortune
+    ->new({mode => "convert_to_html", output_mode => "filename"})
+    ->run({input => $abs_xml_fn, output => $abs_out_fn})
+    ;
 
 open my $text_fh, "<", $abs_out_fn;
 binmode ($text_fh, ":utf8");

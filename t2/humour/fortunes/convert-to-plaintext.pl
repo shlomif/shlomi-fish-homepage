e3b2758358921b72980ba7c0355317549d790030
shlomif <shlomif@cec68495-dca5-4e2b-845c-11fdaaa4f967>
2009-02-21 01:42:41 +0000

    r6124@telaviv1:  shlomi | 2008-08-21 17:57:45 +0300  Fortunes - now calling the system perl now that the bug was fixed.
     Bug: https://qa.mandriva.com/show_bug.cgi?id=41993 .

--- t2/humour/fortunes/convert-to-plaintext.pl
+++ t2/humour/fortunes/convert-to-plaintext.pl
@@ -1,16 +1,9 @@
-use Cwd;
-use File::Spec;
-use String::ShellQuote;
 use strict;
 use warnings;
 
-### Definitions:
-
-my $fortune_xml_base_dir = "$ENV{HOME}/progs/perl/cpan/XML/Grammar/Fortune/trunk/XML-Grammar-Fortune/module";
-
-my $good_perl_path = "$ENV{HOME}/apps/perl/perl-5.8.x-latest/bin/perl";
-
-##########################################################################
+use Cwd;
+use File::Spec;
+use String::ShellQuote;
 
 my $xml_fn = shift(@ARGV);
 my $out_fn = shift(@ARGV);
@@ -22,8 +15,8 @@ my $abs_xml_fn = File::Spec->rel2abs($xml_fn);
 
 my $abs_out_fn = File::Spec->rel2abs($out_fn);
 
-my $xml_data_gen_cmd = "cd " . shell_quote($fortune_xml_base_dir) . 
-    q# ; perl -Mblib -MXML::Grammar::Fortune::ToText -e 'my ($in_fn, $out_fn) = @ARGV; open my $out, ">", $out_fn; binmode($out, ":utf8"); XML::Grammar::Fortune::ToText->new({input => shift(@ARGV), output => $out})->run();close($out)' # .  shell_quote($abs_xml_fn, $abs_out_fn);
+my $xml_data_gen_cmd = 
+    q#perl -MXML::Grammar::Fortune::ToText -e 'my ($in_fn, $out_fn) = @ARGV; open my $out, ">", $out_fn; binmode($out, ":utf8"); XML::Grammar::Fortune::ToText->new({input => shift(@ARGV), output => $out})->run();close($out)' # .  shell_quote($abs_xml_fn, $abs_out_fn);
 
 # print STDERR $xml_data_gen_cmd; exit(0);
 system($xml_data_gen_cmd);
--- t2/humour/fortunes/convert-to-xhtml.pl
+++ t2/humour/fortunes/convert-to-xhtml.pl
@@ -1,29 +1,21 @@
-use Cwd;
-use File::Spec;
-use String::ShellQuote;
 use strict;
 use warnings;
 
-### Definitions:
-
-my $fortune_xml_base_dir = "$ENV{HOME}/progs/perl/cpan/XML/Grammar/Fortune/trunk/XML-Grammar-Fortune/module";
-
-my $good_perl_path = "$ENV{HOME}/apps/perl/perl-5.8.x-latest/bin/perl";
-
-##########################################################################
+use Cwd;
+use File::Spec;
+use String::ShellQuote;
 
 my $xml_fn = shift(@ARGV);
 my $out_fn = shift(@ARGV);
 
 my $html_fn = $xml_fn;
 
-
 my $abs_xml_fn = File::Spec->rel2abs($xml_fn);
 
 my $abs_out_fn = File::Spec->rel2abs($out_fn);
 
 my $xml_data_gen_cmd = 
-    q#~/apps/perl/perl-5.8.x-latest/bin/perl -MXML::Grammar::Fortune -e 'XML::Grammar::Fortune->new({mode => "convert_to_html", output_mode => "filename"})->run({input => shift(@ARGV), output => shift(@ARGV)})' # .  shell_quote($abs_xml_fn, $abs_out_fn);
+    q#perl -MXML::Grammar::Fortune -e 'XML::Grammar::Fortune->new({mode => "convert_to_html", output_mode => "filename"})->run({input => shift(@ARGV), output => shift(@ARGV)})' # .  shell_quote($abs_xml_fn, $abs_out_fn);
 
 # print STDERR $xml_data_gen_cmd; exit(0);
 system($xml_data_gen_cmd);

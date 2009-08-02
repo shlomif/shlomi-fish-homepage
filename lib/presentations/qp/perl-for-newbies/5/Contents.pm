c43432f77f2018b8151a3c290f7d75fa02149ec6
shlomif <shlomif@cec68495-dca5-4e2b-845c-11fdaaa4f967>
2009-08-02 14:09:02 +0000

    Added the 5th part of Perl-for-Newbies (under development).

---
+++ lib/presentations/qp/perl-for-newbies/5/Contents.pm
@@ -0,0 +1,85 @@
+package Contents;
+
+use strict;
+
+my $contents =
+{
+    'title' => "\"Perl for Perl Newbies\" - Part 5 - Good Programming Practices",
+    'subs' =>
+    [
+        {
+            url => "intro.html",
+            title => "Introduction",
+        },
+
+        {
+            url => "testing",
+            title => "Automated Testing",
+            subs =>
+            [
+                {
+                    url => "motivation.html",
+                    title => "Motivation for Testing",
+                },
+                {
+                    url => "demo",
+                    title => "Demo",
+                    subs =>
+                    [
+                        {
+                            url => "test-more.html",
+                            title => "Test::More",
+                        },
+                        {
+                            url => "Build-test.html",
+                            title => "./Build test",
+                        },
+                    ],
+                },
+                {
+                    url => "types.html",
+                    title => "Types of Tests: Unit Tests, Integration Tests, System Tests",
+                },
+                {
+                    url => "mocking.html",
+                    title => "Mocking",
+                },
+                {
+                    url => "data-driven",
+                    title => "Data-Driven Testing",
+                    subs =>
+                    [
+                        {
+                            url => "fit.html",
+                            title => "FIT",
+                        },
+                        {
+                            url => "Test-Base.html",
+                            title => "Test-Base",
+                        },
+                    ],
+                },
+            ],
+        },
+    ],
+    'images' =>
+    [
+        'style.css',
+        'somerights20.gif',
+        qw(
+testing/demo/Add1.pm
+testing/demo/add1-test-2.pl
+testing/demo/add1-test.pl
+testing/demo/Add2.pm
+testing/demo/add2-test-2.pl
+testing/demo/Test-More-1.t
+        ),
+    ],
+};
+
+sub get_contents
+{
+    return $contents;
+}
+
+1;

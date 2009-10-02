a518424207afd3b587ce43d075fb6f3704973048
shlomif <shlomif@iglu.org.il@cec68495-dca5-4e2b-845c-11fdaaa4f967>
2009-10-02 10:38:46 +0000

    [P4N4] Add error checking to the backticks.

--- lib/presentations/qp/perl-for-newbies/4/src/processes/count-dirs.pl
+++ lib/presentations/qp/perl-for-newbies/4/src/processes/count-dirs.pl
@@ -5,12 +5,24 @@ use warnings;
 
 my $dir = shift;
 # Prepare $dir for placement inside a '...' argument
+# A safer way would be to use String::ShellQuote
 $dir =~ s!'!'\\''!g;
 
 my $count = `ls -l '$dir' | grep ^d | wc -l`;
 
-$count =~ /(\d+)/;
-# Retrieve the number via the special regex variable $1
-$count = $1;
+if ($?)
+{
+    die "Error returned by ls -l command is $@.";
+}
 
-print "There are $count directories\n";
+if ($count !~ /(\d+)/)
+{
+    # Retrieve the number via the special regex variable $1
+    $count = $1;
+    print "There are $count directories\n";
+}
+else
+{
+    die "Wrong output."
+}
+

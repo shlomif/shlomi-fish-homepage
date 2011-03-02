44a09820a514f13205138b4506b3bda1409f7501
shlomif <shlomif@iglu.org.il@cec68495-dca5-4e2b-845c-11fdaaa4f967>
2011-03-02 23:39:12 +0000

    The script works now pretty well - not to test the result.

--- bin/processors/convert-hitchhiker-guide-to-st-tng-to-screenplay-xml.pl
+++ bin/processors/convert-hitchhiker-guide-to-st-tng-to-screenplay-xml.pl
@@ -3,6 +3,8 @@
 use strict;
 use warnings;
 
+use utf8;
+
 use IO::All;
 
 my $text =
@@ -12,31 +14,41 @@ sub prefix_lines
 {
     my $s = shift;
 
-    $s =~ s{^}{\\#}ms;
-    $s =~ s{$}{<br />}ms;
+    $s =~ s{^}{// }gms;
+    $s =~ s{$}{<br />}gms;
 
     return $s;
+}
+
+sub wrap_as_desc
+{
+    my $s = shift;
+    return '[' . prefix_lines($s) . "]\n\n";
 }
 
 sub process_scene
 {
     my $s = shift;
 
-    $s =~ s{^ +}{};
+    $s =~ s{^ +}{}gms;
     $s =~ tr/()/[]/;
 
     return $s;
 }
 
 my $END = '* * *      T  h  e      E  n  d      * * *';
-$text =~ s{\A(.*?)^(SCENE 1:)}{[prefix_lines($1)]\n\n$2}ms;
+$text =~ s{\A(.*?)^(SCENE 1:)}{wrap_as_desc($1) . $2}ems;
 $text =~ 
-s{^((?:SCENE (\d+)|EPILOGUE):[^\n]*\n(?: +[^\n]*\n))(.*?)(?=^(?:(?:SCENE)|(?:\Q$END\E)))}
-{my $id = defined($2) ? $2 : "EPILOGUE"; 
-    qq{<scene id="scene_$id" title="Scene $id">\n\n}
+s{^((?:SCENE (\d+)|EPILOGUE):[^\n]*\n(?: +[^\n]*\n)*)(.*?)(?=^(?:(?:SCENE|EPILOGUE)|(?:\s*\Q$END\E)))}
+{
+    # print "Foo <D1 = $1> <D2 = $2> <D3 = $3>"; 
+    my $id = defined($2) ? $2 : "EPILOGUE"; 
+    qq{\n\n<scene id="scene_$id" title="Scene $id">\n\n}
     . "[\n" . prefix_lines($1)
-    . "\n]\n\n" . process_scene($3)
-    . "</scene>"
+    . "\n]\n\n" . process_scene($3) .
+    "\n\n</scene>\n\n"
 }egms;
+
+$text =~ s{^(\s*\Q$END\E.*)\z}{wrap_as_desc($1)}ems;
 
 print($text);

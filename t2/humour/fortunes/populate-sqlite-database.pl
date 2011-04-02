c556302133ff20016cf39886eeb5391505f59053
shlomif <shlomif@iglu.org.il>
2011-04-02 14:53:22 +0000

    Script is mostly finished and inserts into a database.
    TODO : check that the database is sane.

--- t2/humour/fortunes/populate-sqlite-database.pl
+++ t2/humour/fortunes/populate-sqlite-database.pl
@@ -15,7 +15,19 @@ use DBI;
 use File::Spec;
 use IO::All;
 
-my $dbh = DBI->connect("dbi:SQLite:dbname=$script_dir/$db_base_name","","");
+STDOUT->autoflush(1);
+
+my $full_db_path = "$script_dir/$db_base_name";
+
+# To reset the database.
+# May not be a good idea in the future.
+unlink($full_db_path, "$full_db_path-journal");
+
+my $dbh = DBI->connect("dbi:SQLite:dbname=$full_db_path","","");
+
+$dbh->do("CREATE TABLE fortune_cookies (id INTEGER PRIMARY KEY ASC, str_id VARCHAR(255), text TEXT)");
+
+my $insert_sth = $dbh->prepare("INSERT INTO fortune_cookies (str_id, text) VALUES(?, ?)");
 
 my @lines = io->file("$script_dir/fortunes-list.mak")->getlines();
 my @file_bases = (map { /(\b[a-z_\-]+\b)/g } @lines);
@@ -26,11 +38,29 @@ foreach my $basename (@file_bases)
 
     my $nodes_list = $tree->findnodes(q{//div[@class = "fortune"]});
 
+    my $count = @$nodes_list;
+
+    my $idx = 0;
+
     while (defined(my $node = shift(@$nodes_list)))
     {
+        printf ("%-70s\r", "$basename $idx/$count");
         my $id = $node->findnodes(q{//h3[@id]})->[0]->id;
-        print "\n\n\n[[[START ID=$id]]]\n";
-        print $node->as_XML();
-        print "\n[[[END]]]\n\n\n";
+
+        if (! $id)
+        {
+            die "No ID in file '$basename' in " . $node->as_XML . "!";
+        }
+
+        $insert_sth->execute($id, $node->as_XML());
+
+        # Some debugging statement.
+        # print "\n\n\n[[[START ID=$id]]]\n";
+        # print $node->as_XML();
+        # print "\n[[[END]]]\n\n\n";
+    }
+    continue
+    {
+        $idx++;
     }
 }

b4fa4623fd5d596209c67f7adbf5472ad085502f
shlomif <shlomif@iglu.org.il@cec68495-dca5-4e2b-845c-11fdaaa4f967>
2011-04-03 17:09:30 +0000

    Mostly merged the 3 gen-prod-synd files.

--- lib/Shlomif/Homepage/Amazon.pm
+++ lib/Shlomif/Homepage/Amazon.pm
@@ -2,14 +2,47 @@ package Shlomif::Homepage::Amazon;
 
 use Moose;
 
+use XML::Grammar::ProductsSyndication;
+use XML::LibXML::XPathContext;
 use Term::ReadPassword;
 
 has 'wml_dir' => (isa => 'Str', is => 'ro');
-has 'ps' => (isa => 'XML::Grammar::ProductsSyndication', is => 'ro');
+has 'lib_dir' => (isa => 'Str', is => 'ro');
+has 'xml_basename' => (isa => 'Str', is => 'ro');
+has 'ps' => (isa => 'XML::Grammar::ProductsSyndication', is => 'rw');
+
+sub BUILD
+{
+    my $self = shift;
+
+    $self->ps(XML::Grammar::ProductsSyndication->new(
+        {
+            'source' =>
+            {
+                'file' => sprintf("%s/%s", $self->wml_dir, $self->xml_basename),
+            }
+        },
+    ));
+
+}
 
 sub process
 {
     my ($self) = @_;
+
+    if (!$self->ps->is_valid())
+    {
+        die "Not valid.";
+    }
+    my $xml = $self->ps->transform_into_html({ 'output' => "xml" });
+
+    my $xc = XML::LibXML::XPathContext->new($xml);
+    $xc->registerNs('html' => "http://www.w3.org/1999/xhtml");
+
+    open my $out, ">", $self->lib_dir() . "/include-me.html";
+    binmode $out, ":utf8";
+    print {$out} $xc->findnodes('/html:html/html:body/html:div')->[0]->toString(0);
+    close ($out);
 
     $self->ps->update_cover_images(
         {

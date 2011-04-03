9c60555f5018b9e392a1eb0a142a6a9a9f9492a4
shlomif <shlomif@iglu.org.il>
2011-04-03 17:16:08 +0000

    Convert to implement everything inside an import.

--- lib/prod-synd/films/gen-prod-synd.pl
+++ lib/prod-synd/films/gen-prod-synd.pl
@@ -3,21 +3,9 @@
 use strict;
 use warnings;
 
-use Shlomif::Homepage::Amazon;
-
-use XML::Grammar::ProductsSyndication;
-use XML::LibXML::XPathContext;
-
-my $wml_dir = "t2/humour/recommendations/films";
-my $lib_dir = "lib/prod-synd/films";
-my $xml_basename = "shlomi-fish-films-recommendations.xml";
-
-my $amazon = 
-    Shlomif::Homepage::Amazon->new(
-    {
-        lib_dir => $lib_dir,
-        xml_basename => $xml_basename,
-        wml_dir => $wml_dir,
-    });
-
-$amazon->process;
+use Shlomif::Homepage::Amazon 
+{
+    wml_dir => "t2/humour/recommendations/films",
+    lib_dir => "lib/prod-synd/films",
+    xml_basename => "shlomi-fish-films-recommendations.xml",
+};
--- lib/prod-synd/music/gen-prod-synd.pl
+++ lib/prod-synd/music/gen-prod-synd.pl
@@ -3,18 +3,10 @@
 use strict;
 use warnings;
 
-use Shlomif::Homepage::Amazon;
-
-my $wml_dir = "t2/art/recommendations/music";
-my $lib_dir = "lib/prod-synd/music";
-my $xml_basename = "shlomi-fish-music-recommendations.xml";
-
-my $amazon = 
-    Shlomif::Homepage::Amazon->new(
+use Shlomif::Homepage::Amazon
     {
-        lib_dir => $lib_dir,
-        xml_basename => $xml_basename,
-        wml_dir => $wml_dir,
-    });
-
-$amazon->process;
+        wml_dir => "t2/art/recommendations/music",
+        lib_dir => "lib/prod-synd/music",
+        xml_basename => "shlomi-fish-music-recommendations.xml",
+    }
+    ;

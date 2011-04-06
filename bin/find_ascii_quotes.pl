21ddff4190ae4fb53062410b01220dcd510015c0
shlomif <shlomif@iglu.org.il>
2011-04-06 14:59:04 +0000

    Fix the find_ascii_quotes perl script.

--- bin/find_ascii_quotes.pl
+++ bin/find_ascii_quotes.pl
@@ -10,11 +10,13 @@ use Getopt::Long;
 
 my $out_fn;
 
+binmode STDOUT, ":utf8";
+
 # Input the filename
 foreach my $filename (@ARGV)
 {
     my $reader = XML::LibXML::Reader->new(
-        location => $filename, load_ext_dtd => 0, 'no_network' => 1
+        location => $filename, load_ext_dtd => 1, 'no_network' => 1,
     ) or die "Cannot read '$filename'.";
     while ($reader->read())
     {

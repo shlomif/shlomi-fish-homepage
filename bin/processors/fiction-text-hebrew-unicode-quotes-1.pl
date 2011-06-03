20fdc509794c14319f63808ce99762389115a6d2
shlomif <shlomif@iglu.org.il@cec68495-dca5-4e2b-845c-11fdaaa4f967>
2011-06-03 22:55:30 +0000

    Improved the processor.

--- bin/processors/fiction-text-hebrew-unicode-quotes-1.pl
+++ bin/processors/fiction-text-hebrew-unicode-quotes-1.pl
@@ -3,15 +3,45 @@
 use strict;
 use warnings;
 
+
 use utf8;
 
 use open IO => ':encoding(utf8)';
 
+use Carp;
+
 local $/;
+
+sub _process_paragraph
+{
+    my $s = shift;
+
+    if ($s =~ m{<})
+    {
+        return $s;
+    }
+
+    my $count = () = ($s =~ m{"}g);
+
+    if ($count % 2 != 0)
+    {
+        return $s;
+    }
+
+    my $i = 0;
+    $s =~ s/"/(($i++) % 2 == 0) ? '„' : '“'/ge;
+
+    if ($i != $count)
+    {
+        Carp::confess("Error in paragraph '$s'");
+    }
+
+    return $s;
+}
 
 my $text = <ARGV>;
 
-$text =~ s{(?<=\n\n)"([^"]+)"(?=\n\n)}{„$1“}msg;
+$text =~ s{(?<=\n\n)((?:(?: *\S+)+\n)+)(?=\n)}{_process_paragraph($1)}egms;
 
 binmode STDOUT, ':encoding(utf8)';
 print $text;

7de378a00ed2ce5bd49c3e6083c84a084d02aae0
shlomif <shlomif@iglu.org.il@cec68495-dca5-4e2b-845c-11fdaaa4f967>
2011-06-04 21:16:46 +0000

    skipping already processed.

--- bin/t2_find_ascii_quotes.bash
+++ bin/t2_find_ascii_quotes.bash
@@ -9,8 +9,14 @@ temp_filter()
     grep -vP '^(dest/t2-homepage/lecture/)'
 }
 
+temp_only_from_reached()
+{
+    perl -lne 'print if /MathVentures/..1'
+}
+
 find dest/t2-homepage/ -regextype posix-extended -regex '.*x?html' -print | 
     sort | 
+    temp_only_from_reached |
     grep -vP '(catb-heb|WebMetaLecture/slides/examples|t2-homepage/rewrite\.html|humour/by-others/|humour/bits/COBOL-the-New-Age|humour/bits/Mastering-Cat|humour/fortunes/nyh-sigs|humour/fortunes/sharp-perl|humour/fortunes/sharp-programming|humour/fortunes/|humour/human-hacking/arabic-v2|humour/human-hacking/human-hacking-field-guide/|humour/human-hacking/human-hacking-field-guide-v2-arabic/|humour/TheEnemy/TheEnemy_eng\.html|humour/TheEnemy/The-Enemy-English-rev4\.html|humour/TheEnemy/The-Enemy-English-rev5\.html|humour/TheEnemy/The-Enemy-English-rev6\.html|humour/TheEnemy/The-Enemy-English-v7/|humour/TheEnemy/The-Enemy-Hebrew-v7\.html|humour/TheEnemy/The-Enemy-English-v7\.html|humour/TheEnemy/TheEnemy\.html|humour/TheEnemy/The-Enemy-rev[456]\.html)' | 
     temp_filter |
     xargs -d '\n' perl bin/find_ascii_quotes-xmlp.pl > results.txt

dce330eef830db23ba960986d65e19c8c362beda
shlomif <shlomif@iglu.org.il@cec68495-dca5-4e2b-845c-11fdaaa4f967>
2011-06-04 12:19:10 +0000

    Add a whitelist to the script.

--- bin/t2_find_ascii_quotes.bash
+++ bin/t2_find_ascii_quotes.bash
@@ -1,2 +1,2 @@
 #!/bin/bash
-find dest/t2-homepage/ -regextype posix-extended -regex '.*x?html' -print | sort | grep -vP '(catb-heb|WebMetaLecture/slides/examples|t2-homepage/rewrite\.html|humour/by-others/)' | xargs -d '\n' perl bin/find_ascii_quotes-xmlp.pl > results.txt
+find dest/t2-homepage/ -regextype posix-extended -regex '.*x?html' -print | sort | grep -vP '(catb-heb|WebMetaLecture/slides/examples|t2-homepage/rewrite\.html|humour/by-others/|humour/bits/COBOL-the-New-Age|humour/bits/Mastering-Cat)' | xargs -d '\n' perl bin/find_ascii_quotes-xmlp.pl > results.txt

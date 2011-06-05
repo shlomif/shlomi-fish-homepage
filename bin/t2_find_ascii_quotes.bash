99d6185a240397ae3f4a94fa66e9f2ceb77a53e5
shlomif <shlomif@iglu.org.il>
2011-06-05 14:51:07 +0000

    Renamed into ascii_results.

--- bin/t2_find_ascii_quotes.bash
+++ bin/t2_find_ascii_quotes.bash
@@ -37,4 +37,5 @@ find dest/t2-homepage/ -regextype posix-extended -regex '.*x?html' -print |
     grep -vP 'philosophy/computers/software-management/perfect-workplace/perfect-it-workplace(/|\.xhtml$)' | # old
     grep -vP 'philosophy/computers/web/create-a-great-personal-homesite/index\.html' | # in code
     grep -vP 'philosophy/computers/web/create-a-great-personal-homesite/rev2\.html' | # in code
-    xargs -d '\n' perl bin/find_ascii_quotes-xmlp.pl > results.txt
+    xargs -d '\n' perl bin/find_ascii_quotes-xmlp.pl > \
+        ascii_quotes_results.txt

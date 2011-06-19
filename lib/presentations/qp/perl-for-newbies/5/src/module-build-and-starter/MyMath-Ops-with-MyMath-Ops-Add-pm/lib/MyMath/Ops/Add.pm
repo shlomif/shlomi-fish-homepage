9c1e355a485c36480e63b8060d62fe6acd198d2a
shlomif <shlomif@iglu.org.il>
2011-06-19 12:12:39 +0000

    Add more.

---
+++ lib/presentations/qp/perl-for-newbies/5/src/module-build-and-starter/MyMath-Ops-with-MyMath-Ops-Add-pm/lib/MyMath/Ops/Add.pm
@@ -0,0 +1,147 @@
+package MyMath::Ops::Add;
+
+use warnings;
+use strict;
+
+=head1 NAME
+
+MyMath::Ops::Add - The great new MyMath::Ops::Add!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use MyMath::Ops::Add;
+
+    my $foo = MyMath::Ops::Add->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 FUNCTIONS
+
+=head2 new
+
+Construct a new object.
+
+=cut
+
+sub new
+{
+    my $class = shift;
+
+    my $self = bless {}, $class;
+
+    $self->_init(@_);
+
+    return $self;
+}
+
+sub _init
+{
+    my $self = shift;
+
+    return;
+}
+
+=head2 $self->add($x, $y)
+
+Adds two numbers.
+
+=cut
+
+sub add
+{
+    my $self = shift;
+    
+    my ($x, $y) = @_;
+    return $x+$y;
+}
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Perl Newbie, C<< <perl-newbie at perl-begin.org> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-mymath::ops at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=MyMath::Ops>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc MyMath::Ops::Add
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=MyMath::Ops>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/MyMath::Ops>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/MyMath::Ops>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/MyMath::Ops/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2009 Perl Newbie.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See http://dev.perl.org/licenses/ for more information.
+
+
+=cut
+
+1; # End of MyMath::Ops::Add

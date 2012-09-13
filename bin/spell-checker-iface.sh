53f32bb5086a932a510e311829270e89d3960f2a
Shlomi Fish <shlomif@shlomifish.org>
2012-09-14 00:28:25 +0300

    Spell check more.

--- bin/spell-checker-iface.sh
+++ bin/spell-checker-iface.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 find dest/t2-homepage/ -name '*.html' -or -name '*.xhtml' |
     ( LC_ALL=C sort  ) |
-    perl -lne 'print if 1../MathVentures/'|
+    perl -lne 'print if 1../MathVentures\/3d/'|
     xargs perl bin/html-check-spelling-xmlp.pl |
     grep ':'
     # perl -lne 'print if /MathVentures\/3d.*\.xhtml/' |

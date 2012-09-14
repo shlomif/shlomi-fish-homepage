52cd4c439c908b972a0a0067ce32cf15b71c6435
Shlomi Fish <shlomif@shlomifish.org>
2012-09-14 16:27:25 +0300

    Correct more spelling problems.

--- bin/spell-checker-iface.sh
+++ bin/spell-checker-iface.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 find dest/t2-homepage/ -name '*.html' -or -name '*.xhtml' |
     ( LC_ALL=C sort  ) |
-    perl -lne 'print if 1../MathVentures\/bugs/'|
+    perl -lne 'print if 1..m#MathVentures/disco#'|
     xargs perl bin/html-check-spelling-xmlp.pl |
     grep ':'
     # perl -lne 'print if /MathVentures\/3d.*\.xhtml/' |

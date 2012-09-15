383a2f6213db25115bde79d10aa7563894c113b0
Shlomi Fish <shlomif@shlomifish.org>
2012-09-15 12:11:35 +0300

    Spell check up to humour-heb.

--- bin/spell-checker-iface.sh
+++ bin/spell-checker-iface.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 find dest/t2-homepage/ -name '*.html' -or -name '*.xhtml' |
     ( LC_ALL=C sort  ) |
-    perl -lne 'print if 1..m#homesteading/catb#'|
+    perl -lne 'print if 1..m#humour-heb#'|
     grep -vP '(guide2ee/undergrad)' |
     xargs perl bin/html-check-spelling-xmlp.pl |
     grep ':'

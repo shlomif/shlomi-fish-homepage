3c85e678e0c706fdb34f71866f07e3e2e94e5f4a
Shlomi Fish <shlomif@shlomifish.org>
2012-09-15 12:01:40 +0300

    Spell check more.

--- bin/spell-checker-iface.sh
+++ bin/spell-checker-iface.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 find dest/t2-homepage/ -name '*.html' -or -name '*.xhtml' |
     ( LC_ALL=C sort  ) |
-    perl -lne 'print if 1..m#grad-fu/index#'|
+    perl -lne 'print if 1..m#guide2ee/index#'|
     xargs perl bin/html-check-spelling-xmlp.pl |
     grep ':'
     # perl -lne 'print if /MathVentures\/3d.*\.xhtml/' |

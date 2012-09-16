e7d3e77f3fec2af2b9daac2755c6b1ca6ebf4e44
Shlomi Fish <shlomif@shlomifish.org>
2012-09-16 13:00:28 +0300

    More spell-checking.

--- bin/spell-checker-iface.sh
+++ bin/spell-checker-iface.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 find dest/t2-homepage/ -name '*.html' -or -name '*.xhtml' |
     ( LC_ALL=C sort  ) |
-    perl -lne 'print if 1..m#humour\.html#'|
+    perl -lne 'print if 1..m#humour/GNU-Visual-Basic#'|
     grep -vP '(guide2ee/undergrad)' |
     xargs perl bin/html-check-spelling-xmlp.pl |
     grep ':'

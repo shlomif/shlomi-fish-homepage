a9c403ad919a27cb4b8b3128bbd5cf190eae1243
Shlomi Fish <shlomif@shlomifish.org>
2012-09-18 16:03:54 +0300

    Spell checked TOWTF.

--- bin/spell-checker-iface.sh
+++ bin/spell-checker-iface.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 find dest/t2-homepage/ -name '*.html' -or -name '*.xhtml' |
     ( LC_ALL=C sort  ) |
-    perl -lne 'print if 1..m#humour/Star-Trek/index#'|
+    perl -lne 'print if 1..m#humour/TOWTF/index#'|
     grep -vP '(guide2ee/undergrad)' |
     xargs perl bin/html-check-spelling-xmlp.pl |
     grep ':'

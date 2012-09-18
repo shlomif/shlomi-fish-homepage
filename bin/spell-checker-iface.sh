0e3ee350f91d294dafa93ebac5b000ae53bd4157
Shlomi Fish <shlomif@shlomifish.org>
2012-09-18 18:19:39 +0300

    Spell checked up to "The Enemy".

--- bin/spell-checker-iface.sh
+++ bin/spell-checker-iface.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 find dest/t2-homepage/ -name '*.html' -or -name '*.xhtml' |
     ( LC_ALL=C sort  ) |
-    perl -lne 'print if 1..m#humour/TOWTF/index#'|
+    perl -lne 'print if 1..m#humour/TheEnemy/The-Enemy-English-v7#'|
     grep -vP '(guide2ee/undergrad)' |
+    grep -vP '(humour/TheEnemy/The-Enemy-(?:English-)?rev)' |
     xargs perl bin/html-check-spelling-xmlp.pl |
     grep ':'
     # perl -lne 'print if /MathVentures\/3d.*\.xhtml/' |

ae83649047f4f3c2849db2a1f6864a95da87ef3a
Shlomi Fish <shlomif@shlomifish.org>
2012-09-19 14:54:19 +0300

    Correct more typos.

--- bin/spell-checker-iface.sh
+++ bin/spell-checker-iface.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 find dest/t2-homepage/ -name '*.html' -or -name '*.xhtml' |
     ( LC_ALL=C sort  ) |
-    perl -lne 'print if 1..m#humour/bits/Crackas#'|
+    perl -lne 'print if 1..m#humour/bits/How-many-Wikipedia-Editors#'|
     grep -vP '(guide2ee/undergrad)' |
     grep -vP '(?:humour/TheEnemy/(?:The-Enemy-(?:English-)?rev|TheEnemy))' |
     xargs perl bin/html-check-spelling-xmlp.pl |

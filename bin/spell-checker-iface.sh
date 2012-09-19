3b8f74ce91a44fb9b1855f19199faf6e84ad86aa
Shlomi Fish <shlomif@shlomifish.org>
2012-09-19 12:35:25 +0300

    Spell check more.

--- bin/spell-checker-iface.sh
+++ bin/spell-checker-iface.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 find dest/t2-homepage/ -name '*.html' -or -name '*.xhtml' |
     ( LC_ALL=C sort  ) |
-    perl -lne 'print if 1..m#humour/TheEnemy/The-Enemy-Hebrew-v7#'|
+    perl -lne 'print if 1..m#humour/aphorisms#'|
     grep -vP '(guide2ee/undergrad)' |
-    grep -vP '(humour/TheEnemy/The-Enemy-(?:English-)?rev)' |
+    grep -vP '(?:humour/TheEnemy/(?:The-Enemy-(?:English-)?rev|TheEnemy))' |
     xargs perl bin/html-check-spelling-xmlp.pl |
     grep ':'
     # perl -lne 'print if /MathVentures\/3d.*\.xhtml/' |

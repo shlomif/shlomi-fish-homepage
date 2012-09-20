6d4c4593d2a04e71f0ee725f9f59076cf7c41738
Shlomi Fish <shlomif@shlomifish.org>
2012-09-20 19:51:39 +0300

    Spell checked the HHFG.

--- bin/spell-checker-iface.sh
+++ bin/spell-checker-iface.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 find dest/t2-homepage/ -name '*.html' -or -name '*.xhtml' |
     ( LC_ALL=C sort  ) |
-    perl -lne 'print if 1..m#humour/by-others/was-the-death-star-attack#'|
+    perl -lne 'print if 1..m#humour/human-hacking/index#'|
     grep -vP 'guide2ee/undergrad' |
     grep -vP '(?:humour/TheEnemy/(?:The-Enemy-(?:English-)?rev|TheEnemy))' |
     grep -vP '(?:humour/by-others/(?:English-is-a-Crazy-Language|darien|hitchhiker|how-many-newsgroup-readers|oded-c|s-stands-for-simple|technion-bit-1|top-12-things-likely|was-the-death-star-attack))' |
+    grep -vP 'humour/fortunes' |
+    grep -vP 'humour/human-hacking/.*arabic' |
+    grep -vP 'humour/human-hacking/human-hacking-field-guide/' |
+    grep -vP 'humour/human-hacking/hebrew-v2' |
     xargs perl bin/html-check-spelling-xmlp.pl |
     grep ':'
     # perl -lne 'print if /MathVentures\/3d.*\.xhtml/' |

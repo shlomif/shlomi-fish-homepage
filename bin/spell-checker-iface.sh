03886b0589bc38041cebe4cb341c8ae745e1b018
Shlomi Fish <shlomif@shlomifish.org>
2012-09-21 15:15:07 +0300

    Add Graduate Student Jokes.

--- bin/spell-checker-iface.sh
+++ bin/spell-checker-iface.sh
@@ -4,7 +4,7 @@ find dest/t2-homepage/ -name '*.html' -or -name '*.xhtml' |
     perl -lne 'print if 1..m#humour/index#'|
     grep -vP 'guide2ee/undergrad' |
     grep -vP '(?:humour/TheEnemy/(?:The-Enemy-(?:English-)?rev|TheEnemy))' |
-    grep -vP '(?:humour/by-others/(?:English-is-a-Crazy-Language|darien|hitchhiker|how-many-newsgroup-readers|oded-c|s-stands-for-simple|technion-bit-1|top-12-things-likely|was-the-death-star-attack))' |
+    grep -vP '(?:humour/by-others/(?:English-is-a-Crazy-Language|darien|hitchhiker|how-many-newsgroup-readers|oded-c|s-stands-for-simple|technion-bit-1|top-12-things-likely|was-the-death-star-attack|grad-student-jokes-from-jnoakes))' |
     grep -vP 'humour/fortunes' |
     grep -vP 'humour/human-hacking/.*arabic' |
     grep -vP 'humour/human-hacking/human-hacking-field-guide/' |

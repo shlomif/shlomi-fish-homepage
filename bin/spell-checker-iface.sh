a4646a7f0ed064e062951e4d5046aadbf06e80c8
Shlomi Fish <shlomif@shlomifish.org>
2012-09-23 10:58:26 +0200

    Add more.

--- bin/spell-checker-iface.sh
+++ bin/spell-checker-iface.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 find dest/t2-homepage/ -name '*.html' -or -name '*.xhtml' |
     ( LC_ALL=C sort  ) |
-    perl -lne 'print if 1..m#^\Qdest/t2-homepage/philosophy/ideas/unixdoc/index.html\E#' |
+    perl -lne 'print if 1..m#^\Qdest/t2-homepage/philosophy/index.html\E#' |
     grep -vP 'philosophy/foss-other-beasts/revision-2/' |
     grep -vP 'guide2ee/undergrad' |
     grep -vP '(?:humour/TheEnemy/(?:The-Enemy-(?:English-)?rev|TheEnemy))' |

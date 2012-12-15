ff211d159ad759070c23a7fdcb8b7c33f6b1cb84
Shlomi Fish <shlomif@shlomifish.org>
2012-12-15 19:40:49 +0200

    Add the test suite for the client-side JS.

---
+++ Tests/js/jqtree-from-h-w-json-nav-menu-test.js
@@ -0,0 +1,16 @@
+"use strict";
+/*
+ * Tests for the jQtree generation routines.
+ * Copyright by Shlomi Fish, 2012.
+ * */
+function test_nav_menu_generation()
+{
+    module("NavMenu.Main.ToJQTree");
+
+    test("NavMenu main test", function () {
+        expect(1);
+
+        // TEST
+        ok (true, "True is, well, true.");
+    });
+}

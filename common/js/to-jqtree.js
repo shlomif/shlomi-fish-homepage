d2499bf39341fb0394fa9ad9341b6b1bda62e0af
Shlomi Fish <shlomif@shlomifish.org>
2012-12-18 23:46:46 +0200

    Add support for the IDs in _data/nav.json to to-jqtree.

--- common/js/to-jqtree.js
+++ common/js/to-jqtree.js
@@ -88,7 +88,6 @@ function calc_jqtree_data_from_html_w_nav_menu_json (args) {
 
     // TODO : XSS - cross site scripting.
     var _recurse;
-    var _next_id = 0;
 
     _recurse = function(sub_tree) {
         if ($.isArray(sub_tree)) {
@@ -101,11 +100,11 @@ function calc_jqtree_data_from_html_w_nav_menu_json (args) {
             title_attr = ' title="' + sub_tree['title'] + '"'
         }
         var ret = {
+            id: parseInt(sub_tree['id']),
             label: ("<a href=\"" +
                 escape_html(_get_rel(sub_tree['url'])) + "\"" + title_attr +
                 ">" + sub_tree['text'] + "</a>"
-                ),
-            id: (++_next_id),
+                )
         };
 
         if ('subs' in sub_tree) {

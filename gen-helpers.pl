c1d5cc8bbd74c3f4fab627222c3f4eab5b931ffd
Shlomi Fish <shlomif@shlomifish.org>
2012-12-25 20:12:40 +0200

    Better dependency tracking.
    Filtering more headers.

--- gen-helpers.pl
+++ gen-helpers.pl
@@ -7,6 +7,8 @@ use HTML::Latemp::GenMakeHelpers;
 use File::Find::Object::Rule;
 use IO::All;
 
+use List::MoreUtils;
+
 my $generator =
     HTML::Latemp::GenMakeHelpers->new(
         'hosts' =>
@@ -45,5 +47,36 @@ foreach my $cmd (
     _my_system($cmd);
 }
 
-1;
+=begin removed_duplicate_code
 
+my $wml_deps = io->file("deps.mak");
+
+foreach my $wml_obj (
+    io->dir("t2")->filter(sub { $_->filename =~ qr/\.wml\z/ })->All_Files
+)
+{
+    my $filename = $wml_obj->canonpath;
+    my @deps =
+        grep { $_ ne '../template.wml' }
+    List::MoreUtils::uniq(
+        map { /\A\s*#include (?:'([^']+)'|"([^"]+)"|<([^>]+)>)\s*\z/ ? ($1 || $2 || $3) : () }
+        grep { /\A\s*#include/ } $wml_obj->chomp->getlines);
+
+    foreach my $dep (@deps)
+    {
+        foreach my $dir (".", "lib")
+        {
+            my $full_dep = "$dir/$dep";
+            if (-e $full_dep)
+            {
+                print "$filename: $full_dep\n";
+            }
+        }
+    }
+}
+
+=end removed_duplicate_code
+
+=cut
+
+1;

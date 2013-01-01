c28fd4a512cf4535906a330870675e1a0b58f94c
Shlomi Fish <shlomif@shlomifish.org>
2013-01-01 13:35:17 +0200

    Global nav-arrows now traverse the whole site.

--- lib/MyNavLinks.pm
+++ lib/MyNavLinks.pm
@@ -3,8 +3,7 @@ package MyNavLinks;
 use strict;
 use warnings;
 
-use base 'HTML::Latemp::NavLinks::GenHtml::ArrowImages';
-
+use parent 'HTML::Latemp::NavLinks::GenHtml::ArrowImages';
 sub get_image_base
 {
     return "arrow-2-";

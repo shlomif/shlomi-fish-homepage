ea1e8141637023a39175b9c12723d365ab33544b
Shlomi Fish <shlomif@shlomifish.org>
2013-04-20 23:43:39 +0300

    Add the self-link - only on hover.

--- common/js/selfl.js
+++ common/js/selfl.js
@@ -3,12 +3,13 @@ function add_self_links()
     var myclass = "self_link";
     if (! $("body").hasClass(myclass))
     {
-        $("h1[id],h2[id],h3[id],h4[id],h5[id],h6[id]").each(function(i){ $(this).append( ' <span class="selfl">[<a href="#' + this.id + '">link</a>]</span>' ) })
+        $("h1[id],h2[id],h3[id],h4[id],h5[id],h6[id]").each(function(i){ $(this).append( '<span class="selfl"> <a href="#' + this.id + '">¶</a></span>' ) })
         $("body").addClass(myclass);
     }
 }
 
 $(function() {
+    add_self_links();
     $(".sub_menu ul.nav_links li a img").mouseover(function() {
             var src = $(this).attr("src").replace(/\.svg$/, "-pressed.svg");
             $(this).attr("src", src);

8f221a8bb606ad42caab3f75ef223e56e55186e3
Shlomi Fish <shlomif@shlomifish.org>
2013-04-21 13:19:08 +0300

    More <latemp_meta_desc>s.

--- bin/find_pages_wo_meta_desc.bash
+++ bin/find_pages_wo_meta_desc.bash
@@ -1,4 +1,4 @@
 #!/bin/bash
 find t2 -name '*.wml' | (LC_ALL=C sort) | \
     xargs grep -L latemp_meta_desc | \
-    grep -vP '^t2/(?:personal-heb\.|personal\.|SFresume|humour\.|humour-heb\.|lecture/Perl/Newbies/lecture5-heb-notes|me/resumes/Shlomi-Fish-Heb-Resume)'
+    grep -vP '^t2/(?:work/hire-me/hebrew\.|work/hire-me/index\.html|personal-heb\.|personal\.|SFresume|humour\.|humour-heb\.|lecture/Perl/Newbies/lecture5-heb-notes|me/resumes/Shlomi-Fish-Heb-Resume)'

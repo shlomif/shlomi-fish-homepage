82949649c1ba213d6c182e4ad1b197be901a8b04
Shlomi Fish <shlomif@shlomifish.org>
2013-04-22 19:56:03 +0300

    Add more.

--- gen-helpers.pl
+++ gen-helpers.pl
@@ -7,6 +7,7 @@ use HTML::Latemp::GenMakeHelpers;
 use File::Find::Object::Rule;
 use IO::All;
 use File::Which qw(which);
+use YAML::XS (qw(LoadFile));
 
 use List::MoreUtils;
 
@@ -41,93 +42,15 @@ use List::MoreUtils;
 }
 
 {
-    my %required_modules =
-    (
-        'CGI' => 0,
-        'CGI::Minimal' => 0,
-        'Carp' => 0,
-        'Cwd' => 0,
-        'DBD::SQLite' => 0,
-        'DBI' => 0,
-        'Data::Dumper' => 0,
-        'DateTime' => 0,
-        'Encode' => 0,
-        'Exporter' => 0,
-        'Fcntl' => 0,
-        'File::Basename' => 0,
-        'File::Copy' => 0,
-        'File::Find' => 0,
-        'File::Find::Object' => 0,
-        'File::Find::Object::Rule' => 0,
-        'File::Path' => 0,
-        'File::Spec' => 0,
-        'File::Spec::Functions' => 0,
-        'File::Which' => 0,
-        'Getopt::Long' => 0,
-        'HTML::Latemp::GenMakeHelpers' => 0,
-        'HTML::Parser' => '3.00',
-        'HTML::Tidy' => 0,
-        'HTML::TokeParser::Simple' => 0,
-        'HTML::TreeBuilder::LibXML' => 0,
-        'HTML::Widgets::NavMenu' => 0,
-        'HTML::Widgets::NavMenu::ToJSON' => 0,
-        'HTML::Widgets::NavMenu::ToJSON::Data_Persistence::YAML' => 0,
-        'IO::All' => 0,
-        'IO::Handle' => 0,
-        'Imager' => 0,
-        'JSON' => 0,
-        'LWP::Simple' => 0,
-        'List::MoreUtils' => 0,
-        'List::Util' => 0,
-        'MIME::Types' => 0,
-        'Math::Trig' => 0,
-        'Module::Build' => 0,
-        'Moose' => 0,
-        'POSIX' => 0,
-        'Parallel::ForkManager' => 0,
-        'SVG' => 0,
-        'String::ShellQuote' => 0,
-        'Template' => 0,
-        'Template::Preprocessor::TTML' => 0,
-        'Template::Preprocessor::TTML::CmdLineProc' => 0,
-        'Term::ReadPassword' => 0,
-        'Test::Count' => 0,
-        'Test::Count::Filter' => 0,
-        'Test::More' => 0,
-        'Text::Hunspell' => 0,
-        'Text::VimColor' => 0,
-        'XML::Feed' => 0,
-        'XML::Grammar::Fortune' => 0,
-        'XML::Grammar::Fiction::App::ToDocBook' => 0,
-        'XML::Grammar::Fiction::App::FromProto' => 0,
-        'XML::Grammar::Screenplay::App::FromProto' => 0,
-        'XML::Grammar::Screenplay::App::ToHTML' => 0,
-        'XML::Grammar::Fortune::Synd::App' => 0,
-        'XML::Grammar::Fortune::ToText' => 0,
-        'XML::Grammar::ProductsSyndication' => 0,
-        'XML::LibXML' => 0,
-        'XML::LibXML::Reader' => 0,
-        'XML::LibXML::XPathContext' => 0,
-        'XML::LibXSLT' => 0,
-        'XML::Parser' => 0,
-        'XML::Writer' => 0,
-        'XML::Writer' => 0,
-        'autodie' => 0,
-        'base' => 0,
-        'constant' => 0,
-        'lib' => 0,
-        'parent' => 0,
-        'strict' => 0,
-        'utf8' => 0,
-        'vars' => 0,
-        'warnings' => 0,
-    );
+    my ($yaml_data) = LoadFile("./bin/required-modules.yml");
+
+    my $required_modules = $yaml_data->{required}->{modules};
 
     my @not_found;
 
-    foreach my $m (sort { $a cmp $b } keys(%required_modules))
+    foreach my $m (sort { $a cmp $b } keys(%$required_modules))
     {
-        my $v = $required_modules{$m};
+        my $v = $required_modules->{$m};
         local $SIG{__WARN__} = sub {};
         my $verdict = eval( "use $m " . ($v ||'') . ';' );
         my $Err = $@;

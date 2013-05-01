1c162e383c3109797dd5fcf4b018698776dbb4cb
Shlomi Fish <shlomif@shlomifish.org>
2013-05-01 13:37:49 +0300

    Add the Print Version button.

---
+++ common/js/print-ver.js
@@ -0,0 +1,4 @@
+$('#prnt_ver').html('<button id="prnt_ver_link">Print Version</button>');
+$('#prnt_ver_link').click(function() {
+    alert("The page is printable as it is. Just hit print preview and you'll be able to print it.");
+});

25f4912effdbf8eddbe95ee142f49cadc6246c35
Shlomi Fish <shlomif@shlomifish.org>
2013-05-07 00:48:10 +0300

    Extract the data from the fortunes pages into a .yml file.
    Placed it there so we can generate t2/humour/fortunes/${id}.html.wml ,
    to avoid duplicate markup.

--- lib/Shlomif/Homepage/FortuneCollections.pm
+++ lib/Shlomif/Homepage/FortuneCollections.pm
@@ -34,10 +34,14 @@ package Shlomif::Homepage::FortuneCollections;
 use strict;
 use warnings;
 
+use 5.014;
+
 use utf8;
 
 use Carp;
 use Data::Dumper;
+
+use YAML::XS (qw(LoadFile DumpFile));
 
 use Shlomif::WrapAsUtf8 (qw(_wrap_as_utf8));
 
@@ -57,100 +61,14 @@ sub _init_fortune
     return Shlomif::Homepage::FortuneCollections::Record->new($rec);
 }
 
+my $yaml_data_fn = $INC{'Shlomif/Homepage/FortuneCollections.pm'} =~ s#/[^/]+\z#/fortunes-meta-data.yml#r;
+my $orig_fortunes_records = LoadFile($yaml_data_fn)->{'shlomif_fortunes_collections'}->{'fortunes'};
+
+# DumpFile('./foo.yml', { shlomif_fortunes_collections => { fortunes => \@orig_fortunes_records, }, }, );
+
 my @forts =
 (
-    map { _init_fortune($_) }
-(
-    {
-        id => "shlomif",
-        desc => "A collection of my own quotes.",
-        text => "shlomif",
-        title => "Quotes by Shlomi Fish",
-    },
-    {
-        id => "shlomif-fav",
-        desc => "Favourite quotes I collected from various sources.",
-        text => "shlomif-fav",
-        title => "Other Favourite Quotes",
-    },
-    {
-        id => "friends",
-        desc => "Quotes from the scripts of the T.V. show Friends. They cover the first three seasons.",
-        text => "friends",
-        title => "Excerpts from the T.V. Show Friends",
-    },
-    {
-        id => "joel-on-software",
-        desc => qq{Quotes from the <a href="http://www.joelonsoftware.com/">Joel on Software</a> site.},
-        text => "joel-on-software",
-        title => "Quotes from the site “Joel on Software”",
-    },
-    {
-        id => "nyh-sigs",
-        desc => <<"EOF",
-a collection of quotes found in the
-signatures of <a href="http://nadav.harel.org.il/">Nadav Har’El</a>,
-as collected using an automated script.
-EOF
-        text => "nyh-sigs",
-        title => "Nadav Har’El’s Signatures",
-    },
-    {
-        id => "osp_rules",
-        desc => <<"EOF",
-“The Rules of Open-Source Programming”.
-Also check a <a href="http://www.advogato.org/article/395.html">discussion of it</a> on
-<a href="http://www.advogato.org/">Advogato</a>.
-EOF
-        text => "osp_rules",
-        title => "Rules of Open Source Programming",
-    },
-    {
-        id => "paul-graham",
-        desc => <<"EOF",
-Quotes from the essays and writings
-of <a href="http://www.paulgraham.com/">Paul Graham</a>.
-EOF
-        text => "paul-graham",
-        title => "Paul Graham Quotes",
-    },
-    {
-        id => "subversion",
-        desc => <<"EOF",
-Excerpts from the online
-<a href="http://subversion.tigris.org/">Subversion</a> folklore.
-EOF
-        text => "subversion",
-        title => "Quotes from the Online Folklore of the Subversion Version Control System",
-    },
-    {
-        id => "tinic",
-        desc => <<"EOF",
-The ultimate collection of reasons why
-“there is no IGLU cabal”, and other Israeli Linux on-line folklore.
-EOF
-        text => "tinic",
-        title => "Quotes from the online Linux-IL Folklore",
-    },
-    {
-        id => "sharp-perl",
-        desc => "A collection of conversations from Freenode’s #perl .",
-        text => "#perl",
-        title => "Quotes from the Freenode #perl channel",
-    },
-    {
-        id => "sharp-programming",
-        desc => "A collection of conversations from Freenode’s ##programming .",
-        text => "##programming",
-        title => "Quotes from the Freenode ##programming channel",
-    },
-    {
-        id => "shlomif-factoids",
-        desc => "A collection of factoids about people and things (e.g: Chuck Norris).",
-        text => "shlomif-factoids",
-        title => "Funny Factoids about People and Things (Chuck Norris, etc.)",
-    },
-)
+    map { _init_fortune($_) } @$orig_fortunes_records,
 );
 
 sub get_fortune_records

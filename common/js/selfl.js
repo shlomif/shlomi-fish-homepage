40884e79007f851e3eb848b1d9663a1785e9d4ba
Shlomi Fish <shlomif@shlomifish.org>
2013-05-08 17:52:27 +0300

    Extract to a dedicated JS file.

--- common/js/selfl.js
+++ common/js/selfl.js
@@ -10,12 +10,4 @@ function add_self_links()
 
 $(function() {
     add_self_links();
-    $(".sub_menu ul.nav_links li a img").mouseover(function() {
-            var src = $(this).attr("src").replace(/\.svg$/, "-pressed.svg");
-            $(this).attr("src", src);
-        })
-        .mouseout(function() {
-            var src = $(this).attr("src").replace("-pressed.svg", ".svg");
-            $(this).attr("src", src);
-        });
 });

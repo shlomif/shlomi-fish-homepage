c9a3198f813999a5d41b28b10542016105be57cf
Shlomi Fish <shlomif@shlomifish.org>
2013-05-08 19:48:52 +0300

    Corrected the file.

--- bin/Run-YUI-Compressor.bash
+++ bin/Run-YUI-Compressor.bash
@@ -1,2 +1,2 @@
 #!/bin/bash
-perl -d bin/Run-YUI-Compressor.pl -o c.js common/js/jq.js common/js/jquery.treeview.min.js common/js/tree.jquery.js common/js/jquery.cookie.js common/js/to-jqtree.js common/js/to-jqtree-2.js common/js/print-ver.js common/js/selfl.js common/js/sub_menu.js
+perl bin/Run-YUI-Compressor.pl -o c.js common/js/jq.js common/js/jquery.treeview.min.js common/js/tree.jquery.js common/js/jquery.cookie.js common/js/to-jqtree.js common/js/to-jqtree-2.js common/js/print-ver.js common/js/selfl.js common/js/sub_menu.js

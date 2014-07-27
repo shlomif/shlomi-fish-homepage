d2003ab06cd6b0af78003a6573adf5df990349a7
Shlomi Fish <shlomif@shlomifish.org>
2014-07-27 05:35:12 +0300

    Add a script to indent a Screenplay-XML.

---
+++ bin/indent-screenplay-xml.pl
@@ -0,0 +1,14 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+my $re = qr#(?:saying|para|description|inlinedesc)#;
+
+while (my $l = <>)
+{
+    print ($l =~ s#(<${re}[^>]*>)#\n$1\n#gr
+              =~ s#(</${re}>)#\n$1\n#gr
+              =~ s#[ \t]+$##gmsr
+          );
+}

edb98519a98996ac6484d7b8d6d10a91a923be3c
Shlomi Fish <shlomif@shlomifish.org>
2014-08-22 22:54:27 +0300

    Add the first test.

--- lib/Shlomif/Homepage/NavBlocks.pm
+++ lib/Shlomif/Homepage/NavBlocks.pm
@@ -27,5 +27,49 @@ has [qw(
     url
 )] => (is => 'ro', isa => 'Str', required => 1);
 
+package Shlomif::Homepage::NavBlocks::Renderer;
+
+use strict;
+use warnings;
+
+use utf8;
+
+use MooX (qw( late ));
+
+use CGI ();
+
+has 'host' => (is => 'ro', isa => 'Str', required => 1);
+
+has [qw(
+    nav_menu
+)] => (is => 'ro', required => 1);
+
+sub render
+{
+    my ($self, $thingy) = @_;
+
+    if ($thingy->isa('Shlomif::Homepage::NavBlocks::LocalLink'))
+    {
+        return sprintf(q#<li><p><a href="%s">%s</a></p></li>#,
+            CGI::escapeHTML(
+                $self->nav_menu->get_cross_host_rel_url_ref(
+                    {
+                        host => $self->host,
+                        host_url => $thingy->path,
+                        url_type => 'rel',
+                        url_is_abs => 0,
+                    },
+                ),
+            ),
+            $thingy->inner_html(),
+        );
+    }
+    else
+    {
+        Carp::confess('unimplemented');
+    }
+}
+
 1;
+
 

cca733a3c580f8dd399ee53a64928981414c7b22
Shlomi Fish <shlomif@shlomifish.org>
2014-08-24 18:09:35 +0300

    Allow to generate the fortune cookies' EPUB.

--- bin/gen-forts-all-in-one-page.pl
+++ bin/gen-forts-all-in-one-page.pl
@@ -3,6 +3,8 @@
 use strict;
 use warnings;
 
+use utf8;
+
 use Shlomif::Homepage::FortuneCollections;
 
 binmode STDOUT, ':utf8';
@@ -12,5 +14,7 @@ my $filename = shift(@ARGV);
 Shlomif::Homepage::FortuneCollections->write_fortune_all_in_one_page_to_file(
     $filename
 );
+
+Shlomif::Homepage::FortuneCollections->write_epub_json('lib/fortunes/xhtmls/book.json');
 
 1;

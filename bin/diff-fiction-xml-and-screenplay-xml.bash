675aa1caf54b57d60ff7c57e6ff15370bcfe9af5
Shlomi Fish <shlomif@shlomifish.org>
2014-09-07 05:41:10 +0300

    Handle the dates better.

--- bin/diff-fiction-xml-and-screenplay-xml.bash
+++ bin/diff-fiction-xml-and-screenplay-xml.bash
@@ -15,7 +15,8 @@ ls -d lib/fiction-xml/from-vcs/* lib/screenplay-xml/from-vcs/* |
         (
             echo "==== Diff for $repo ===="
             cd "$repo"
-            git diff updated-"$start_date"..updated-"$end_date" 2>&1 | cat
+            # git diff updated-"$start_date"..updated-"$end_date" 2>&1 | cat
+            git diff master@'{'"$start_date"\ 03:00:00'}'..master@'{'"$end_date"\ 03:00:00'}' 2>&1 | cat
         )
     done
     )

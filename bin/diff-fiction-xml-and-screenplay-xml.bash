a185fb3d19254caad00b18d66f4650bfc1cbce99
Shlomi Fish <shlomif@shlomifish.org>
2014-09-07 14:36:09 +0300

    Fix.

--- bin/diff-fiction-xml-and-screenplay-xml.bash
+++ bin/diff-fiction-xml-and-screenplay-xml.bash
@@ -9,6 +9,13 @@ shift
 end_date="$1"
 shift
 
+function _get_date()
+{
+    local d="$1"
+    shift
+    git show $(git log --format='%H' --since="$d" | tail -1) | head -1 | perl -lapE 's/^commit //'
+}
+
 ls -d lib/fiction-xml/from-vcs/* lib/screenplay-xml/from-vcs/* |
     grep -v PLACE |
     (while read repo ; do
@@ -16,7 +23,8 @@ ls -d lib/fiction-xml/from-vcs/* lib/screenplay-xml/from-vcs/* |
             echo "==== Diff for $repo ===="
             cd "$repo"
             # git diff updated-"$start_date"..updated-"$end_date" 2>&1 | cat
-            git diff master@'{'"$start_date"\ 03:00:00'}'..master@'{'"$end_date"\ 03:00:00'}' 2>&1 | cat
+            # git diff master@'{'"$start_date"\ 03:00:00'}'..master@'{'"$end_date"\ 03:00:00'}' 2>&1 | cat
+            git diff "$(_get_date "$start_date")".."$(_get_date "$end_date")" 2>&1 | cat
         )
     done
     )

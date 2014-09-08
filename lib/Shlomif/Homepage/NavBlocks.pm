4bf4ba10c8f1bec2eda9ef019ca7e12e05401d91
Shlomi Fish <shlomif@shlomifish.org>
2014-09-08 15:34:49 +0300

    Add the Thingy  thing.

--- lib/Shlomif/Homepage/NavBlocks.pm
+++ lib/Shlomif/Homepage/NavBlocks.pm
@@ -1,28 +1,3 @@
-package Shlomif::Homepage::NavBlocks::Thingy;
-
-use strict;
-use warnings;
-
-use MooX (qw( late ));
-
-has 'cache' => (is => 'rw');
-has 'is_cached' => (is => 'rw', isa => 'Bool', default => 0);
-
-sub cached_render
-{
-    my ($self, $promise_cb) = @_;
-
-    if (! $self->is_cached)
-    {
-        $self->cache(
-            scalar ( $promise_cb->() ),
-        );
-        $self->is_cached(1);
-    }
-
-    return $self->cache;
-}
-
 package Shlomif::Homepage::NavBlocks::LocalLink;
 
 use strict;

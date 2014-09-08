742ed4ddc8796e6895f52a5a71e7ebc76c1616b5
Shlomi Fish <shlomif@shlomifish.org>
2014-09-08 17:15:15 +0300

    Extract more .pm-s.

---
+++ lib/Shlomif/Homepage/NavBlocks/Renderer.pm
@@ -0,0 +1,41 @@
+package Shlomif::Homepage::NavBlocks::Renderer;
+
+use strict;
+use warnings;
+
+use utf8;
+
+use Carp ();
+
+use MooX (qw( late ));
+
+use CGI ();
+
+extends ('Exporter');
+
+has 'host' => (is => 'ro', isa => 'Str', required => 1);
+
+has [qw(
+    nav_menu
+)] => (is => 'ro', required => 1);
+
+sub render
+{
+    my ($self, $thingy) = @_;
+
+    return $thingy->cached_render(
+        sub {
+            return $self->_non_cached_render($thingy);
+        }
+    );
+}
+
+sub _non_cached_render
+{
+    my ($self, $thingy) = @_;
+
+    return $thingy->render($self);
+}
+
+1;
+
---
+++ lib/Shlomif/Homepage/NavBlocks/Subdiv_Tr.pm
@@ -0,0 +1,12 @@
+package Shlomif::Homepage::NavBlocks::Subdiv_Tr;
+
+use strict;
+use warnings;
+
+use MooX (qw( late ));
+
+extends ('Shlomif::Homepage::NavBlocks::Title_Tr');
+
+has 'css_class' => (is => 'ro', isa => 'Str', default => 'subdiv');
+
+1;

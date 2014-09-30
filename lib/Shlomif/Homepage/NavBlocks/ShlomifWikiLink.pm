0c8aa5ce50fc13fd7bb06120878fa9a2a57cedfa
Shlomi Fish <shlomif@shlomifish.org>
2014-09-30 13:00:10 +0300

    Add more to the politics navblock.

---
+++ lib/Shlomif/Homepage/NavBlocks/ShlomifWikiLink.pm
@@ -0,0 +1,25 @@
+package Shlomif::Homepage::NavBlocks::ShlomifWikiLink;
+
+use strict;
+use warnings;
+
+use MooX (qw( late ));
+
+use HTML::Widgets::NavMenu::EscapeHtml qw(escape_html);
+
+extends ('Shlomif::Homepage::NavBlocks::ExternalLink');
+
+sub render
+{
+    my ($self, $r) = @_;
+
+    return sprintf(q#<li><p><a class="ext wiki shlomif" href="%s">%s</a></p></li>#,
+        escape_html(
+            $self->url,
+        ),
+        'Shlomif Wiki Page',
+    );
+}
+
+1;
+

11525427170d0f140e12e5d9d7724f13787dc88c
Shlomi Fish <shlomif@shlomifish.org>
2014-10-21 17:30:21 +0300

    Add the toggler API for showing/hiding stuff.

--- common/js/toggle_sect.js
+++ common/js/toggle_sect.js
@@ -1,25 +1,16 @@
+"use strict";
+
 var toggle_sect_key = "shlomifish.org_section_navigation_menu_shown";
 
-var has_ls = (typeof localStorage !== "undefined" && localStorage !== null);
-
-function toggle_sect_menu() {
-    var elem = $("#toggle_sect_menu");
-
-    if (elem.hasClass("off")) {
-        elem.text("Hide");
-        if (has_ls) {
-            localStorage.setItem(toggle_sect_key, "1");
-        }
-    }
-    else {
-        elem.text("Show");
-        if (has_ls) {
-            localStorage.removeItem(toggle_sect_key);
-        }
-    }
-    $("#sect_menu_wrapper").toggleClass("novis");
-    elem.toggleClass("off");
-    elem.toggleClass("on");
+function build_sect_nav_menu_toggler() {
+    build_toggler({
+        ls_key: toggle_sect_key,
+        toggler_selector: '#toggle_sect_menu',
+        toggled_selector: '#sect_menu_wrapper',
+        toggled_class: 'novis',
+        hide_text: 'Hide',
+        show_text: 'Show',
+    });
 }
 
 $(document).ready(function(){
@@ -29,12 +20,5 @@ $(document).ready(function(){
 		unique: false
 	});
 
-    if (has_ls) {
-        var in_storage = localStorage.getItem(toggle_sect_key);
-        var in_elem = $("#toggle_sect_menu").hasClass("on");
-
-        if ((in_storage && (!in_elem)) || ((!in_storage) && in_elem)) {
-            toggle_sect_menu();
-        }
-    }
+    build_sect_nav_menu_toggler();
 });

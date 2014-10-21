4a7d4bd02bf68e2b5c6e4458b621434682f88ad5
Shlomi Fish <shlomif@shlomifish.org>
2014-10-21 18:04:55 +0300

    Main navbar toggling.

--- common/js/toggle_sect.js
+++ common/js/toggle_sect.js
@@ -1,6 +1,7 @@
 "use strict";
 
 var toggle_sect_key = "shlomifish.org_section_navigation_menu_shown";
+var toggle_main_key = "shlomifish.org_main_navigation_menu_shown";
 
 function build_sect_nav_menu_toggler() {
     build_toggler({
@@ -13,6 +14,17 @@ function build_sect_nav_menu_toggler() {
     });
 }
 
+function build_main_nav_menu_toggler() {
+    build_toggler({
+        ls_key: toggle_main_key,
+        toggler_selector: '#show_navbar',
+        toggled_selector: '#navbar',
+        toggled_class: 'novis',
+        hide_text: 'Hide NavBar',
+        show_text: 'Show NavBar',
+    });
+}
+
 $(document).ready(function(){
 	$("#sect_menu_wrapper > ul").treeview({
 		persist: "location",
@@ -21,4 +33,5 @@ $(document).ready(function(){
 	});
 
     build_sect_nav_menu_toggler();
+    build_main_nav_menu_toggler();
 });

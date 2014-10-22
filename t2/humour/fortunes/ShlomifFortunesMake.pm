b9e51ea9f80306e372b6146d4dfb75a9aa4d4a75
Shlomi Fish <shlomif@shlomifish.org>
2014-10-22 15:15:41 +0300

    got rid of the jobserver -j1 warning.
    Get rid fo cd dir && make warnings by converting to a Perl-based API.

---
+++ t2/humour/fortunes/ShlomifFortunesMake.pm
@@ -0,0 +1,47 @@
+package ShlomifFortunesMake;
+
+use strict;
+use warnings;
+
+my $VERSION_FILE = 'ver.txt';
+
+sub version_file
+{
+    return $VERSION_FILE;
+}
+
+sub ver
+{
+    my ($class) = @_;
+
+    my $fn = $class->version_file;
+
+    my $ret = `head -1 "$fn"`;
+    chomp($ret);
+
+    return $ret;
+}
+
+sub dist_base
+{
+    my ($class) = @_;
+
+    return 'fortunes-shlomif';
+}
+
+sub dist_dir
+{
+    my ($class) = @_;
+
+    return $class->dist_base . '-' . $class->ver;
+}
+
+sub package_base
+{
+    my ($class) = @_;
+
+    return $class->dist_dir . '.tar.gz';
+}
+
+1;
+

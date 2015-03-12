254de6c3d26daf2e068fe830dbd228e56ebd388d
Shlomi Fish <shlomif@shlomifish.org>
2015-03-13 00:03:55 +0200

    Add the link-checker script.
    This uses https://metacpan.org/release/WWW-LinkChecker-Internal .

---
+++ bin/link-checker-for-shlomifish.org.bash
@@ -0,0 +1,13 @@
+perl -Ilib scripts/link-checker --base='http://localhost/sites/hp/shlomif/' \
+    --before-insert-skip='\.(?:js|epub|zip|diff|pl|xsl|xslt|txt|raw\.html)\z' \
+    --before-insert-skip='art/by-others/(?:BertycoX/BertycoX-dirs|Yachar/Yachar-dirs)/\z' \
+    --before-insert-skip='/lm-solve/\z' \
+    --before-insert-skip='/me/blogs/agg/[\w\-]*\.xml\z' \
+    --before-insert-skip='/lecture/WebMetaLecture/.*\.wml\z' \
+    --before-insert-skip='/lecture/WebMetaLecture/slides/examples/frames/' \
+    --before-insert-skip='/js/MathJax/' \
+    --before-insert-skip='\.htaccess\z' \
+    --before-insert-skip='/humour/fortunes/show\.cgi\z' \
+    --before-insert-skip='/humour/fortunes/show-cgi\.txt\z' \
+    --before-insert-skip='/humour/fortunes/(?:bbt|friends|joel-on-software|nyh-sigs|osp_rules|paul-graham|sharp-perl|sharp-programming|shlomif|shlomif-factoids|shlomif-fav|subversion|tinic)\z' \
+    ;

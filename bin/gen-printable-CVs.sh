86513b4f2f5c1adfc55db333f14819d86152a3bb
Shlomi Fish <shlomif@shlomifish.org>
2015-04-23 15:51:34 +0300

    Add the new CV with more recent items first.

--- bin/gen-printable-CVs.sh
+++ bin/gen-printable-CVs.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-render()
+render_i_o()
 {
   filename="$1"
+  shift
+  out_filename="$1"
   shift
   (cd t2 && wml --passoption=2,-X3074 --passoption=3,-I../lib/ \
     -I../lib/ -DROOT~. -DLATEMP_SERVER=t2 -DLATEMP_FILENAME="$filename" \
@@ -9,9 +11,15 @@ render()
     -DLATEMP_THEME=better-scm \
     -I $HOME/conf/wml/Latemp/lib \
     -I $HOME/apps/wml \
-    "$filename"
     ) | grep -vP '^<\?xml ver' \
-        > printable/"$filename"
+        > printable/"$out_filename"
 }
+
+render()
+{
+    render_i_o "$1" "$1"
+}
+
 render "SFresume_detailed.html"
 render "SFresume.html"
+render_i_o "me/resumes/Resume-Recent.html" "SFresume-Recent.html"

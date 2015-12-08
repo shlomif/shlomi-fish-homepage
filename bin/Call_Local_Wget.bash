a1b77cded509a0150dab27fb9315f719f4b2e47c
Shlomi Fish <shlomif@shlomifish.org>
2015-12-08 20:41:35 +0200

    Add the Call local wget script for reference

---
+++ bin/Call_Local_Wget.bash
@@ -0,0 +1,4 @@
+#!/bin/bash
+# wget --mirror -np 'http://localhost/shlomif/homepage-local'
+# wget --mirror 'http://localhost/shlomif/homepage-local'
+wget --mirror --convert-links 'http://localhost/shlomif/homepage-local'

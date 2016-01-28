73b9d157753a940c17a5bf536680300968537759
Shlomi Fish <shlomif@shlomifish.org>
2016-01-28 13:49:36 +0200

    Made a parameter customisable.

--- lib/Shlomif/Spelling/Check.pm
+++ lib/Shlomif/Spelling/Check.pm
@@ -13,11 +13,25 @@ sub spell_check
 {
     my ($self, $args) = @_;
 
+    my $speller = Text::Hunspell->new(
+        '/usr/share/hunspell/en_GB.aff',
+        '/usr/share/hunspell/en_GB.dic',
+    );
+
+    if (not $speller)
+    {
+        die "Could not initialize speller!";
+    }
+
     my $files = $args->{files};
 
     return HTML::Spelling::Site::Checker->new(
         {
             whitelist_fn => 'lib/hunspell/whitelist1.txt',
+            check_word_cb => sub {
+                my ($word) = @_;
+                return $speller->check($word);
+            },
         }
     )->spell_check(
         {

cc9c09c08894bc6cea96834a4bec6ae7469e5035
Shlomi Fish <shlomif@shlomifish.org>
2016-03-22 12:30:46 +0200

    Convert to the new spell-checking interface.

--- lib/Shlomif/Spelling/Check.pm
+++ lib/Shlomif/Spelling/Check.pm
@@ -11,32 +11,36 @@ use Text::Hunspell;
 use Shlomif::Spelling::Whitelist;
 use HTML::Spelling::Site::Checker;
 
+has obj => (is => 'ro', default => sub {
+        my ($self) = @_;
+        my $speller = Text::Hunspell->new(
+            '/usr/share/hunspell/en_GB.aff',
+            '/usr/share/hunspell/en_GB.dic',
+        );
+
+        if (not $speller)
+        {
+            die "Could not initialize speller!";
+        }
+
+        return HTML::Spelling::Site::Checker->new(
+            {
+                timestamp_cache_fn => './Tests/data/cache/spelling-timestamp.json',
+                whitelist_parser => scalar( Shlomif::Spelling::Whitelist->new() ),
+                check_word_cb => sub {
+                    my ($word) = @_;
+                    return $speller->check($word);
+                },
+            }
+        )
+    }
+);
+
 sub spell_check
 {
     my ($self, $args) = @_;
 
-    my $speller = Text::Hunspell->new(
-        '/usr/share/hunspell/en_GB.aff',
-        '/usr/share/hunspell/en_GB.dic',
-    );
-
-    if (not $speller)
-    {
-        die "Could not initialize speller!";
-    }
-
-    my $files = $args->{files};
-
-    return HTML::Spelling::Site::Checker->new(
-        {
-            timestamp_cache_fn => './Tests/data/cache/spelling-timestamp.json',
-            whitelist_parser => scalar( Shlomif::Spelling::Whitelist->new() ),
-            check_word_cb => sub {
-                my ($word) = @_;
-                return $speller->check($word);
-            },
-        }
-    )->spell_check(
+    return $self->obj->spell_check(
         {
             files => $args->{files}
         }

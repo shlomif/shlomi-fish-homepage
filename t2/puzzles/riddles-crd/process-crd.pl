a440894acdf85f106900bc44aaea195873809c03
Shlomi Fish <shlomif@shlomifish.org>
2016-04-02 14:09:43 +0300

    trailing space

--- t2/puzzles/riddles-crd/process-crd.pl
+++ t2/puzzles/riddles-crd/process-crd.pl
@@ -5,6 +5,11 @@ use warnings;
 
 use File::Format::CRD::Reader;
 
+sub _remove_trail_ws
+{
+    return shift =~ s/[ \t]*(\r?)$//gmrs;
+}
+
 my $filename = shift;
 
 my $reader = File::Format::CRD::Reader->new({ filename => $filename });
@@ -13,7 +18,7 @@ binmode STDOUT, ":encoding(utf-8)";
 
 while (my $card = $reader->get_next_card({encoding => "windows-1255"}))
 {
-    print "Title = " , $card->{'title'}, "\nBody = <<<\n",
-        $card->{'body'}, "\n>>>\n\n";
+    print "Title = " , _remove_trail_ws($card->{'title'}), "\nBody = <<<\n",
+        _remove_trail_ws($card->{'body'}), "\n>>>\n\n";
 }
 

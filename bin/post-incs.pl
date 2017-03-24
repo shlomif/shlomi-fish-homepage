8011afa4c3a68b00aa2a3cf5bfc77cab8d609035
Shlomi Fish <shlomif@shlomifish.org>
2017-03-24 19:40:43 +0300

    Make post-incs.pl accept several filenames.

--- bin/post-incs.pl
+++ bin/post-incs.pl
@@ -4,35 +4,7 @@ use strict;
 use warnings;
 
 use IO::All qw/ io /;
-
-my ($fn) = @ARGV;
-
-sub _f
-{
-    return io->file($fn)->utf8;
-}
-
-my $text = _f()->all;
-
-# In case there's an error - fail and need to rebuild.
-_f()->unlink();
-
-$text =~ s#^\({5}include "([^"]+)"\){5}\n#io->file("lib/$1")->utf8->all#egms;
-$text =~
-s#\({5}chomp_inc='([^']+)'\){5}#io->file("lib/$1")->chomp->utf8->getline("\n")#egms;
-
-if ( $ENV{F} )
-{
-    foreach my $class (qw(info irc-conversation))
-    {
-        my $table_from = qq{<table class="$class">};
-        my $table_to   = qq{<table class="$class" summary="">};
-
-        $text =~ s#\Q$table_from\E#$table_to#g;
-    }
-}
-
-use HTML::Tidy;
+use HTML::Tidy ();
 
 my $tidy = HTML::Tidy->new(
     {
@@ -42,14 +14,42 @@ my $tidy = HTML::Tidy->new(
     }
 );
 
-$text = $tidy->clean($text);
-$text =~ s# +$##gms;
-$text =~ s#(</div>|</li>|</html>)\n\n#$1\n#g;
+foreach my $fn (@ARGV)
+{
+    my $_f = sub {
+        return io->file($fn)->utf8;
+    };
 
-# Remove surrounding space of tags.
-$text =~ s#\s*(</?(?:body|(?:br /)|div|head|li|ol|p|title|ul)>)\s*#$1#gms;
+    my $text = $_f->()->all;
 
-# Remove document trailing space.
-$text =~ s#\s+\z##ms;
+    # In case there's an error - fail and need to rebuild.
+    $_f->()->unlink();
 
-_f()->print($text);
+    $text =~
+        s#^\({5}include "([^"]+)"\){5}\n#io->file("lib/$1")->utf8->all#egms;
+    $text =~
+s#\({5}chomp_inc='([^']+)'\){5}#io->file("lib/$1")->chomp->utf8->getline("\n")#egms;
+
+    if ( $ENV{F} )
+    {
+        foreach my $class (qw(info irc-conversation))
+        {
+            my $table_from = qq{<table class="$class">};
+            my $table_to   = qq{<table class="$class" summary="">};
+
+            $text =~ s#\Q$table_from\E#$table_to#g;
+        }
+    }
+
+    $text = $tidy->clean($text);
+    $text =~ s# +$##gms;
+    $text =~ s#(</div>|</li>|</html>)\n\n#$1\n#g;
+
+    # Remove surrounding space of tags.
+    $text =~ s#\s*(</?(?:body|(?:br /)|div|head|li|ol|p|title|ul)>)\s*#$1#gms;
+
+    # Remove document trailing space.
+    $text =~ s#\s+\z##ms;
+
+    $_f->()->print($text);
+}

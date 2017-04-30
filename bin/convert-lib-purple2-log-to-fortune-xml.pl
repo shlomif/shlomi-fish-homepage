37d8dd91fdb9e428a8992fb6920c9be5a5beb9c8
Shlomi Fish <shlomif@shlomifish.org>
2017-04-30 12:21:50 +0300

    Add the new mongodb-vs-null fortune.

---
+++ bin/convert-lib-purple2-log-to-fortune-xml.pl
@@ -0,0 +1,12 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+if ( my ( $who, $text ) =
+m{\A<font color="[^"]*"><font size="2">[^<]*</font> <b>([^<]+)</b></font> (.*)<br/>\z}
+    )
+{
+    $text =~ s#<a href="([^"]+)">\1</a>#$1#g;
+    print qq#<saying who="$who">$text</saying>#;
+}

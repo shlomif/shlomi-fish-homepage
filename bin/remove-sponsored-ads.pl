806011e1143d26df636bcc5b22201cdcfd821d24
Shlomi Fish <shlomif@shlomifish.org>
2017-05-23 16:01:45 +0300

    Cleanups

--- bin/remove-sponsored-ads.pl
+++ bin/remove-sponsored-ads.pl
@@ -5,15 +5,15 @@ use warnings;
 
 use Path::Tiny qw/ path /;
 
-my $date = "2008-10-20";
+my $date = '2008-10-20';
 
 my $remove_at = qq{remove_at="$date"};
 
-foreach my $file (`ack -l '$remove_at' t2`)
+foreach my $fn (`ack -l '$remove_at' t2`)
 {
-    chomp($file);
-    print $file, "\n";
-    path($file)->edit_utf8(
+    chomp($fn);
+    print $fn, "\n";
+    path($fn)->edit_utf8(
         sub {
 s{(?:^;;;[^\n]*\n)*<shlomif_sponsored_ad[^\n]*\Q$remove_at\E.*?</shlomif_sponsored_ad>[^\n]*\n?}{}ms;
         }

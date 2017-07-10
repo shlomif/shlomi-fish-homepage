2dd07e1b7955013dec232424f6103b453414134a
Shlomi Fish <shlomif@shlomifish.org>
2017-07-10 14:48:17 +0300

    Try to build quad-pres from its repo.

--- bin/install-git-cmakey-program-system-wide.bash
+++ bin/install-git-cmakey-program-system-wide.bash
@@ -2,13 +2,15 @@
 
 set -x
 
+vcs_tool="$1"
+shift
 d="$1"
 shift
 url="$1"
 shift
 
 clone_dir='mygit'
-git clone "$url" "$clone_dir"
+"$vcstool" clone "$url" "$clone_dir"
 (cd "$clone_dir/$d"
 
 ( a="$(pwd)"; mkdir B2 && cd B2 && hg clone https://bitbucket.org/shlomif/shlomif-cmake-modules && cd shlomif-cmake-modules/shlomif-cmake-modules && cp -f "$(pwd)"/Shlomif_Common.cmake "$a"/cmake/ )

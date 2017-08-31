edfe7dd2f1e9bb8f9d65dc7717b9aae3ae02c42a
Shlomi Fish <shlomif@shlomifish.org>
2017-08-31 22:14:39 +0300

    Made eslint a little happier.

--- common/js/moz-lang.js
+++ common/js/moz-lang.js
@@ -1,10 +1,9 @@
 function lang_load() {
 $('*').each(
-    function(i){
-        var l = $(this)[0].getAttribute("xml:lang");
-        if (l)
-        {
-            $(this)[0].setAttribute("lang",l);
+    function(i) {
+        var l = $(this)[0].getAttribute('xml:lang');
+        if (l) {
+            $(this)[0].setAttribute('lang', l);
         }
     }
 );

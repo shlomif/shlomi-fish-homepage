ea91bbcc146ce1b89057fa07f787cef261208ab4
Shlomi Fish <shlomif@shlomifish.org>
2017-12-04 02:23:47 +0200

    Try to fix build.

--- bin/install-tidyp-systemwide.bash
+++ bin/install-tidyp-systemwide.bash
@@ -5,7 +5,7 @@ set -x
 bdir="$HOME/tidyp-build"
 mkdir -p "$bdir"
 cd "$bdir"
-wget https://cloud.github.com/downloads/petdance/tidyp/tidyp-1.04.tar.gz
+wget https://github.com/downloads/petdance/tidyp/tidyp-1.04.tar.gz
 tar -xf tidyp-1.04.tar.gz
 cd tidyp-1.04
 ./configure && make && sudo make install && sudo ldconfig

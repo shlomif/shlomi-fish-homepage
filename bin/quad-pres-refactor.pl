4aa1d1bbfa461a67806724f01eff0417d0b89bf9
Shlomi Fish <shlomif@shlomifish.org>
2017-12-28 22:16:03 +0200

    Add a script.

---
+++ bin/quad-pres-refactor.pl
@@ -0,0 +1,10 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+s%
+<quadpres_code_block> [\s\n]*
+<quadpres_include_colorized_file \s+ filename="([^"]+)" \s* /\s*> [\s\n]*
+</quadpres_code_block>
+%<quadpres_code_file filename="$1" />%gmsx;

699aa6e8b13ac3ce4ac03a28c595be797c8267dc
Shlomi Fish <shlomif@shlomifish.org>
2018-01-30 01:31:19 +0200

    More eslint.

--- common/js/print-ver.js
+++ common/js/print-ver.js
@@ -1,4 +1,7 @@
-$('#prnt_ver').html('<button id="prnt_ver_link"><img src="/images/printer_icon.png" alt="Printer Icon" /> Print Version</button>');
+$('#prnt_ver').html('<button id="prnt_ver_link">' +
+    '<img src="/images/printer_icon.png" alt="Printer Icon" /> ' +
+    'Print Version</button>');
 $('#prnt_ver_link').click(function() {
-    alert("The page is printable as it is. Just hit print preview and you'll be able to print it.");
+    alert('The page is printable as it is. Just hit print preview and ' +
+          'you\'ll be able to print it.');
 });
--- common/js/sub_menu.js
+++ common/js/sub_menu.js
@@ -1,10 +1,10 @@
 $(function() {
-    $(".sub_menu ul.nav_links li a img").mouseover(function() {
-            var src = $(this).attr("src").replace(/\.svg$/, "-pressed.svg");
-            $(this).attr("src", src);
+    $('.sub_menu ul.nav_links li a img').mouseover(function() {
+        let e = $(this);
+        e.attr('src', e.attr('src').replace(/\.svg$/, '-pressed.svg'));
         })
         .mouseout(function() {
-            var src = $(this).attr("src").replace("-pressed.svg", ".svg");
-            $(this).attr("src", src);
+            let e = $(this);
+            e.attr('src', e.attr('src').replace('-pressed.svg', '.svg'));
         });
 });

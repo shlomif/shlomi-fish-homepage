ad5ded30572d83e01b64cd1de4e5b54ba3318f26
Shlomi Fish <shlomif@shlomifish.org>
2018-02-01 21:44:07 +0200

    Convert to zenfilter.
    Eat your own dogfood and saving code.

--- bin/filter-make.pl
+++ bin/filter-make.pl
@@ -3,26 +3,7 @@
 use strict;
 use warnings;
 
-use IO::Handle;
-
-STDOUT->autoflush(1);
-my $NUM_LAST = 500;
-my @last;
-my $STATUS_EVERY = 200;
-my $count        = 0;
-while ( my $l = <STDIN> )
-{
-    push @last, $l;
-    if ( @last > $NUM_LAST )
-    {
-        shift @last;
-    }
-    if ( ( ( ++$count ) % $STATUS_EVERY ) == 0 )
-    {
-        print "Reached $count\n";
-    }
-}
-print @last;
+exec( 'zenfilter', '--count-step=200', '--last=500' );
 
 =head1 COPYRIGHT & LICENSE
 
@@ -30,26 +11,5 @@ Copyright 2016 by Shlomi Fish
 
 This program is distributed under the MIT (Expat) License:
 L<http://www.opensource.org/licenses/mit-license.php>
-
-Permission is hereby granted, free of charge, to any person
-obtaining a copy of this software and associated documentation
-files (the "Software"), to deal in the Software without
-restriction, including without limitation the rights to use,
-copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the
-Software is furnished to do so, subject to the following
-conditions:
-
-The above copyright notice and this permission notice shall be
-included in all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
-OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
-HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
-WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
-OTHER DEALINGS IN THE SOFTWARE.
 
 =cut

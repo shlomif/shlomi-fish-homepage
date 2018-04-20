0a465730c9ec119b5106e4b4609202690f01e480
Shlomi Fish <shlomif@shlomifish.org>
2018-04-20 23:28:21 +0300

    reformat and style

--- bin/gen-printable-CVs.sh
+++ bin/gen-printable-CVs.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 render_i_o()
 {
-  filename="$1"
-  shift
-  out_filename="$1"
-  shift
-  (cd t2 && wml --passoption=2,-X3074 --passoption=3,-I../lib/ \
-    -I../lib/ -DROOT~. -DLATEMP_SERVER=t2 -DLATEMP_FILENAME="$filename" \
-    -DPRINTABLE=1 "${filename}.wml" $(latemp-config --wml-flags) \
-    -DLATEMP_THEME=better-scm \
-    -I $HOME/conf/wml/Latemp/lib \
-    -I $HOME/apps/wml \
-    ) | grep -vP '^<\?xml ver' \
+    local filename="$1"
+    shift
+    local out_filename="$1"
+    shift
+
+    (cd t2 && wml --passoption=2,-X3074 --passoption=3,-I../lib/ \
+        -I../lib/ -DROOT~. -DLATEMP_SERVER=t2 -DLATEMP_FILENAME="$filename" \
+        -DPRINTABLE=1 "${filename}.wml" $(latemp-config --wml-flags) \
+        -DLATEMP_THEME=better-scm \
+        -I $HOME/conf/wml/Latemp/lib \
+        -I $HOME/apps/wml \
+        ) | grep -vP '^<\?xml ver' \
         > printable/"$out_filename"
 }
 

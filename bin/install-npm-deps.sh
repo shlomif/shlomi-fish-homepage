94ffe8c0e8e41f6fc1c615801ea565bdbba51bc8
Shlomi Fish <shlomif@shlomifish.org>
2018-05-01 13:10:14 +0300

    extract a shell program to install.

---
+++ bin/install-npm-deps.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+# install-npm-deps.sh
+# Copyright (C) 2018 shlomif <shlomif@cpan.org>
+#
+# Distributed under terms of the MIT license.
+#
+npm install \
+    camel-case \
+    html-minifier \
+    param-case \
+    uglify-es \
+

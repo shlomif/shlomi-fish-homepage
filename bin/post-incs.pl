9e63199d9733843714b0acb807e70e99526a7cab
Shlomi Fish <shlomif@shlomifish.org>
2018-05-03 17:51:25 +0300

    made post-incs more generic.

--- bin/post-incs.pl
+++ bin/post-incs.pl
@@ -78,29 +78,9 @@ system(
 
 if ( $ENV{APPLY_ADS} )
 {
-    my $PW_TEXT1 = <<'EOF' =~ s#\n\z##r;
-<!-- Project Wonderful Ad Box Loader -->
-<script type="text/javascript">
-(function(){function pw_load(){if(arguments.callee.z)return;else arguments.callee.z=true;var d=document;var s=d.createElement('script');var x=d.getElementsByTagName('script')[0];s.type='text/javascript';s.async=true;s.src='//www.projectwonderful.com/pwa.js';x.parentNode.insertBefore(s,x);}if (window.attachEvent){window.attachEvent('DOMContentLoaded',pw_load);window.attachEvent('onload',pw_load);}else{window.addEventListener('DOMContentLoaded',pw_load,false);window.addEventListener('load',pw_load,false);}})();
-</script>
-<!-- End Project Wonderful Ad Box Loader -->
-<!-- Project Wonderful Ad Box Code -->
-<div id="pw_adbox_42457_1_0"></div><script type="text/javascript"></script>
-<noscript><div><map id="admap42457"><area href="http://www.projectwonderful.com/out_nojs.php?r=0&amp;c=0&amp;id=42457&amp;type=1" shape="rect" coords="0,0,468,60" title="" alt="" /></map></div><table summary=""><tr><td><img src="http://www.projectwonderful.com/nojs.php?id=42457&amp;type=1" height="60" usemap="admap42457" alt="" /></td></tr><tr><td colspan="1"><div class="cen"><a style="font-size:10px;color:#0000ff;text-decoration:none;line-height:1.2;font-weight:bold;font-family:Tahoma, verdana,arial,helvetica,sans-serif;text-transform: none;letter-spacing:normal;text-shadow:none;white-space:normal;word-spacing:normal;" href="http://www.projectwonderful.com/advertisehere.php?id=42457&amp;type=1">Ads by Project Wonderful! Your ad here, right now: $0.02</a></div></td></tr><tr><td colspan="1" style="height:3px;font-size:1px;padding:0px;max-height:3px;vertical-align:top;"></td></tr></table>
-</noscript>
-<!-- End Project Wonderful Ad Box Code -->
-<script type="text/javascript" src="http://www.projectwonderful.com/ad_display.js"></script>
-<!-- End of Project Wonderful ad code. -->
-EOF
-
-    my $PW_TEXT2 = <<'EOF' =~ s#\n\z##r;
-<!-- Project Wonderful Ad Box Code -->
-<div id="pw_adbox_42545_3_0"></div><script type="text/javascript"></script>
-<noscript><div><map id="admap42545"><area href="http://www.projectwonderful.com/out_nojs.php?r=0&amp;c=0&amp;id=42545&amp;type=3" shape="rect" coords="0,0,160,600" title="" alt="" /></map></div><table summary=""><tr><td><img src="http://www.projectwonderful.com/nojs.php?id=42545&amp;type=3" width="160" height="600" usemap="admap42545" alt="" /></td></tr><tr><td colspan="1"><div class="cen"><a style="font-size:10px;color:#0000ff;text-decoration:none;line-height:1.2;font-weight:bold;font-family:Tahoma, verdana,arial,helvetica,sans-serif;text-transform: none;letter-spacing:normal;text-shadow:none;white-space:normal;word-spacing:normal;" href="http://www.projectwonderful.com/advertisehere.php?id=42545&amp;type=3">Ads by Project Wonderful! Your ad here, right now: $0.02</a></div></td></tr><tr><td colspan="1" style="height:3px;font-size:1px;padding:0px;max-height:3px;vertical-align:top;"></td></tr></table>
-</noscript>
-<!-- End Project Wonderful Ad Box Code -->
-EOF
-
+    my $dir = "lib/ads/";
+    my %TEXTS = ( map { $_ => path("$dir/texts/$_")->slurp_utf8 }
+            path("$dir/texts-lists.txt")->lines_utf8( { chomp => 1 } ) );
     foreach my $fn ( @filenames, @ad_filenames )
     {
         modify_on_change(
@@ -109,13 +89,9 @@ EOF
                 my $text_ref = shift;
                 my $r1 =
                     ( $$text_ref =~
-s%<div id="project_wonderful_top_proto">Placeholder</div>%\n$PW_TEXT1%ms
+s%<div id="([^"]+)">Placeholder</div>%"\n" . $TEXTS{$1}%egms
                     );
-                my $r2 =
-                    ( $$text_ref =~
-s%<div id="project_wonderful_code_side_proto">Placeholder</div>%\n$PW_TEXT2%ms
-                    );
-                return ( $r1 || $r2 );
+                return $r1;
             }
         );
     }

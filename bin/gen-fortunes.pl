a546bc97d6f92f509369c90e4e0dd45e35e983c2
Shlomi Fish <shlomif@shlomifish.org>
2018-09-02 16:58:39 +0300

    Refactoring / code cleanup.
    See:
    
    * https://en.wikipedia.org/wiki/Code_refactoring
    
    * https://www.refactoring.com/
    
    * https://www.joelonsoftware.com/2002/01/23/rub-a-dub-dub/
    
    Some small optimisations may have slipped in as well.

--- bin/gen-fortunes.pl
+++ bin/gen-fortunes.pl
@@ -5,8 +5,6 @@ use warnings;
 
 use Cwd qw(getcwd);
 
-# This is to in order to generate the t2/humour/fortunes/arcs-list.mak
-# file, which is inclduded by the makefile.
 {
     my $orig_dir = getcwd();
 
@@ -16,8 +14,6 @@ use Cwd qw(getcwd);
     {
         system( "make", "-s", "dist" );
     }
-
-    system( "make", "-s", "list_files" );
 
     chdir($orig_dir);
 }

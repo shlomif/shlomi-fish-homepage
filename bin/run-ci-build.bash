0d6e7faf7b6c1706d729cb73286197466cbb7403
Shlomi Fish <shlomif@shlomifish.org>
2018-09-04 14:49:32 +0300

    Add a .travis.yml file for Travis-CI.
    Based on https://github.com/shlomif/perl6-CI-Gen and
    https://github.com/shlomif/ci-gen-framework .
    
    See:
    
    * https://en.wikipedia.org/wiki/Travis_CI
    
    * https://en.wikipedia.org/wiki/Continuous_integration .

---
+++ bin/run-ci-build.bash
@@ -0,0 +1,11 @@
+#! /bin/bash
+#
+# run-ci-build.bash
+# Copyright (C) 2018 Shlomi Fish <shlomif@cpan.org>
+#
+# Distributed under terms of the MIT license.
+#
+set -e
+set -x
+touch t2/philosophy/SummerNSA/Letter-to-SGlau-2014-10/*.{html,pdf}
+bash -x bin/travis-ci-script.bash

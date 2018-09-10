4e6f5b6d2bb15e2829fc9f4c3c512263be4ba836
Shlomi Fish <shlomif@shlomifish.org>
2018-09-10 17:08:00 +0300

    convert more pages to (x)html5

--- lib/factoids/gen-html.pl
+++ lib/factoids/gen-html.pl
@@ -993,7 +993,7 @@ foreach my $page (@pages)
     my $vars = { p => $page, };
 
     my $tt_text = <<'END_OF_TEMPLATE';
-#include "template.wml"
+#include "template5.wml"
 
 #include "Inc/emma_watson.wml"
 #include "Inc/factoids_jqui_tabs_multi_lang.wml"

6ecbbc804a9425dcb0ec915053e5596de3ff6123
Shlomi Fish <shlomif@shlomifish.org>
2018-10-10 14:11:50 +0300

    Update appveyor / Travis-CI.
    Based on https://github.com/shlomif/perl6-CI-Gen and
    https://github.com/shlomif/ci-gen-framework .
    
    See:
    
    * https://en.wikipedia.org/wiki/Travis_CI
    
    * https://en.wikipedia.org/wiki/Continuous_integration .

--- .travis.bash
+++ .travis.bash
@@ -26,6 +26,33 @@ then
     sudo apt-get --no-install-recommends install -y ack-grep asciidoc build-essential cmake cpanminus dbtoepub docbook-defguide docbook-xsl docbook-xsl-ns fortune-mod hunspell inkscape myspell-en-gb libdb5.3-dev libgd-dev libhunspell-dev libncurses-dev libpcre3-dev libperl-dev libxml2-dev mercurial myspell-en-gb lynx optipng perl python3 python3-setuptools python3-pip silversearcher-ag tidy valgrind wml xsltproc xz-utils zip
     sudo dpkg-divert --local --divert /usr/bin/ack --rename --add /usr/bin/ack-grep
     cpanm local::lib
+    go get -u github.com/tdewolff/minify/cmd/minify
+    eval "$(perl -Mlocal::lib=$HOME/perl_modules)"
+    cpanm Alien::Tidyp App::Deps::Verify App::XML::DocBook::Builder Pod::Xhtml
+    cpanm --notest HTML::Tidy
+    cpanm HTML::T5
+    # For wml
+    cpanm --notest Bit::Vector Class::XSAccessor GD Getopt::Long IO::All Image::Size Term::ReadKey
+    # For quadp
+    cpanm --notest Class::XSAccessor Config::IniFiles HTML::Links::Localize
+    bash bin/install-git-cmakey-program-system-wide.bash 'git' 'src' 'https://github.com/thewml/website-meta-language.git'
+    bash bin/install-git-cmakey-program-system-wide.bash 'git' 'installer' 'https://github.com/thewml/latemp.git'
+    deps-app plinst -i bin/required-modules.yml
+    gem install asciidoctor compass compass-blueprint
+    sudo -H `which python3` -m pip install beautifulsoup4 bs4 cookiecutter lxml pycotap vnu_validator Zenfilter
+    ( cd .. && git clone https://github.com/thewml/wml-extended-apis.git && cd wml-extended-apis/xhtml/1.x && bash Install.bash )
+    ( cd .. && git clone https://github.com/thewml/latemp.git && cd latemp/support-headers && perl install.pl )
+    ( cd .. && git clone https://github.com/shlomif/wml-affiliations.git && cd wml-affiliations/wml && bash Install.bash )
+    bash -x bin/install-npm-deps.sh
+    bash -x bin/install-tidyp-systemwide.bash
+    bash bin/install-git-cmakey-program-system-wide.bash 'git' 'installer' 'https://github.com/shlomif/quad-pres'
+    echo '{"amazon_sak":"invalid"}' > "$HOME"/.shlomifish-amazon-sak.json
+    ( cd "$HOME" && git clone https://github.com/w3c/markup-validator.git )
+    pwd
+    echo "HOME=$HOME"
+    bash -x bin/install-npm-deps.sh
+    sudo ln -s /usr/bin/make /usr/bin/gmake
+
 
 elif test "$cmd" = "install"
 then

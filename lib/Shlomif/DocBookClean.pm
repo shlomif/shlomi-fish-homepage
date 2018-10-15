fd67b45a90970f22bc518c86bee5d6e99fbdcc83
Shlomi Fish <shlomif@shlomifish.org>
2018-10-16 01:02:15 +0300

    Fix markup / code validation.
    See https://www.shlomifish.org/philosophy/computers/web/validate-your-html/ .

--- lib/Shlomif/DocBookClean.pm
+++ lib/Shlomif/DocBookClean.pm
@@ -8,8 +8,6 @@ sub cleanup_docbook
     my ($str_ref) = @_;
 
     # It's a kludge
-    $$str_ref =~ s{ lang="(?:en)?"}{}g;
-    $$str_ref =~ s{ xml:lang="(?:en)?"}{}g;
     $$str_ref =~ s#<head profile="">#<head>#g;
     $$str_ref =~
 s# (?:xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"|xml:space="preserve")([ >]|/>)#$1#g;

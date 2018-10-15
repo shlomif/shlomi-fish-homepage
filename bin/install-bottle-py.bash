1a42e87c9f80b819ebe4e6966f2ecc1940efe152
Shlomi Fish <shlomif@shlomifish.org>
2018-10-15 16:02:54 +0300

    start writing the python port of show.cgi.

---
+++ bin/install-bottle-py.bash
@@ -0,0 +1,8 @@
+#! /bin/bash
+#
+# install-bottle-py.bash
+# Copyright (C) 2018 Shlomi Fish <shlomif@cpan.org>
+#
+# Distributed under terms of the MIT license.
+#
+wget -O t2/humour/fortunes/bottle.py https://bottlepy.org/bottle.py

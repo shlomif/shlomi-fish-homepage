629a92848d285ff270b304d24c08dd38d70ae6c5
Shlomi Fish <shlomif@shlomifish.org>
2018-10-21 21:38:09 +0300

    start implementing cache in vnu validate wraper.
    broken due to https://github.com/validator/validator/issues/722 .

--- Tests/validate-html-using-vnu.py
+++ Tests/validate-html-using-vnu.py
@@ -34,10 +34,126 @@ OTHER DEALINGS IN THE SOFTWARE.
 
 import re
 import unittest
-import vnu_validator
+
+import os
+from os.path import join
+import tempfile
+import hashlib
+import json
+from urllib.parse import urlparse
+from subprocess import Popen, PIPE
 
 
-class MyTests(vnu_validator.VnuTest):
+class VnuValidate:
+    """
+    Run the Nu HTML Validator on a directory tree of XHTML5 and HTML5 files.
+
+    :param path the path of the root directory
+    :param jar path to the Java .jar of the Nu validator
+    :param non_xhtml_cb A callback that accepts a path and returns whether it
+    is not XHTML
+    :param skip_cb Return whether a path should be skipped entirely
+    """
+    def __init__(self, path, jar, non_xhtml_cb, skip_cb):
+        self.path = path
+        self.jar = jar
+        self.non_xhtml_cb = non_xhtml_cb
+        self.skip_cb = skip_cb
+
+    def run(self):
+        """
+        :returns boolean for sucess or failure.
+        """
+        t = tempfile.TemporaryDirectory()
+        # t = VnuValidate(0, 0, 0, 0)
+        # t.name = "/tmp/zjzj"
+        whitelist = {'html': {}, 'xhtml': {}}
+        which = {}
+        for dirpath, _, fns in os.walk(self.path):
+            dn = join(t.name, dirpath)
+            os.makedirs(dn)
+            for fn in fns:
+                path = join(dirpath, fn)
+                if self.skip_cb(path):
+                    continue
+                html = re.match(r'.*\.html?$', fn)
+
+                def _content():
+                    return open(path, 'r').read()
+                out_fn = None
+                if re.match('.*\\.xhtml$', fn) or (
+                        html and not self.non_xhtml_cb(path)):
+                    out_fn = re.sub('\.[^\.]*$', '.xhtml', fn)
+                elif html:
+                    out_fn = fn
+
+                if out_fn:
+                    c = _content()
+                    m = hashlib.sha256()
+                    m.update(bytes(c, 'utf-8'))
+                    d = m.digest()
+                    key = 'html' if html else 'xhtml'
+                    if d not in whitelist[key]:
+                        fn = join(dn, out_fn)
+                        open(fn, 'w').write(c)
+                        which[fn] = key
+
+        cmd = ['java', '-jar', self.jar, '--format', 'json', '--Werror',
+               '--skip-non-html', t.name]
+        print(" ".join(cmd))
+        import sys
+        sys.exit(0)
+        with Popen(cmd, stderr=PIPE) as ret:
+            ret.wait()
+            text = ret.stderr.read()
+            # open('foo.json', 'w').write(text.decode('utf-8'))
+            data = json.loads(text)
+            blacklist = {'html': {}, 'xhtml': {}}
+            found = set()
+            for msg in data['messages']:
+                url = msg['url']
+                fn = urlparse(url).path
+                if fn not in found:
+                    found.append(fn)
+                    c = open(fn, 'r').read()
+                    m = hashlib.sha256()
+                    m.update(c)
+                    d = m.digest()
+                    blacklist[which[fn]][d] = True
+            for key in ['html', 'xhtml']:
+                for k in list(whitelist[key].keys()):
+                    if k not in blacklist[key]:
+                        del whitelist[key][k]
+            return len(blacklist['html']) + len(blacklist['xhtml']) == 0
+
+
+class VnuTest(unittest.TestCase):
+    """
+    One can find some examples for this here:
+
+    """
+    def vnu_test_dir(self, path, non_xhtml_cb, skip_cb):
+        """
+        A unit test helper for checking a directory tree.
+        :param path the path of the root directory
+        :param non_xhtml_cb A callback that accepts a path and returns whether
+        it
+        :param skip_cb Return whether a path should be skipped entirely
+
+        Uses an environment variable HTML_VALID_VNU_JAR that points to the
+        validator .jar (see https://github.com/validator/validator/ ).
+        """
+        key = 'HTML_VALID_VNU_JAR'
+        if key in os.environ:
+            self.assertTrue(
+                VnuValidate(path, os.environ[key], non_xhtml_cb,
+                            skip_cb).run(),
+                "passed validation")
+        else:
+            self.assertTrue(True, key + ' not set')
+
+
+class MyTests(VnuTest):
     def test_main(self):
         dir_ = './dest/post-incs/t2/'
 

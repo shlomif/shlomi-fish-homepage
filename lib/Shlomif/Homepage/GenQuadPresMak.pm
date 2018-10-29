618fba6806da657777cc0abfbd066c910ed42578
Shlomi Fish <shlomif@shlomifish.org>
2018-10-29 17:52:41 +0200

    Generate the vim-colored files progly.

--- lib/Shlomif/Homepage/GenQuadPresMak.pm
+++ lib/Shlomif/Homepage/GenQuadPresMak.pm
@@ -6,15 +6,48 @@ use warnings;
 use Moo;
 
 use Path::Tiny qw/ path /;
+use File::Update qw/ write_on_change /;
 use Template                         ();
+use Text::VimColor                   ();
 use Shlomif::Homepage::Presentations ();
 
 my $gen_quadpres_fn = "lib/make/docbook/sf-homepage-quadpres-generated.mak";
+
+my $BASENAMES = <<'EOF';
+APIs/grid/index.html.wml
+APIs/toc/index.html.wml
+bolding/template.wml
+common_look/Makefile
+common_look/download.html.wml
+common_look/index.html.wml
+common_look/links.html.wml
+common_look/template.wml
+faq-l/answers.html.wml
+faq-l/api.wml
+faq-l/index.html.wml
+faq-l/questions.wml
+frames/Makefile
+frames/navbar-frame.html.wml
+frames/navbar.wml
+frames/template.wml
+meta-tag/template.wml
+EOF
 
 sub generate
 {
     my ( $self, $args ) = @_;
 
+    foreach my $fn ( split /\n/, $BASENAMES )
+    {
+        my $ffn = "lib/presentations/qp/Website-Meta-Lecture/src/examples/$fn";
+        my $syntax = Text::VimColor->new(
+            file           => $ffn,
+            html_full_page => 1,
+        );
+
+        write_on_change( path( $ffn . ".html" ),
+            \( $syntax->html =~ s#(<meta[^/>]+[^/])>#$1/>#gr ) );
+    }
     my $tt = Template->new( {} );
     $tt->process(
         "lib/make/docbook/sf-homepage-quadpres-gen.tt",

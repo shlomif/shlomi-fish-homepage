7edffec6f5130292496caa737a81ae647b202fc1
Shlomi Fish <shlomif@shlomifish.org>
2019-02-06 21:15:37 +0200

    remove deps

--- lib/Shlomif/MD.pm
+++ lib/Shlomif/MD.pm
@@ -7,13 +7,18 @@ use parent 'Exporter';
 
 our @EXPORT_OK = qw/ print_markdown /;
 
-use Text::Markdown qw/ markdown /;
+use Markdent::Simple::Fragment ();
 use Path::Tiny qw/ path /;
 use Text::WrapAsUtf8 qw/ print_utf8 /;
 
 sub print_markdown
 {
-    print_utf8( markdown( path(shift)->slurp_utf8 ) );
+    print_utf8(
+        Markdent::Simple::Fragment->new->markdown_to_html(
+            markdown => path(shift)->slurp_utf8,
+            dialects => [qw/ GitHub /],
+        ),
+    );
 
     return;
 }

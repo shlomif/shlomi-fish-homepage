65e003bd38657e3502bb90bd4243d5ed4a6b3de4
Shlomi Fish <shlomif@shlomifish.org>
2019-02-18 12:20:30 +0200

    add scripts

---
+++ bin/heb-spell.bash
@@ -0,0 +1,15 @@
+#! /bin/bash
+#
+# hebspell.bash
+# Copyright (C) 2018 Shlomi Fish <shlomif@cpan.org>
+#
+# Distributed under terms of the MIT license.
+#
+
+unalias l
+l()
+{
+    make
+    bin/spell-checker-iface > foo.txt
+    perl bin/extract-spelling-errors.pl > y.txt
+}

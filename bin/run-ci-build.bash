24998736cd1807d0e23fd6ffbf15545f65db0457
Shlomi Fish <shlomif@shlomifish.org>
2019-04-06 20:37:12 +0300

    test identical contents

--- bin/run-ci-build.bash
+++ bin/run-ci-build.bash
@@ -9,3 +9,4 @@ set -e
 set -x
 touch t2/philosophy/SummerNSA/Letter-to-SGlau-2014-10/*.{xhtml,pdf}
 bash -x bin/travis-ci-script.bash
+find dest/post-incs/t2/ -type f | (LC_ALL=C sort) | head -500 | xargs sha256sum

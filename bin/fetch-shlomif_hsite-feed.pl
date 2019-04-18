5f950dafd08cee494946ccbb2d6b0dfb4c5cd139
Shlomi Fish <shlomif@shlomifish.org>
2019-04-18 19:52:30 +0300

    add the new news item blog post.

--- bin/fetch-shlomif_hsite-feed.pl
+++ bin/fetch-shlomif_hsite-feed.pl
@@ -11,4 +11,4 @@ my $feed = Shlomif::Homepage::Rss->new();
 $feed->run();
 
 # Touch the files so they'll be recompiled.
-utime( undef, undef, "t2/index.html.wml", "t2/old-news.html.wml" );
+utime( undef, undef, "t2/index.xhtml.wml", "t2/old-news.html.wml" );

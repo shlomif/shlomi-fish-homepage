d2fcfb47f5915cfc439ec14c35fc862c47b9fe9f
Shlomi Fish <shlomif@shlomifish.org>
2019-08-05 18:08:07 +0300

    convert from wml to template toolkit

--- bin/tot.bash
+++ bin/tot.bash
@@ -1,5 +1,5 @@
-./gen-helpers&& make fastrender&& make -j 17
-a="dest/post-incs/t2/art/by-others/index.xhtml"
+./gen-helpers && make fastrender && make -j 17
+a="dest/post-incs/t2/philosophy/philosophy/closed-books-are-so-19th-century/index.xhtml"
 b=/home/shlomif/Backup/Arcs/post-$a
 adest=have.xhtml
 bdest=want.xhtml
--- bin/wml2tt.pl
+++ bin/wml2tt.pl
@@ -17,7 +17,7 @@ s#<pdoc_f f="(\w+)">#[%- WRAPPER pdoc_f f = "$1" -%]#g;
 s#<cpan_dist d="([^"]*)">#[%- WRAPPER cpan_dist d = "$1" -%]#g;
 s#<pdoc d="(\w+)">#[%- WRAPPER pdoc d = "$1" -%]#g;
 my $re =
-qr#(?:perl_for_newbies_entry|note|modern_perl_entry|beginning_perl_entry|cpan_dist|pdoc|pdoc_f|licence_sect|links_sect|h[234]_section)#;
+qr#(?:intro|perl_for_newbies_entry|note|modern_perl_entry|beginning_perl_entry|cpan_dist|pdoc|pdoc_f|licence_sect|links_sect|h[234]_section)#;
 
 s#<($re)(?:\s([^>]*?))?>#    my ($tag, $args) = ($1, $2);
     "[% WRAPPER $tag " . ($args =~ s{([a-z]+)="([^"]+)"}{$1 = "$2" }gmrs) . " %]"

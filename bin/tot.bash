e640a565709dabf5c1496347285f76e256ebe80b
Shlomi Fish <shlomif@shlomifish.org>
2019-08-07 18:12:45 +0300

    convert from wml to template toolkit

--- bin/tot.bash
+++ bin/tot.bash
@@ -1,5 +1,5 @@
 ./gen-helpers && make fastrender && make -j 17
-a="dest/post-incs/t2/lecture/Perl/Lightning/Test-Run/updates/2006-05-09/index.xhtml"
+a="dest/post-incs/t2/open-source/resources/databases-list/index.xhtml"
 b=/home/shlomif/Backup/Arcs/post-$a
 adest=have.xhtml
 bdest=want.xhtml

18bc0039f573f673b0daa77b0d06fed093714a7a
Shlomi Fish <shlomif@shlomifish.org>
2019-08-08 13:38:09 +0300

    convert from wml to template toolkit

--- bin/tot.bash
+++ bin/tot.bash
@@ -1,5 +1,5 @@
 ./gen-helpers && make fastrender && make -j 17
-a="dest/post-incs/t2/philosophy/ideas/index.xhtml"
+a="dest/post-incs/t2/philosophy/computers/web/which-wiki/update-2006-07/index.xhtml"
 b=/home/shlomif/Backup/Arcs/post-$a
 adest=have.xhtml
 bdest=want.xhtml

6a3ebdcb46cdacac8a6a37fd90c4edd92753819c
Shlomi Fish <shlomif@shlomifish.org>
2019-08-19 15:18:05 +0300

    convert from wml to template toolkit

--- bin/tot.bash
+++
@@ -1,8 +0,0 @@
-./gen-helpers && make fastrender && make -j 17
-a="dest/post-incs/t2/open-source/resources/israel/guide-to-israeli-foss-resources/index.xhtml"
-b=/home/shlomif/Backup/Arcs/post-$a
-adest=have.xhtml
-bdest=want.xhtml
-tidy < $a > $adest
-tidy < $b > $bdest
-gvimdiff $adest $bdest +colorscheme" apprentice" +"exe \"normal \\<c-w>J\""

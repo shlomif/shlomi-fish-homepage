cef99e573f79e65c6d3da9f5457168ad43f99667
Shlomi Fish <shlomif@shlomifish.org>
2019-08-25 03:31:15 +0300

    convert from wml to template toolkit

--- bin/tot.bash
+++ bin/tot.bash
@@ -1,5 +1,5 @@
 ./gen-helpers && make fastrender && make -j 17
-a="dest/post-incs/t2/philosophy/computers/open-source/linus-torvalds-bus-factor/index.xhtml"
+a="dest/post-incs/t2/work/private-lessons/index.xhtml"
 b=/home/shlomif/Backup/Arcs/post-$a
 adest=have.xhtml
 bdest=want.xhtml

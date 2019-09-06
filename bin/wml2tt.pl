ae5fae7c533f7de880a2a07b42b0a2932c72045a
Shlomi Fish <shlomif@shlomifish.org>
2019-09-06 18:21:07 +0300

    convert from wml to template toolkit

--- bin/wml2tt.pl
+++ bin/wml2tt.pl
@@ -8,7 +8,7 @@ s#<(cc_by_british_blurb|cc_by_sa_british_blurb|cc_by_sa_license_british|book_inf
     "[% $tag( " . ($args =~ s{([a-z]+)="([^"]+)"}{"$1" => "$2",}gmrs) . ") %]"
     #egms;
 my $assigns = '';
-s#<set-var\s+(rtl_layout)="([^"]+)"\s+/>#$assigns .= qq/[% SET $1 = "$2" %]\n/;""#egms;
+s#\s*<set-var\s+(rtl_layout|shlomif_lang)="([^"]+)"\s*/>\s*#$assigns .= qq/[% SET $1 = "$2" %]\n/;""#egms;
 
 s#<(story_ongoing_episode_li|screenplay_github_repo|cc_by_licence_section|cc_by_nc_sa_british_blurb|common_art__graphics_design|docbook_formats_w_base|emma_watson_common_links|emma_watson_intro_text|emma_watson_intro|factoids_frame|home_pages_on_forges|humanity_licensing|lightning_talks_list|mailto_link_to_self|qp-lect|rellink|shlomif_docbook_doc_text|link_to_fiction_text|link_to_screenplay|link_to_epub_only|link_to_epub|link_to_fiction_text|tagline|humour_link|humour_story|humour_stories_list_text|story_episode_li|x11_licence|(?:[a-z_]+_nav_block))\s([^>]*?)/>#
     my ($tag, $args) = ($1, ($2 // ''));

a0a8634a2abadde7e52863af04de5112e4d6c53f
Shlomi Fish <shlomif@shlomifish.org>
2019-09-07 12:58:07 +0300

    convert from wml to template toolkit

--- bin/tot.bash
+++ bin/tot.bash
@@ -1,5 +1,5 @@
 ./gen-helpers && make fastrender && make -j 17
-a="dest/post-incs/t2/philosophy/language/hebrew/common-errors/rev1.html"
+a="dest/post-incs/t2/humour/Selina-Mandrake/index.xhtml"
 b=/home/shlomif/Backup/Arcs/post-$a
 dir=../temp-compare-out--to-del
 mkdir -p "$dir"

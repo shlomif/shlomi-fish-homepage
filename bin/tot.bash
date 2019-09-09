d3e98ba3da145561dd1c7fee4c2a8c8d493984a6
Shlomi Fish <shlomif@shlomifish.org>
2019-09-09 11:52:06 +0300

    convert from wml to template toolkit - min diff

--- bin/tot.bash
+++ bin/tot.bash
@@ -1,14 +1,18 @@
 ./gen-helpers && make fastrender && make -j 17
-a="dest/post-incs/t2/humour-heb.html"
-b=/home/shlomif/Backup/Arcs/post-$a
 dir=../temp-compare-out--to-del
 mkdir -p "$dir"
 adest="$dir/have.xhtml"
 bdest="$dir/want.xhtml"
+rm -f "$adest" "$bdest"
+for a in "dest/post-incs/t2/humour.html" "dest/post-incs/t2/open-source/bits.html" "dest/post-incs/t2/humour-heb.html" "dest/post-incs/t2/humour/RoadToHeaven/abstract.xhtml"
+do
+b=/home/shlomif/Backup/Arcs/post-$a
 f()
 {
     perl -lapE 's/</\n</g'
 }
-f < $a > $adest
-f < $b > $bdest
+(echo "== $a == " ; f < $a ) >> $adest
+(echo "== $a == " ; f < $b ) >> $bdest
+done
 gvimdiff $adest $bdest +colorscheme" apprentice" +"exe \"normal \\<c-w>J\""
+

35a54d9d38a492e4294a8cffeb6da2293ced9d8b
Shlomi Fish <shlomif@shlomifish.org>
2019-11-03 19:04:47 +0200

    changed from http: to https:

---
+++ lib/factoids/merge_into_fortunes.py
@@ -0,0 +1,11 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2019 Shlomi Fish <shlomif@cpan.org>
+#
+# Distributed under terms of the MIT license.
+
+"""
+
+"""

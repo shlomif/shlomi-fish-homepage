b937dd9b6edec24a5b89a37823413a4fe25892c9
Shlomi Fish <shlomif@shlomifish.org>
2019-11-04 19:42:06 +0200

    [factoids] progress w the merge prog; finished!

--- lib/factoids/merge_into_fortunes.py
+++ lib/factoids/merge_into_fortunes.py
@@ -18,6 +18,10 @@ from lxml import etree
 
 XML_NS = "{http://www.w3.org/XML/1998/namespace}"
 ns = {"xml": XML_NS, }
+FULL_NAMES = {"chuck": "Chuck Norris",
+              "emma-watson": "Emma Watson",
+              "nsa": "NSA",
+              "taylor-swift": "Taylor Swift", }
 
 
 def main():
@@ -26,6 +30,8 @@ def main():
         "./t2/humour/fortunes/proto--shlomif-factoids.xml")
     max_idxs = defaultdict(int)
     max_idxs_ids = {}
+    max_idxs_elems = {}
+    target_list = target_root.xpath("./list")[0]
     for targetelem in target_root.xpath("./list/fortune"):
         id_ = targetelem.get("id")
         match = re.match("^[a-z\\-]+-fact-([a-z\\-]+)-([0-9]+)$", id_)
@@ -35,6 +41,7 @@ def main():
             if max_idxs[category] < idx:
                 max_idxs[category] = idx
                 max_idxs_ids[category] = id_
+                max_idxs_elems[category] = targetelem
 
             print('ID: {} cat: {} idx: {}'.format(id_, category, idx))
     print(max_idxs)
@@ -53,6 +60,39 @@ def main():
                 facts_to_add.append(deepcopy(fact))
                 fact = fact.getnext()
             print("\t{}".format(len(facts_to_add)))
+            target_idx = target_list.index(
+                target_list.xpath(
+                    "./fortune[@id='{}']".format(
+                        max_idxs_ids[facts_basename]))[0])
+            full_name = FULL_NAMES[facts_basename]
+            for idx, iter_fact in enumerate(facts_to_add):
+                newidx = idx + max_idxs[facts_basename] + 1
+                new_elem = etree.Element(
+                    "fortune", id="{}-fact-{}-{}".format(
+                        "shlomif",
+                        facts_basename,
+                        newidx
+                        ))
+                meta = etree.SubElement(new_elem, "meta")
+                title = etree.SubElement(meta, "title")
+                title.text = "Shlomi Fish’s {} Fact #{}".format(
+                    full_name, newidx)
+                quote = etree.SubElement(new_elem, "quote")
+                quote.insert(0, iter_fact.xpath(
+                    "./lang[@xml:lang='en-US']/body", namespaces=ns)[0])
+                info = etree.SubElement(quote, "info")
+                author = etree.SubElement(info, "author")
+                author.text = "Shlomi Fish"
+                work = etree.SubElement(
+                    info,
+                    "work",
+                    href="http://www.shlomifish.org/" +
+                    "humour/bits/facts/{}/".format(
+                        full_name.replace(' ', '-')))
+                work.text = ("{} Facts by " +
+                             "Shlomi Fish and Friends").format(full_name)
+                target_list.insert(target_idx+idx+1, new_elem)
+
     with open("./t2/humour/fortunes/shlomif-factoids.xml", "wb") as f:
         f.write(etree.tostring(target_root, pretty_print=True))
 

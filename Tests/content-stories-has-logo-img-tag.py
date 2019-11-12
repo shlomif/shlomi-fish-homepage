90c2209ea24e256938e3b2e09b3f33ec4335337b
Shlomi Fish <shlomif@shlomifish.org>
2019-11-12 07:31:13 +0200

    Refactoring / code cleanup.
    See:
    
    * https://en.wikipedia.org/wiki/Code_refactoring
    
    * https://www.refactoring.com/
    
    * https://www.joelonsoftware.com/2002/01/23/rub-a-dub-dub/
    
    Some small optimisations may have slipped in as well.

--- Tests/content-stories-has-logo-img-tag.py
+++ Tests/content-stories-has-logo-img-tag.py
@@ -10,23 +10,15 @@ import unittest
 
 from lxml import etree
 
-XML_NS = "{http://www.w3.org/XML/1998/namespace}"
-XHTML_NS = "{http://www.w3.org/1999/xhtml}"
-ns = {"xml": XML_NS, "xhtml": XHTML_NS, }
-FULL_NAMES = {"chuck": "Chuck Norris",
-              "emma-watson": "Emma Watson",
-              "nsa": "NSA",
-              "taylor-swift": "Taylor Swift", }
-
 
 class MyTests(unittest.TestCase):
     def test_main(self):
         input_fn = './dest/post-incs/t2/humour/Selina-Mandrake/index.xhtml'
         root = etree.HTML(open(input_fn, "rb").read())
         self.assertTrue(len(root.xpath(
-            (".//*[name()='img' and @id='selina_mandrake_logo' and " +
-             "@src='images/Green-d10-dice.png']"
-             ), namespaces=ns)) == 1)
+            ".//*[name()='img' and @id='selina_mandrake_logo' and " +
+            "@src='images/Green-d10-dice.png']"
+            )) == 1)
 
 
 if __name__ == '__main__':

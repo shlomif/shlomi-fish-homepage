f3488aa02ccfe7ab4d3b8cb0da17a0a42a611321
Shlomi Fish <shlomif@shlomifish.org>
2019-11-13 18:46:03 +0200

    add a test

---
+++ Tests/content-docbook5-has-sub-header.py
@@ -0,0 +1,28 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2019 Shlomi Fish <shlomif@cpan.org>
+#
+# Distributed under terms of the MIT license.
+
+import unittest
+
+from lxml import etree
+
+
+class MyTests(unittest.TestCase):
+    def test_main(self):
+        input_fn = 'dest/post-incs/t2/philosophy/philosophy/' + \
+            'putting-cards-on-the-table-2019-2020/index.xhtml'
+        root = etree.HTML(open(input_fn, "rb").read())
+        self.assertEqual(len(root.xpath(
+            ".//h5[@id='fox-in-the-hens-coop' and " +
+            "text()='The Fox in the Chickens Coop']"
+            )), 1)
+
+
+if __name__ == '__main__':
+    from pycotap import TAPTestRunner
+    suite = unittest.TestLoader().loadTestsFromTestCase(MyTests)
+    TAPTestRunner().run(suite)

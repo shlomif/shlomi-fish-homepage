63d4d7e746f9e9c3a5fbc077ac66eb0f445de4b7
Shlomi Fish <shlomif@shlomifish.org>
2019-11-29 10:38:50 +0200

    progress

--- lib/faq/split_into_sections.py
+++ lib/faq/split_into_sections.py
@@ -34,6 +34,33 @@ FULL_NAMES = {"chuck": "Chuck Norris",
 ID_IS_FACTOID_RE = re.compile("^[a-z\\-]+-fact-([a-z\\-]+)-([0-9]+)$")
 BASENAME_EXTRACT_RE = re.compile('^([a-z_\\-]*)_facts$')
 
+XHTML_START_FMT = '''<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en-US" lang="en-US">
+<head>
+<title>Shlomi Fish’s FAQ - {title}</title>
+<meta charset="utf-8" />
+<meta name="description" content=
+"Shlomi Fish’s Frequently Asked Questions (FAQ) List" />
+<link rel="stylesheet" href="../../style.css" media="screen" title=
+"Normal" />
+<link rel="stylesheet" href="../../print.css" media="print" />
+<link rel="shortcut icon" href="../../favicon.ico" type=
+"image/x-icon" />
+<meta name="viewport" content="width=device-width,initial-scale=1" />
+<script src="../../js/main_all.js"></script>
+</head>
+<body>
+<main class="main" id="main">
+'''
+
+XHTML_END_FMT = '''</main>
+<footer>
+<a href="../../"><img src="../../images/bk2hp-v2.min.svg" class=
+"bk2hp" alt="Back to my Homepage" /></a></footer>
+</body>
+</html>'''
+
 
 class FortunesMerger:
     def __init__(self, input_fn, merge_into_fn):
@@ -49,9 +76,12 @@ class FortunesMerger:
             h_tag = list_elem.xpath("./xhtml:header/*[@id]", namespaces=ns)[0]
             id_ = h_tag.xpath("./@id", namespaces=ns)[0]
             header_text = h_tag.xpath("./text()", namespaces=ns)[0]
-            print([id_, header_text])
+            # print([id_, header_text])
             with open("{}/{}.xhtml".format(OUT_DN, id_), "wt") as f:
-                f.write(etree.tostring(list_elem).decode('utf-8'))
+                f.write(XHTML_START_FMT.format(title=header_text) +
+                        etree.tostring(list_elem).decode('utf-8') +
+                        XHTML_END_FMT.format(title=header_text)
+                        )
             # print(etree.tostring(id_))
 
 

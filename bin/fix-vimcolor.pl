fb3d43454d5027ae8fd620803eff34b4e4c25444
Shlomi Fish <shlomif@shlomifish.org>
2019-12-03 09:03:27 +0200

    avoid no op

--- bin/fix-vimcolor.pl
+++ bin/fix-vimcolor.pl
@@ -7,7 +7,6 @@ use Path::Tiny qw/ path /;
 path(shift)->edit_utf8(
     sub {
 s#<html>#<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en-US">#ms;
-        s#^( *)<style>$#$1<style>#ms;
         s#\A#<?xml version="1.0" encoding="utf-8"?>#;
         s#(<!DOCTYPE html)[^>]+(>)#$1$2#;
     }

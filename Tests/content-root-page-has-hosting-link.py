e1938b2e0234ddf7ea3f8950cc727b15d9bf4493
Shlomi Fish <shlomif@shlomifish.org>
2019-12-03 13:35:34 +0200

    restore the hosting icon on the /.
    the root page.

---
+++ Tests/content-root-page-has-hosting-link.py
@@ -0,0 +1,27 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2019 Shlomi Fish <shlomif@cpan.org>
+#
+# Distributed under terms of the MIT license.
+
+import unittest
+
+from lxml import etree
+
+
+class MyTests(unittest.TestCase):
+    def test_main(self):
+        input_fn = './dest/post-incs/t2/index.xhtml'
+        root = etree.HTML(open(input_fn, "rb").read())
+        self.assertTrue(len(root.xpath(
+            ".//footer//a[@href = 'meta/hosting/#hostgator']/"
+            + "img[@src='images/hostgator.png' and @alt='Hosted at HostGator']"
+            )) == 1)
+
+
+if __name__ == '__main__':
+    from pycotap import TAPTestRunner
+    suite = unittest.TestLoader().loadTestsFromTestCase(MyTests)
+    TAPTestRunner().run(suite)

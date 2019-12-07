191f17bdb6c1a1bd79f67ef3d3d52d61fee45f47
Shlomi Fish <shlomif@shlomifish.org>
2019-12-07 21:53:08 +0200

    fix a charset problem.

--- src/humour/fortunes/gen-htaccess.pl
+++ src/humour/fortunes/gen-htaccess.pl
@@ -19,13 +19,15 @@ my @basenames = @ARGV;
 open my $o, '>', $output_filename;
 
 $o->print("AddType text/plain .txt\n");
+$o->print("AddDefaultCharset utf-8\n");
 foreach my $base (@basenames)
 {
     foreach my $ext ( '', '.xml', '.xhtml' )
     {
+        my $type = $ext eq '' ? "ForceType text/plain\n" : '';
         $o->print(<<"EOF");
 <Files "$base$ext">
-        Header add Link "<http://www.shlomifish.org/humour/fortunes/$base.html>; rel=\\"canonical\\""
+${type}        Header add Link "<http://www.shlomifish.org/humour/fortunes/$base.html>; rel=\\"canonical\\""
 </Files>
 EOF
     }

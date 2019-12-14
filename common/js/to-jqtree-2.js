95bb3c0dfad78dfc6a5e0bfabc4dbb1d87a30921
Shlomi Fish <shlomif@shlomifish.org>
2019-12-14 15:48:32 +0200

    refactoring

--- common/js/to-jqtree-2.js
+++ common/js/to-jqtree-2.js
@@ -1,16 +1,15 @@
 "use strict";
 function shlomif_load_nav (page_path) {
-    var b = 'http://www.shlomifish.org/';
     $.getJSON(
         get_relative_path({
             rel_path: page_path,
             to: '_data/nav.json',
         }),
         function(json_input) {
-            var nav_menu = $('#nav_menu');
-            var has_ls = (typeof localStorage !== "undefined" && localStorage !== null);
-            var storage_key = 'shlomifish.org_main_nav_menu_data';
-            var was_storage_already_populated = false;
+            const nav_menu = $('#nav_menu');
+            const has_ls = (typeof localStorage !== "undefined" && localStorage !== null);
+            const storage_key = 'shlomifish.org_main_nav_menu_data';
+            let was_storage_already_populated = false;
             if (has_ls) {
                 was_storage_already_populated = (localStorage.getItem(storage_key) ? true : false);
             }
@@ -27,17 +26,16 @@ function shlomif_load_nav (page_path) {
             });
 
             if (! was_storage_already_populated) {
-                var about_myself_node_id = "2";
-                var art_node_id = "120";
-                var essays_node_id = "235";
-                var humour_node_id = "13";
-                var lectures_node_id = "193";
-                var meta_node_id = "311";
-                var soft_node_id = "127";
+                const about_myself_node_id = "2";
+                const art_node_id = "120";
+                const essays_node_id = "235";
+                const humour_node_id = "13";
+                const lectures_node_id = "193";
+                const meta_node_id = "311";
+                const soft_node_id = "127";
 
                 [about_myself_node_id, art_node_id, essays_node_id, humour_node_id, lectures_node_id, meta_node_id, soft_node_id].forEach(function (node_id) {
-                    var node = nav_menu.tree('getNodeById', node_id);
-                    nav_menu.tree('openNode', node, false);
+                    nav_menu.tree('openNode', nav_menu.tree('getNodeById', node_id), false);
                 });
             }
         }

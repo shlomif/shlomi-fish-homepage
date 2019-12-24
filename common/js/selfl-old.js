9b6c3f36a7ffcd4de5b9507386e02b66b17e3871
Shlomi Fish <shlomif@shlomifish.org>
2019-12-24 17:00:26 +0200

    Refactoring / code cleanup.
    See:
    
    * https://en.wikipedia.org/wiki/Code_refactoring
    
    * https://www.refactoring.com/
    
    * https://www.joelonsoftware.com/2002/01/23/rub-a-dub-dub/
    
    Some small optimisations may have slipped in as well.

--- common/js/selfl-old.js
+++
@@ -1,1 +0,0 @@
-$("h1[id],h2[id],h3[id],h4[id],h5[id],h6[id]").each(function(i){ $(this).append( ' <span class="selfl">[<a href="#' + this.id + '">link</a>]</span>' ) });
--- common/js/toggler.js
+++ common/js/toggler.js
@@ -1,34 +1,34 @@
 "use strict";
 
 function build_toggler(args) {
-    var has_ls = (typeof localStorage !== "undefined" && localStorage !== null);
-    var toggle_sect_key = args['ls_key'];
-    var toggler_selector = args['toggler_selector'];
-    var toggled_selector = args['toggled_selector'];
-    var toggled_class = args['toggled_class'];
-    var hide_text = args['hide_text'];
-    var show_text = args['show_text'];
-    var default_state = (('default_state' in args) ? args['default_state'] : true);
+    const has_ls = (typeof localStorage !== "undefined" && localStorage !== null);
+    const toggle_sect_key = args['ls_key'];
+    const toggler_selector = args['toggler_selector'];
+    const toggled_selector = args['toggled_selector'];
+    const toggled_class = args['toggled_class'];
+    const hide_text = args['hide_text'];
+    const show_text = args['show_text'];
+    const default_state = (('default_state' in args) ? args['default_state'] : true);
 
-    var toggle_sect_menu = function() {
-        var elem = $(toggler_selector);
+    function toggle_sect_menu() {
+        const elem = $(toggler_selector);
 
-        var was_off = elem.hasClass("off");
-        var was_on = !was_off;
-        var is_on_now = !was_on;
+        const was_off = elem.hasClass("off");
+        const was_on = !was_off;
+        const is_on_now = !was_on;
 
         elem.html(was_off ? hide_text : show_text);
         $(toggled_selector).toggleClass(toggled_class);
         elem.toggleClass("off");
         elem.toggleClass("on");
         if (has_ls) {
-            if (is_on_now == default_state) {
+            if (is_on_now === default_state) {
                 localStorage.removeItem(toggle_sect_key);
             } else {
                 localStorage.setItem(toggle_sect_key, (is_on_now ? "1" : "0"));
             }
         }
-    };
+    }
 
     const elem = $(toggler_selector);
 
@@ -41,10 +41,10 @@ function build_toggler(args) {
         }
         let in_storage = localStorage.getItem(toggle_sect_key);
 
-        if (in_storage == undefined) {
+        if (in_storage === undefined) {
             in_storage = default_state;
         } else {
-            in_storage = (in_storage == "1" ? true : false);
+            in_storage = (in_storage === "1" ? true : false);
         }
 
         const in_elem = elem.hasClass("on");

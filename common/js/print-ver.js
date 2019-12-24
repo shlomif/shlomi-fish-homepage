313d539c843a50b6cbf59c32f9c9e387a139488d
Shlomi Fish <shlomif@shlomifish.org>
2019-12-24 16:44:17 +0200

    remove unused JS files

--- common/js/print-ver.js
+++
@@ -1,7 +0,0 @@
-$('#prnt_ver').html('<button id="prnt_ver_link">' +
-    '<img src="/images/printer_icon.png" alt="Printer Icon" /> ' +
-    'Print Version</button>');
-$('#prnt_ver_link').click(function() {
-    alert('The page is printable as it is. Just hit print preview and ' +
-          'you\'ll be able to print it.');
-});

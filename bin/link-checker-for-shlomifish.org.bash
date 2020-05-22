e5e93df6a8d22eb4f79e746c800fc185e2b067eb
Shlomi Fish <shlomif@shlomifish.org>
2020-05-22 18:44:48 +0300

    made the running of link-check less errory

--- bin/link-checker-for-shlomifish.org.bash
+++ bin/link-checker-for-shlomifish.org.bash
@@ -1,4 +1,4 @@
-perl -Ilib scripts/link-checker --base='http://localhost/shlomif/homepage-local/' \
+perl -Ilib -MWWW::LinkChecker::Internal::App -e 'WWW::LinkChecker::Internal::App->run()' -- check --base='http://localhost/shlomif/homepage-local/' \
     --before-insert-skip='\.(?:js|epub|zip|diff|pl|xsl|xslt|rtf|txt|raw\.html)\z' \
     --before-insert-skip='art/by-others/(?:BertycoX/BertycoX-dirs|Yachar/Yachar-dirs)/\z' \
     --before-insert-skip='/lm-solve/\z' \

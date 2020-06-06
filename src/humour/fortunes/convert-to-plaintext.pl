7bcd9c9ea11923090243829d0c4a9cd853880696
Shlomi Fish <shlomif@shlomifish.org>
2020-06-06 12:59:12 +0300

    optimize

--- src/humour/fortunes/convert-to-plaintext.pl
+++ src/humour/fortunes/convert-to-plaintext.pl
@@ -1,21 +1,11 @@
 use strict;
 use warnings;
 
-use XML::Grammar::Fortune::ToText;
-use File::Spec;
+use XML::Grammar::Fortune::ToText();
 
-my $xml_fn = shift(@ARGV);
-my $out_fn = shift(@ARGV);
+my ( $xml_fn, $out_fn ) = @ARGV;
 
-my $html_fn = $xml_fn;
-
-my $abs_xml_fn = File::Spec->rel2abs($xml_fn);
-
-my $abs_out_fn = File::Spec->rel2abs($out_fn);
-
-{
-    open my $out, ">:encoding(UTF-8)", $abs_out_fn;
-    XML::Grammar::Fortune::ToText->new(
-        { input => $abs_xml_fn, output => $out } )->run();
-    close($out);
-}
+open my $out, ">:encoding(UTF-8)", $out_fn;
+XML::Grammar::Fortune::ToText->new( { input => $xml_fn, output => $out } )
+    ->run();
+close($out);
--- src/humour/fortunes/convert-to-xhtml.pl
+++ src/humour/fortunes/convert-to-xhtml.pl
@@ -2,59 +2,50 @@ use strict;
 use warnings;
 use utf8;
 
-use XML::Grammar::Fortune ();
-use XML::LibXML           ();
-use URI::Find             ();
+use XML::Grammar::Fortune     ();
+use XML::LibXML               ();
+use URI::Find                 ();
+use XML::LibXML::XPathContext ();
 use Encode qw/ decode /;
 
-my $xml_fn = shift(@ARGV);
-my $out_fn = shift(@ARGV);
-
-my $html_fn = $xml_fn;
-
-my $abs_xml_fn = $xml_fn;
-
-my $abs_out_fn = $out_fn;
+my ( $xml_fn, $out_fn ) = @ARGV;
 
 XML::Grammar::Fortune->new( { mode => "validate", } )
-    ->run( { input => $abs_xml_fn, } );
+    ->run( { input => $xml_fn, } );
 XML::Grammar::Fortune->new(
     { mode => "convert_to_html", output_mode => "filename" } )
-    ->run( { input => $abs_xml_fn, output => $abs_out_fn } );
+    ->run( { input => $xml_fn, output => $out_fn } );
 my $contents;
 
 my $PREF = '«⋄⋄אבג«';
 my $SUF  = '»גבא⋄⋄»';
-{
-    use XML::LibXML::XPathContext;
-    my $doc = XML::LibXML->load_xml( location => $abs_out_fn );
-    my $xc  = XML::LibXML::XPathContext->new($doc);
-    $xc->registerNs( 'html' => "http://www.w3.org/1999/xhtml" );
-    my $finder = URI::Find->new(
-        sub {
-            my ( $obj, $text ) = @_;
-            return qq#$PREF$text$SUF#;
-        }
-    );
-    foreach my $node (
-        $xc->findnodes(
-q#//html:table[@class='irc-conversation']/html:tbody/html:tr[@class='saying']/html:td[@class='text']/text()#
-        )
-        )
-    {
-        my $text_content = $node->data;
-        $finder->find( \$text_content );
-
-        $node->setData($text_content);
+my $doc  = XML::LibXML->load_xml( location => $out_fn );
+my $xc   = XML::LibXML::XPathContext->new($doc);
+$xc->registerNs( 'html' => "http://www.w3.org/1999/xhtml" );
+my $finder = URI::Find->new(
+    sub {
+        my ( $obj, $text ) = @_;
+        return qq#$PREF$text$SUF#;
     }
-    $contents = decode( 'UTF-8', $doc->toString() );
+);
+foreach my $node (
+    $xc->findnodes(
+q#//html:table[@class='irc-conversation']/html:tbody/html:tr[@class='saying']/html:td[@class='text']/text()#
+    )
+    )
+{
+    my $text_content = $node->data;
+    $finder->find( \$text_content );
+
+    $node->setData($text_content);
 }
+$contents = decode( 'UTF-8', $doc->toString() );
 $contents =~ s# xmlns:xsi="[^"]*"##gms;
 $contents =~ s/[ \t]+$//gms;
 
 $contents =~ s#\Q$PREF\E(.*?)\Q$SUF\E#<a href="$1" rel="nofollow">$1</a>#gms;
 
-open my $back_fh, '>:encoding(UTF-8)', $abs_out_fn
-    or die "Cannot open '$abs_out_fn' for writing - $!";
+open my $back_fh, '>:encoding(UTF-8)', $out_fn
+    or die "Cannot open '$out_fn' for writing - $!";
 print {$back_fh} $contents;
 close($back_fh);

0ddad067ca27a2aa6ebb7ec9744385b13e4a0401
Shlomi Fish <shlomif@shlomifish.org>
2020-07-01 18:14:03 +0300

    refactoring/optimisation: del unused "vipe" host

--- bin/gen-sect-nav-menus.pl
+++ bin/gen-sect-nav-menus.pl
@@ -31,149 +31,141 @@ use File::Update qw/ write_on_change /;
 
 # At least temporarily disable Parallel::ForkManager because it causes
 # the main process to exit before all the work is done.
-my $hosts = MyNavData::Hosts::get_hosts();
-foreach my $host (qw(t2 vipe))
-{
-    my $host_base_url = $hosts->{$host}->{base_url};
-    my $hostp         = "lib/cache/combined/$host";
+my $hosts         = MyNavData::Hosts::get_hosts();
+my $host          = 't2';
+my $host_base_url = $hosts->{$host}->{base_url};
+my $hostp         = "lib/cache/combined/$host";
 
-    Parallel::ForkManager::Segmented->new->run(
-        {
-            items => (
-                  ( $host eq 't2' )
-                ? ( [ ( split /\n/, path("lib/make/tt2.txt")->slurp_raw() ), ] )
-                : [qw(index.html lecture/index.html)]
-            ),
-            nproc        => 4,
-            batch_size   => 16,
-            process_item => sub {
-                my $proto_url = shift;
-                my $url       = $proto_url =~ s#(?:\A|/)\Kindex\.x?html\z##r;
+Parallel::ForkManager::Segmented->new->run(
+    {
+        items => [ ( split /\n/, path("lib/make/tt2.txt")->slurp_raw() ), ],
+        nproc => 4,
+        batch_size   => 16,
+        process_item => sub {
+            my $proto_url = shift;
+            my $url       = $proto_url =~ s#(?:\A|/)\Kindex\.x?html\z##r;
 
-                my $filename = "/$url";
+            my $filename = "/$url";
 
-                # urlpath.
-                my $urlp = path("$hostp/$proto_url");
-                $urlp->mkpath;
+            # urlpath.
+            my $urlp = path("$hostp/$proto_url");
+            $urlp->mkpath;
 
-                # print "start filename=$filename\n";
+            # print "start filename=$filename\n";
 
-                my $nav_bar = HTML::Widgets::NavMenu::JQueryTreeView->new(
-                    'path_info'    => $filename,
-                    'current_host' => $host,
-                    MyNavData::get_params(),
-                    'ul_classes'     => [],
-                    'no_leading_dot' => 1,
-                );
-                my $ROOT    = get_root($url);
-                my $results = NavSectMenuRender->init_section_nav_menu(
-                    {
-                        filename => $filename,
-                        host     => $host,
-                        ROOT     => $ROOT,
-                    }
-                );
-
-                my $section_nav_menu = $results->{section_nav_menu};
-                my $rendered_results = $nav_bar->render();
-
-                my $leading_path = $rendered_results->{leading_path};
-
-                my $nav_results = NavDataRender->nav_data_render(
-                    {
-                        filename => $url,
-                        host     => $host,
-                        ROOT     => $ROOT,
-                    }
-                );
-
-                # warn Dumper({Results => $results,});
-
-                my $shlomif_nav_links_renderer =
-                    $nav_results->{nav_links_renderer};
-
-                my $nav_links_obj = $rendered_results->{nav_links_obj};
-
-                my $out = sub {
-                    my ( $id, $ref ) = @_;
-
-                    return write_on_change( $urlp->child($id), $ref );
-                };
-
-                $out->( 'sect-navmenu', \( $section_nav_menu->get_html ), );
-                $out->(
-                    'breadcrumbs-trail',
-                    \(
-                        NavDataRender->get_breadcrumbs_trail_unconditionally(
-                            {
-                                total_leading_path =>
-                                    $section_nav_menu->total_leading_path(
-                                    {
-                                        main_leading_path => $leading_path,
-                                    }
-                                    ),
-                            }
-                        )
-                    ),
-                );
-                $out->(
-                    'main_nav_menu_html',
-                    \( join( "\n", @{ $rendered_results->{html} } ) ),
-                );
-                $out->(
-                    'html_head_nav_links',
-                    \(
-                        NavDataRender->get_html_head_nav_links(
-                            { nav_links_obj => $nav_links_obj }
-                        )
-                    ),
-                );
-
-                $out->(
-                    'page_url',
-                    \( escape_html( uri_escape( $host_base_url . $url ) ) ),
-                );
-
-                foreach my $with_accesskey ( '', 1 )
+            my $nav_bar = HTML::Widgets::NavMenu::JQueryTreeView->new(
+                'path_info'    => $filename,
+                'current_host' => $host,
+                MyNavData::get_params(),
+                'ul_classes'     => [],
+                'no_leading_dot' => 1,
+            );
+            my $ROOT    = get_root($url);
+            my $results = NavSectMenuRender->init_section_nav_menu(
                 {
-                    $out->(
-"shlomif_nav_links_renderer-with_accesskey=$with_accesskey",
-                        do
-                        {
-                            my @params;
-                            if ( $with_accesskey ne "" )
-                            {
-                                push @params,
-                                    ( 'with_accesskey' => $with_accesskey );
-                            }
-                            \(
-                                join(
-                                    '',
-                                    $shlomif_nav_links_renderer->get_total_html(
-                                        @params)
-                                ) =~ s%<img src="\K\./%%gr
-                            );
-                        },
-                    );
+                    filename => $filename,
+                    host     => $host,
+                    ROOT     => $ROOT,
                 }
+            );
 
-                if ( $filename eq '/site-map/' )
+            my $section_nav_menu = $results->{section_nav_menu};
+            my $rendered_results = $nav_bar->render();
+
+            my $leading_path = $rendered_results->{leading_path};
+
+            my $nav_results = NavDataRender->nav_data_render(
                 {
-                    $out->(
-                        'shlomif_main_expanded_nav_bar',
+                    filename => $url,
+                    host     => $host,
+                    ROOT     => $ROOT,
+                }
+            );
+
+            # warn Dumper({Results => $results,});
+
+            my $shlomif_nav_links_renderer = $nav_results->{nav_links_renderer};
+
+            my $nav_links_obj = $rendered_results->{nav_links_obj};
+
+            my $out = sub {
+                my ( $id, $ref ) = @_;
+
+                return write_on_change( $urlp->child($id), $ref );
+            };
+
+            $out->( 'sect-navmenu', \( $section_nav_menu->get_html ), );
+            $out->(
+                'breadcrumbs-trail',
+                \(
+                    NavDataRender->get_breadcrumbs_trail_unconditionally(
+                        {
+                            total_leading_path =>
+                                $section_nav_menu->total_leading_path(
+                                {
+                                    main_leading_path => $leading_path,
+                                }
+                                ),
+                        }
+                    )
+                ),
+            );
+            $out->(
+                'main_nav_menu_html',
+                \( join( "\n", @{ $rendered_results->{html} } ) ),
+            );
+            $out->(
+                'html_head_nav_links',
+                \(
+                    NavDataRender->get_html_head_nav_links(
+                        { nav_links_obj => $nav_links_obj }
+                    )
+                ),
+            );
+
+            $out->(
+                'page_url',
+                \( escape_html( uri_escape( $host_base_url . $url ) ) ),
+            );
+
+            foreach my $with_accesskey ( '', 1 )
+            {
+                $out->(
+                    "shlomif_nav_links_renderer-with_accesskey=$with_accesskey",
+                    do
+                    {
+                        my @params;
+                        if ( $with_accesskey ne "" )
+                        {
+                            push @params,
+                                ( 'with_accesskey' => $with_accesskey );
+                        }
                         \(
                             join(
                                 '',
-                                map { "$_\n" } @{
-                                    $nav_results
-                                        ->{shlomif_main_expanded_nav_bar}
-                                        ->gen_site_map()
-                                }
-                            )
-                        ),
-                    );
-                }
+                                $shlomif_nav_links_renderer->get_total_html(
+                                    @params)
+                            ) =~ s%<img src="\K\./%%gr
+                        );
+                    },
+                );
+            }
+
+            if ( $filename eq '/site-map/' )
+            {
+                $out->(
+                    'shlomif_main_expanded_nav_bar',
+                    \(
+                        join(
+                            '',
+                            map { "$_\n" } @{
+                                $nav_results->{shlomif_main_expanded_nav_bar}
+                                    ->gen_site_map()
+                            }
+                        )
+                    ),
+                );
             }
         }
-    );
-}
+    }
+);

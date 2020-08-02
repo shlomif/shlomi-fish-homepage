1af5382c1b04babaa637711c81f604d9d01c2201
Shlomi Fish <shlomif@shlomifish.org>
2020-08-02 22:29:52 +0300

    remove unused cache* methods

--- lib/Shlomif/Homepage/NavBlocks/Thingy.pm
+++ lib/Shlomif/Homepage/NavBlocks/Thingy.pm
@@ -3,22 +3,6 @@ package Shlomif::Homepage::NavBlocks::Thingy;
 use strict;
 use warnings;
 
-use MooX (qw( late ));
-
-has 'cache'     => ( is => 'rw' );
-has 'is_cached' => ( is => 'rw', isa => 'Bool', default => 0 );
-
-sub cached_render
-{
-    my ( $self, $promise_cb ) = @_;
-
-    if ( !$self->is_cached )
-    {
-        $self->cache( scalar( $promise_cb->() ), );
-        $self->is_cached(1);
-    }
-
-    return $self->cache;
-}
+use Moo;
 
 1;

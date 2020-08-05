fb5b43b56f7f9ccb2600be328d7eeba9d3fc1df1
Shlomi Fish <shlomif@shlomifish.org>
2020-08-05 16:33:47 +0300

    add licence

--- bin/normalize-zips.pl
+++ bin/normalize-zips.pl
@@ -1,11 +1,26 @@
 #! /usr/bin/env perl
+
+# Copyright 2014 Andrew Ayer
 #
-# Short description for normalize-zip.pl
+# This file is part of strip-nondeterminism.
 #
-# Author Shlomi Fish <shlomif@cpan.org>
-# Version 0.0.1
+# strip-nondeterminism is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# strip-nondeterminism is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with strip-nondeterminism.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+# Modified by Shlomi Fish <shlomif@cpan.org>
 # Based on strip-nondeterminism. GPLed.
-#
+
 use strict;
 use warnings;
 

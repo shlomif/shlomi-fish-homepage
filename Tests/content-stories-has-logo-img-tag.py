f6da5576b0273c1123e78a774948162b50db05de
Shlomi Fish <shlomif@shlomifish.org>
2020-09-30 12:20:08 +0300

    refactoring

--- Tests/content-stories-has-logo-img-tag.py
+++ Tests/content-stories-has-logo-img-tag.py
@@ -8,18 +8,13 @@
 
 import unittest
 
-from lxml import etree
-
-
-def _binary_slurp(fn):
-    with open(fn, 'rb') as f:
-        return f.read()
+from lxml import html
 
 
 class MyTests(unittest.TestCase):
     def test_main(self):
         input_fn = './dest/post-incs/t2/humour/Selina-Mandrake/index.xhtml'
-        root = etree.HTML(_binary_slurp(input_fn))
+        root = html.parse(input_fn)
         self.assertTrue(len(root.xpath(
             ".//img[@id='selina_mandrake_logo' and " +
             "@src='images/Green-d10-dice.webp']"
@@ -27,7 +22,7 @@ class MyTests(unittest.TestCase):
 
     def test_factoids(self):
         input_fn = './dest/post-incs/t2/humour/fortunes/shlomif-factoids.html'
-        root = etree.HTML(_binary_slurp(input_fn))
+        root = html.parse(input_fn)
         self.assertTrue(len(root.xpath(
             ".//h3[@id='shlomif-fact-xena-1' and " +
             "text()='Shlomi Fish’s Xena the Warrior Princess Fact #1']"

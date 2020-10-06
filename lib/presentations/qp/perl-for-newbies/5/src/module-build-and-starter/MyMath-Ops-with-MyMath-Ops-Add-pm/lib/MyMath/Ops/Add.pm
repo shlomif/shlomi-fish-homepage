5efaaf4ffc0c29c9a72414f120b0c3baa6d753f1
Shlomi Fish <shlomif@shlomifish.org>
2020-10-06 20:12:10 +0300

    remove dead link to ancpan

--- lib/presentations/qp/perl-for-newbies/5/src/module-build-and-starter/MyMath-Ops-with-MyMath-Ops-Add-pm/lib/MyMath/Ops/Add.pm
+++ lib/presentations/qp/perl-for-newbies/5/src/module-build-and-starter/MyMath-Ops-with-MyMath-Ops-Add-pm/lib/MyMath/Ops/Add.pm
@@ -113,10 +113,6 @@ You can also look for information at:
 
 L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=MyMath::Ops>
 
-=item * AnnoCPAN: Annotated CPAN documentation
-
-L<http://annocpan.org/dist/MyMath::Ops>
-
 =item * CPAN Ratings
 
 L<http://cpanratings.perl.org/d/MyMath::Ops>

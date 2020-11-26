6ac1a616a72e41bad386167d4e3ef8cd349248fd
Shlomi Fish <shlomif@shlomifish.org>
2020-11-26 21:44:59 +0200

    optimize

--- bin/gen-index-xhtmls-redirects.pl
+++ bin/gen-index-xhtmls-redirects.pl
@@ -22,37 +22,42 @@ my $IDX     = 'index.xhtml';
 my @filenames =
     File::Find::Object::Rule->name($IDX)->relative()->in( $dir_src, );
 my @dirs;
+my $out_text = '';
 foreach my $fn (@filenames)
 {
     # next if $fn !~ /XSLT/;
     my $dn = path($fn)->parent;
     die if -e $dn->child($IDXH);
-    push @dirs, ( ( $dn eq "." ) ? "" : ( $dn =~ s#[/\\]\z##r ) );
-}
-
-my $out_text = '';
-foreach my $dn (@dirs)
-{
-    my $is_subdir = length $dn;
-    if ($is_subdir)
+    if ( $dn ne "." )
     {
-        $dn .= "/";
-    }
-    my $new_text = qq#Redirect permanent /${dn}${IDXH} /${dn}\n#;
-    if ($is_subdir)
-    {
+        my $dslash   = "$dn/";
+        my $new_text = qq#Redirect permanent /${dslash}${IDXH} /${dslash}\n#;
         path($dir_src)->child( ( split m#/#, $dn ), ".htaccess" )
             ->append_utf8($new_text);
     }
-    $out_text .= $new_text;
-}
-if (0)
-{
-    print $out_text;
+
+    #push @dirs, ( ( $dn eq "." ) ? "" : ( $dn =~ s#[/\\]\z##r ) );
 }
 
 if (0)
 {
+    foreach my $dn (@dirs)
+    {
+        my $is_subdir = length $dn;
+        if ($is_subdir)
+        {
+            $dn .= "/";
+        }
+        my $new_text = qq#Redirect permanent /${dn}${IDXH} /${dn}\n#;
+        if ($is_subdir)
+        {
+            path($dir_src)->child( ( split m#/#, $dn ), ".htaccess" )
+                ->append_utf8($new_text);
+        }
+        $out_text .= $new_text;
+    }
+    print $out_text;
+
     print
 qq#RewriteRule "^(/(?:@{[join("|", @dirs)]})/)index\\.html\$" "\$1" [R]\n#;
 }

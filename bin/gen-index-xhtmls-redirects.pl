92428f85db19f3cc2b8e9dd089cc32bc9bb1b6f4
Shlomi Fish <shlomif@shlomifish.org>
2020-11-26 16:04:40 +0200

    Break into into individual subdirs htaccesses
    Due to hosting/web-server limitations.

--- bin/gen-index-xhtmls-redirects.pl
+++ bin/gen-index-xhtmls-redirects.pl
@@ -17,6 +17,7 @@ use Path::Tiny qw/ path tempdir tempfile cwd /;
 use File::Find::Object::Rule ();
 
 my $dir_src = 'dest/post-incs/t2';
+my $IDXH    = 'index.html';
 my $IDX     = 'index.xhtml';
 my @filenames =
     File::Find::Object::Rule->name($IDX)->relative()->in( $dir_src, );
@@ -24,20 +25,31 @@ my @dirs;
 foreach my $fn (@filenames)
 {
     # next if $fn !~ /XSLT/;
-    my $dn = path($fn)->dirname;
+    my $dn = path($fn)->parent;
+    die if -e $dn->child($IDXH);
     push @dirs, ( ( $dn eq "." ) ? "" : ( $dn =~ s#[/\\]\z##r ) );
 }
 
 my $out_text = '';
 foreach my $dn (@dirs)
 {
-    if ( length $dn )
+    my $is_subdir = length $dn;
+    if ($is_subdir)
     {
         $dn .= "/";
     }
-    $out_text .= qq#Redirect permanent /${dn}index.html /${dn}\n#;
+    my $new_text = qq#Redirect permanent /${dn}${IDXH} /${dn}\n#;
+    if ($is_subdir)
+    {
+        path($dir_src)->child( ( split m#/#, $dn ), ".htaccess" )
+            ->append_utf8($new_text);
+    }
+    $out_text .= $new_text;
 }
-print $out_text;
+if (0)
+{
+    print $out_text;
+}
 
 if (0)
 {

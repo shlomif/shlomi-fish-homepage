30792524db70d2f7f8e4bec330733cb6b64184a2
Shlomi Fish <shlomif@shlomifish.org>
2021-01-26 18:01:04 +0200

    add custom spell check paths

---
+++ lib/Shlomif/Spelling/Check_Local.pm
@@ -0,0 +1,62 @@
+package Shlomif::Spelling::Check_Local;
+
+use strict;
+use warnings;
+use autodie;
+use utf8;
+
+use Moo;
+
+use Text::Hunspell                ();
+use Shlomif::Spelling::Whitelist  ();
+use HTML::Spelling::Site::Checker ();
+
+has obj => (
+    is      => 'ro',
+    default => sub {
+        my ($self) = @_;
+        my $speller;
+
+    DIRS:
+        foreach my $dir ( "/usr/share/hunspell", "/usr/share/myspell" )
+        {
+            $speller =
+                Text::Hunspell->new( "$dir/en_GB.aff", "$dir/en_GB.dic", );
+            last DIRS
+                if $speller;
+        }
+
+        if ( not $speller )
+        {
+            die "Could not initialize speller!";
+        }
+
+        return HTML::Spelling::Site::Checker->new(
+            {
+                timestamp_cache_fn => (
+                    ( $ENV{LATEMP_SPELL_CACHE_DIR} // './Tests/data/cache' )
+                    . '/spelling-timestamp.json'
+                ),
+                whitelist_parser =>
+                    scalar( Shlomif::Spelling::Whitelist->new() ),
+                check_word_cb => sub {
+                    my ($word) = @_;
+                    return $speller->check($word);
+                },
+            }
+        );
+    }
+);
+
+sub spell_check
+{
+    my ( $self, $args ) = @_;
+
+    return $self->obj->spell_check(
+        {
+            files => $args->{files}
+        }
+    );
+}
+
+1;

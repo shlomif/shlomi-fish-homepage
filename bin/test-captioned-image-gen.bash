d5c030d8b08c1674f8fda91715f3921ab79dfe47
Shlomi Fish <shlomif@shlomifish.org>
2021-01-27 20:09:23 +0200

    add image macro/"meme" generator

---
+++ bin/test-captioned-image-gen.bash
@@ -0,0 +1,19 @@
+#! /usr/bin/env bash
+#
+# test-captioned-image-gen.bash
+# Copyright (C) 2021 Shlomi Fish < https://www.shlomifish.org/ >
+#
+# Distributed under the terms of the MIT license.
+#
+(
+    set -e -x
+    img="Captioned-Image-Badass-Schwarzenegger"
+    test -d "$img" && rm -fr "$img"
+    python3 "$trunk"/bin/generate_captioned_image.py --process none
+    (
+        cd "$img"
+        gmake
+        git i
+    )
+)
+

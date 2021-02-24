7df234936a5bd710258e45fb23e1930dcec075c0
Shlomi Fish <shlomif@shlomifish.org>
2021-02-24 21:44:41 +0200

    wip

---
+++ lib/faq/screenplay_splitter_prog.py
@@ -0,0 +1,97 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2019 Shlomi Fish <shlomif@cpan.org>
+#
+# Distributed under terms of the MIT license.
+
+"""
+Split Shlomi Fish's FAQ-list into individual
+sections or questions.
+"""
+
+from lxml.html import XHTML_NAMESPACE
+
+from split_into_sections import XhtmlSplitter
+
+
+# Removed:
+# <script src="{base_path}js/main_all.js"></script>
+FAQ_SECTION_FORMAT = '''<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+<head>
+<title>Shlomi Fish’s FAQ - {title}</title>
+<meta charset="utf-8"/>
+<meta name="description" content=
+"Shlomi Fish’s Frequently Asked Questions (FAQ) List - {title}"/>
+<link rel="stylesheet" href="{base_path}faq-indiv.css" media="screen" title=
+"Normal"/>
+<link rel="stylesheet" href="{base_path}print.css" media="print"/>
+<link rel="shortcut icon" href="{base_path}favicon.ico" type=
+"image/x-icon"/>
+<meta name="viewport" content="width=device-width,initial-scale=1"/>
+</head>
+<body class="faq_indiv_entry">
+<div class="header" id="header">
+<a href="{base_path}"><img src="{base_path}images/evilphish-flipped.png"
+alt="EvilPHish site logo"/></a>
+<div class="leading_path"><a href="{base_path}">Shlomi Fish’s
+Homepage</a> → <a href="../" title=
+"Information about this Site">Meta Info</a> → <a href="./" title=
+"Frequently Asked Questions and Answers List (FAQ)">FAQ</a>
+{breadcrumbs_trail}
+→ <b>{title}</b>
+</div>
+</div>
+<div id="faux">
+<main class="main faq">
+{body}</main>
+</div>
+<footer>
+<div class="foot_left">
+<ul class="bt_nav">
+<li><a href="{base_path}">Home</a></li>
+<li><a href="{base_path}me/">About</a></li>
+<li><a href="{base_path}me/contact-me/">Contact Us</a></li>
+<li><a href="{base_path}meta/privacy-policy/">Privacy Policy</a></li>
+<li><a href="{base_path}meta/anti-spam-policy/">Anti-Spam
+Policy</a></li>
+<li><a href="{base_path}meta/FAQ/" title=
+"Frequently asked questions list">FAQ</a></li>
+<li><a href="{base_path}me/blogs/">RSS/Atom Feeds</a></li>
+</ul>
+<p>Written, designed, and maintained by Shlomi Fish, <a href=
+"mailto:shlomif@shlomifish.org">shlomif@shlomifish.org</a>.</p>
+</div>
+<a href="{base_path}"><img src="{base_path}images/bk2hp-v2.min.svg" class=
+"bk2hp" alt="Back to my Homepage"/></a></footer>
+</body>
+</html>'''
+
+
+def main():
+    OUT_DN = "./dest/post-incs/t2/humour/Summerschool-at-the-NSA/"
+    TOP_LEVEL_CLASS = 'screenplay'
+    XHTML_NS = XHTML_NAMESPACE
+    XML_NS = "{http://www.w3.org/XML/1998/namespace}"
+    ns = {
+        "xhtml": XHTML_NS,
+        "xml": XML_NS,
+    }
+    XHTML_SECTION_TAG = '{' + XHTML_NAMESPACE + '}section'
+
+    splitter = XhtmlSplitter(
+        input_fn=(OUT_DN + "/ongoing-text.html"),
+        output_dirname=OUT_DN,
+        section_format=FAQ_SECTION_FORMAT,
+        container_elem_xpath=("//xhtml:div[@class='" + TOP_LEVEL_CLASS + "']"),
+        ns=ns,
+        xhtml_section_tag=XHTML_SECTION_TAG,
+    )
+    splitter.process()
+
+
+if __name__ == '__main__':
+    main()

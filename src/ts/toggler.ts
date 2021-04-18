d2caf21dc65db50bba572131f79c9df998901f71
Shlomi Fish <shlomif@shlomifish.org>
2021-04-18 12:59:02 +0300

    convert toggler to typescript

--- src/ts/toggler.ts
+++ src/ts/toggler.ts
@@ -1,26 +1,26 @@
 "use strict";
 
 class TogglerWrapper {
-    constructor(public toggle_cb: () => void, public apply_state_cb: () => void) {
+    constructor(public toggle_cb: () => void, public apply_state_cb: () => void, public calc_in_elem: () => boolean) {
         return;
     }
 };
 
 function build_toggler(args) {
-    function _is_null (x): boolean {
+    function _is_null (x: any): boolean {
         return ((typeof x === "undefined") || (x === undefined) || (x === null));
     }
 
-    const has_ls = !_is_null( localStorage );
-    const toggled_type = (('toggled_type' in args) ? args['toggled_type'] : 'class');
-    const is_class = (toggled_type == 'class');
+    const has_ls: boolean = !_is_null( localStorage );
+    const toggled_type: string = (('toggled_type' in args) ? args['toggled_type'] : 'class');
+    const is_class: boolean = (toggled_type == 'class');
     const toggle_sect_key = args['ls_key'];
     const toggler_selector = args['toggler_selector'];
     const toggled_selector = args['toggled_selector'];
     const toggled_class = args['toggled_class'];
-    const hide_text = args['hide_text'];
-    const show_text = args['show_text'];
-    const default_state = (('default_state' in args) ? args['default_state'] : true);
+    const hide_text: string = args['hide_text'];
+    const show_text: string = args['show_text'];
+    const default_state: boolean = (('default_state' in args) ? args['default_state'] : true);
 
     function toggle_sect_menu() {
         const elem = $(toggler_selector);
@@ -80,10 +80,9 @@ function build_toggler(args) {
     }
     const elem = $(toggler_selector);
     const toggler_wrapper: TogglerWrapper = (is_class ?
-        (new TogglerWrapper(toggle_sect_menu, toggle_sect_menu)) :
-        (new TogglerWrapper(toggle_details, apply_details_state))
+        (new TogglerWrapper(toggle_sect_menu, toggle_sect_menu, (function() { return elem.hasClass('on');}))) :
+        (new TogglerWrapper(toggle_details, apply_details_state, (function() { return _calc__is_on_now(elem);})))
     )
-    const calc_in_elem = (is_class ? (function() { return elem.hasClass('on');}) : (function() { return _calc__is_on_now(elem);}));
 
     elem.on((is_class ? "click" : "toggle"), function (event) { toggler_wrapper.toggle_cb(); });
 
@@ -99,7 +98,7 @@ function build_toggler(args) {
             : ((in_storage_s == "1") ? true : false)
         );
 
-        const in_elem = calc_in_elem();
+        const in_elem: boolean = toggler_wrapper.calc_in_elem();
 
         if ((in_storage && (!in_elem)) || ((!in_storage) && in_elem)) {
             toggler_wrapper.apply_state_cb();

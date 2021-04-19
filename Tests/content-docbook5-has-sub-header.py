0e6cb20f99688a359332bb9bfbc907ba0bf12681
Shlomi Fish <shlomif@shlomifish.org>
2021-04-19 19:36:34 +0300

    Make the markup more semantic using the HTML5 header element.
    See: https://developer.mozilla.org/en-US/docs/Web/HTML/Element/header .
    
    See https://www.shlomifish.org/philosophy/computers/web/validate-your-html/ and
    https://en.wikipedia.org/wiki/Semantic_HTML .

--- Tests/content-docbook5-has-sub-header.py
+++ Tests/content-docbook5-has-sub-header.py
@@ -15,9 +15,14 @@ class MyTests(html_unit_test.TestCase):
     def test_main(self):
         input_fn = 'dest/post-incs/t2/philosophy/philosophy/' + \
             'putting-cards-on-the-table-2019-2020/index.xhtml'
-        return self.doc(input_fn).has_one(
+        doc = self.doc(input_fn)
+
+        doc.has_one(
             ".//h4[@id='fox-in-the-hens-coop' and " +
             "text()='The Fox in the Chickens Coop']"
+        )
+        doc.has_one(
+            ".//section[header/h3[@id='hacking-heroism']]"
         )
 
     def test_fanfic(self):

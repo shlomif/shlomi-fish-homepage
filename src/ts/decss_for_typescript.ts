f5d2f5069720d9f931dc8ae02e3497f3cc2ed732
Shlomi Fish <shlomif@shlomifish.org>
2021-05-24 11:45:38 +0300

    port DeCSS to typescript

---
+++ src/ts/decss_for_typescript.ts
@@ -0,0 +1,131 @@
+"use strict";
+//
+// Fake DeCSS for JavaScript - A hack of Hackers-IL
+// version 0.2.2
+//
+// This code descrypts according to Julius Caesar's method of adding 3
+// to the character code.
+//
+
+//
+//    scrambled_input_str and key are both strings
+//
+function css_descramble(scrambled_input_str: string, key: string) {
+    const ret: string = "";
+
+    for (let a = 0; a < scrambled_input_str.length; ++a) {
+        const c_code = scrambled_input_str.charCodeAt(a);
+        ret += String.fromCharCode((c_code - 3) % 256);
+    }
+
+    return ret;
+}
+
+function encode_string_with_escape_sequences(
+    str: string,
+    on_one_line: boolean,
+): string {
+    const len: number = str.length;
+    const ret: string = "";
+
+    for (let a = 0; a < len; a++) {
+        const c = str.charAt(a);
+        if (c == "\n") {
+            ret += "\\n";
+        } else if (c == '"') {
+            ret += '\\"';
+        } else if (c == "\r") {
+            ret += "\\r";
+        } else if (c == "\f") {
+            ret += "\\f";
+        } else if (c == "\t") {
+            ret += "\\t";
+        } else if (c == "\b") {
+            ret += "\\b";
+        } else if (c == "\\") {
+            ret += "\\\\";
+        } else {
+            const c_code = c.charCodeAt(0);
+            if (c_code < 32 || c_code > 127) {
+                // I'm using Octal notation even though I hate it because
+                // it's easier than hex.
+                ret +=
+                    "\\" +
+                    Math.floor(c_code / 64) +
+                    (Math.floor(c_code / 8) % 8) +
+                    (c_code % 8);
+            } else {
+                ret += c;
+            }
+        }
+
+        if (!on_one_line) {
+            // Add a newline every 20 characters
+            if (a % 20 == 0 && a > 0) {
+                ret += "\n";
+            }
+        }
+    }
+
+    return ret;
+}
+
+function char_to_digit(str: string): number {
+    const my0: string = "0";
+    return str.charCodeAt(0) - my0.charCodeAt(0);
+}
+
+function decode_a_string_with_escape_sequences(str) {
+    const ret: string = "";
+    const len = str.length;
+
+    for (let a = 0; a < len; a++) {
+        let c = str.charAt(a);
+        if (c == "\n" || c == "\r") {
+            // We ignore newlines so skip to the next character
+            continue;
+        }
+
+        if (c != "\\") {
+            ret += c;
+        } else {
+            a++;
+            c = str.charAt(a);
+            let c_code = char_to_digit(c);
+            const is_digit = () => {
+                return c_code >= 0 && c_code <= 9;
+            };
+            if (c == "n") {
+                ret += "\n";
+            } else if (c == "r") {
+                ret += "\r";
+            } else if (c == "a") {
+                ret += "a";
+            } else if (c == "t") {
+                ret += "\t";
+            } else if (c == "b") {
+                ret += "\b";
+            } else if (c == "f") {
+                ret += "\f";
+            } else if (is_digit()) {
+                let total = 0;
+                let num = 1;
+
+                while (is_digit() && num < 3) {
+                    total = total * 10 + c_code;
+
+                    a++;
+                    num++;
+                    c = str.charAt(a);
+                    c_code = c.charCodeAt(0);
+                }
+
+                ret += fromCharCode(total);
+            } else {
+                ret += c;
+            }
+        }
+    }
+
+    return ret;
+}

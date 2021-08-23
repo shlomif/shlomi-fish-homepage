cd63f0a6db884b372c0defd2e3ef7a4a5fa7be6a
Shlomi Fish <shlomif@shlomifish.org>
2021-08-23 16:47:25 +0300

    Extract a common module/library/header.
    This is Refactoring / code cleanup.
    
    See:
    
    * https://refactoring.com/catalog/extractMethod.html
    
    * https://en.wikipedia.org/wiki/Code_refactoring
    
    * https://www.refactoring.com/
    
    * https://www.joelonsoftware.com/2002/01/23/rub-a-dub-dub/
    
    Some small optimisations may have slipped in as well.

--- bin/screenplay-text-to-xml.pl
+++ bin/screenplay-text-to-xml.pl
@@ -3,56 +3,13 @@
 use strict;
 use warnings;
 use autodie;
+use XML::Grammar::Screenplay::App::FromProto qw/ run /;
 
-use Getopt::Long qw/ GetOptions /;
-use XML::Grammar::Screenplay::FromProto              ();
-use XML::Grammar::Screenplay::FromProto::Parser::QnD ();
-
-sub run
-{
-    my $output_fn;
-
-    GetOptions( "output|o=s" => \$output_fn, );
-
-    if ( !defined($output_fn) )
-    {
-        die "Output filename not specified! Use the -o|--output flag!";
-    }
-
-    my $converter = XML::Grammar::Screenplay::FromProto->new(
-        {
-            parser_class => "XML::Grammar::Screenplay::FromProto::Parser::QnD",
-        }
-    );
-
-    my $xml = $converter->convert(
-        {
-            source => { file => shift(@ARGV), },
-        },
-    );
-
-    open my $out, ">:encoding(UTF-8)", $output_fn;
-    print {$out} $xml;
-    close($out);
-
-    exit(0);
-}
-
-run;
-
-1;
+run();
 
 __END__
 
 =encoding UTF-8
-
-=head1 NAME
-
-XML::Grammar::Screenplay::App::FromProto
-
-=head1 VERSION
-
-version v0.16.0
 
 =head1 COPYRIGHT AND LICENSE
 

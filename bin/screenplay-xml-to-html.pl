0f84072c1208bea856822f1f598b1d7293865153
Shlomi Fish <shlomif@shlomifish.org>
2021-08-24 10:28:49 +0300

    Extract a common module/library/header.
    This is Refactoring / code cleanup.
    
    See:
    
    * https://refactoring.com/catalog/extractMethod.html
    
    * https://en.wikipedia.org/wiki/Code_refactoring
    
    * https://www.refactoring.com/
    
    * https://www.joelonsoftware.com/2002/01/23/rub-a-dub-dub/
    
    Some small optimisations may have slipped in as well.

--- bin/screenplay-xml-to-html.pl
+++ bin/screenplay-xml-to-html.pl
@@ -4,57 +4,34 @@ use strict;
 use warnings;
 use autodie;
 
-use Getopt::Long qw/ GetOptions /;
-use XML::Grammar::Screenplay::ToHTML ();
-use XML::LibXML::XPathContext        ();
+use XML::Grammar::Screenplay::App::ToHTML v0.26.0 ();
+use XML::LibXML::XPathContext ();
 
-sub run
-{
-    my $output_fn;
+my $xpc     = XML::LibXML::XPathContext->new();
+my $XHTMLNS = "http://www.w3.org/1999/xhtml";
+$xpc->registerNs( 'x', $XHTMLNS, );
 
-    GetOptions( "output|o=s" => \$output_fn, );
+XML::Grammar::Screenplay::App::ToHTML->run(
+    +{
+        dom_post_proc => sub {
+            my $output_dom = shift()->{dom};
 
-    if ( !defined($output_fn) )
-    {
-        die "Output filename not specified! Use the -o|--output flag!";
+            my @list = $xpc->findnodes(
+                q#descendant::x:figure[contains(@class, 'asciiart')]#,
+                ($$output_dom) );
+
+            foreach my $el (@list)
+            {
+                my $parent  = $el->parentNode;
+                my $wrapper = $$output_dom->createElementNS( $XHTMLNS, 'div' );
+                $wrapper->setAttribute( 'class', 'asciiart_wrapper' );
+                $wrapper->appendChild( $el->cloneNode(1) );
+                $parent->replaceChild( $wrapper, $el );
+            }
+            return;
+        },
     }
-    my $translator = XML::Grammar::Screenplay::ToHTML->new;
-    my $output_dom = $translator->translate_to_html(
-        {
-            source => { file => shift(@ARGV), },
-            output => "dom",
-        }
-    );
-    my $xc      = XML::LibXML::XPathContext->new($output_dom);
-    my $XHTMLNS = "http://www.w3.org/1999/xhtml";
-    $xc->registerNs( 'html' => $XHTMLNS, );
-
-    # print($output_dom);
-    my @list = $xc->findnodes(
-        q#descendant::html:figure[contains(@class, 'asciiart')]#,
-        ($output_dom) );
-
-    foreach my $el (@list)
-    {
-        my $parent  = $el->parentNode;
-        my $wrapper = $output_dom->createElementNS( $XHTMLNS, 'div' );
-        $wrapper->setAttribute( 'class', 'asciiart_wrapper' );
-        $wrapper->appendChild( $el->cloneNode(1) );
-        $parent->replaceChild( $wrapper, $el );
-    }
-
-    my $chars =
-        $translator->_to_html_stylesheet()->output_as_chars($output_dom);
-
-    $chars =~ s/[ \t]+$//gms;
-    open my $out, ">:encoding(UTF-8)", $output_fn;
-    print {$out} $chars;
-    close($out);
-
-    exit(0);
-}
-
-run;
+);
 
 __END__
 

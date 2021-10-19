a5b7bf26c45d2080d051769b8ebdfac632b9d9f2
Shlomi Fish <shlomif@shlomifish.org>
2021-10-19 19:08:35 +0300

    gh actions #5: dbtoepub fix

--- bin/CI-testing/common-env-shim.sh
+++ bin/CI-testing/common-env-shim.sh
@@ -5,4 +5,17 @@
 #
 # Distributed under the terms of the MIT license.
 #
-local_lib_shim() { eval "$(perl -Mlocal::lib=$HOME/perl_modules)"; PATH="$PATH:$GOBIN:$GOPATH/bin:$HOME/go/bin"; } ; local_lib_shim ;
+local_lib_shim() {
+    eval "$(perl -Mlocal::lib=$HOME/perl_modules)";
+    PATH="$PATH:$GOBIN:$GOPATH/bin:$HOME/go/bin";
+
+    local trunk="`pwd`"
+    local repo="$trunk/lib/repos/xslt10-stylesheets"
+    _local_dbtoepub="$trunk/lib/repos/xslt10-stylesheets/xsl/epub/bin/dbtoepub"
+    test -e "$_local_dbtoepub" || git submodule update --init --recursive
+    if test -e "$_local_dbtoepub"
+    then
+        export DBTOEPUB="/usr/bin/ruby $_local_dbtoepub"
+    fi
+}
+local_lib_shim ;

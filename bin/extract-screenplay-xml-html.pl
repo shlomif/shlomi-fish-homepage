f1e1603c8c3c675166fea4f8f86fea27406f4e92
Shlomi Fish <shlomif@shlomifish.org>
2022-03-21 08:02:57 +0200

    Update .gitignore: add generated files or remove old.

--- bin/extract-screenplay-xml-html.pl
+++ bin/extract-screenplay-xml-html.pl
@@ -1,4 +1,4 @@
-#!/usr/bin/perl 
+#!/usr/bin/perl
 
 use strict;
 use warnings;
@@ -8,20 +8,18 @@ use IO::All;
 
 my $out_fn;
 
-GetOptions(
-    "output|o=s" => \$out_fn,
-);
+GetOptions( "output|o=s" => \$out_fn, );
 
 # Input the filename
 my $filename = shift(@ARGV)
     or die "Give me a filename as a command argument: myscript FILENAME";
 
-my $text = scalar(io()->file($filename)->slurp());
+my $text = scalar( io()->file($filename)->utf8()->slurp() );
 
 $text =~ s{<(/?h)(\d+)}{"<".$1.($2+1)}ge;
 
-$text =~ s{\A.*?(<div class="screenplay")}{$1}ms;
-substr($text, rindex($text, "</div>")) = "</div>";
+$text =~ s{\A.*?(<main class="screenplay")}{$1}ms;
+substr( $text, rindex( $text, "</main>" ) ) = "</main>";
 
-io()->file($out_fn)->print($text);
+io()->file($out_fn)->utf8()->print($text);
 

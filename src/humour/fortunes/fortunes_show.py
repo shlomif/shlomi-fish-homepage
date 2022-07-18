213a136a04eabc23f60c347eb0643abfebab5f1f
Shlomi Fish <shlomif@shlomifish.org>
2022-07-18 12:11:38 +0300

    CSS tweak: mobile compat

--- src/humour/fortunes/fortunes_show.py
+++ src/humour/fortunes/fortunes_show.py
@@ -160,6 +160,7 @@ NON_RAW_TEMPLATE = \
 <link rel="stylesheet" href="{{base_dir}}/fort_total.css" media="screen" />
 <meta charset="utf-8" />
 <meta name="description" content="{{description}}" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 </head>
 <body>
 <ul id="nav">

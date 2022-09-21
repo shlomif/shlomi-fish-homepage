ef5ca2117b6ebb87fed8858c243ec576f4145f49
Shlomi Fish <shlomif@shlomifish.org>
2022-09-21 18:58:59 +0300

    Extract a common class/struct.
    This is Refactoring / code cleanup.
    
    See:
    
    * https://en.wikipedia.org/wiki/God_object
    
    * https://en.wikipedia.org/wiki/Extract_class
    
    * https://en.wikipedia.org/wiki/Code_refactoring
    
    * https://www.refactoring.com/
    
    * https://www.joelonsoftware.com/2002/01/23/rub-a-dub-dub/
    
    Some small optimisations may have slipped in as well.

--- lib/Shlomif/Homepage/NavBlocks/ShlomifWikiLink.pm
+++ lib/Shlomif/Homepage/NavBlocks/ShlomifWikiLink.pm
@@ -2,22 +2,20 @@ package Shlomif::Homepage::NavBlocks::ShlomifWikiLink;
 
 use strict;
 use warnings;
-
-use MooX (qw( late ));
-
-use HTML::Widgets::NavMenu::EscapeHtml qw(escape_html);
+use MooX qw/ late /;
 
 extends('Shlomif::Homepage::NavBlocks::ExternalLink');
 
-sub render
-{
-    my ( $self, $r ) = @_;
+has css_class => (
+    is      => 'ro',
+    isa     => 'Str',
+    default => 'wiki shlomif',
+);
 
-    return sprintf(
-        q#<li><p><a class="ext wiki shlomif" href="%s">%s</a></p></li>#,
-        escape_html( $self->url, ),
-        'Shlomif Wiki Page',
-    );
-}
+has inner_html => (
+    is      => 'ro',
+    isa     => 'Str',
+    default => 'Shlomif Wiki Page',
+);
 
 1;

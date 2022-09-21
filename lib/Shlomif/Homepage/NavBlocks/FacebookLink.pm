56558a374b98c0d26f5663ee31157d6b4bd63ec7
Shlomi Fish <shlomif@shlomifish.org>
2022-09-21 18:45:07 +0300

    Extract a common class/struct.
    This is Refactoring / code cleanup.
    
    See:
    
    * https://en.wikipedia.org/wiki/God_object
    
    * https://en.wikipedia.org/wiki/Extract_class
    
    * https://en.wikipedia.org/wiki/Code_refactoring
    
    * https://www.refactoring.com/
    
    * https://www.joelonsoftware.com/2002/01/23/rub-a-dub-dub/
    
    Some small optimisations may have slipped in as well.

--- lib/Shlomif/Homepage/NavBlocks/FacebookLink.pm
+++ lib/Shlomif/Homepage/NavBlocks/FacebookLink.pm
@@ -2,22 +2,20 @@ package Shlomif::Homepage::NavBlocks::FacebookLink;
 
 use strict;
 use warnings;
-
-use HTML::Widgets::NavMenu::EscapeHtml qw(escape_html);
-
-use Moo;
+use MooX qw/ late /;
 
 extends('Shlomif::Homepage::NavBlocks::ExternalLink');
 
-sub render
-{
-    my ( $self, $r ) = @_;
+has css_class => (
+    is      => 'ro',
+    isa     => 'Str',
+    default => 'facebook',
+);
 
-    return sprintf(
-        q#<li><p><a class="ext facebook" href="%s">%s</a></p></li>#,
-        escape_html( $self->url, ),
-        'Facebook Page',
-    );
-}
+has inner_html => (
+    is      => 'ro',
+    isa     => 'Str',
+    default => 'Facebook Page',
+);
 
 1;

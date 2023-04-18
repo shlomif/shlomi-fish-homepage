f971e7d2899c7b53aabafb955c39a305c8521d37
Shlomi Fish <shlomif@shlomifish.org>
2023-04-18 09:35:40 +0300

    add more code

--- bin/reverse-fortunes-order.ts
+++ bin/reverse-fortunes-order.ts
@@ -1,1 +1,7 @@
+// Based on:
+//
+// https://stackoverflow.com/questions/6219152/jquery-reverse-an-order
+//
+// Thanks!
+
 $(".fortunes_list").append($(".fortunes_list div.fortune").get().reverse());

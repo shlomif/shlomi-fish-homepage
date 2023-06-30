ed2f666a163dc8dadc625013456af84895018283
Shlomi Fish <shlomif@shlomifish.org>
2023-06-30 11:59:50 +0300

    wip: extended tpage-like exe

---
+++ bin/my-tt-processor.pl
@@ -0,0 +1,72 @@
+#! /usr/bin/env perl
+#
+# Short description for my-tt-processor.pl
+#
+# Version 0.0.1
+# Copyright (C) 2023 Shlomi Fish < https://www.shlomifish.org/ >
+#
+# Licensed under the terms of the MIT license.
+
+use strict;
+use warnings;
+use 5.014;
+use autodie;
+
+use Carp                                   qw/ confess /;
+use Getopt::Long                           qw/ GetOptions /;
+use Path::Tiny                             qw/ cwd path tempdir tempfile /;
+use Docker::CLI::Wrapper::Container v0.0.4 ();
+
+use lib "./lib";
+
+use Shlomif::Homepage::TTRender ();
+
+my $printable;
+my $stdout;
+my $obj = Shlomif::Homepage::TTRender->new(
+    { printable => $printable, stdout => $stdout, } );
+
+sub run
+{
+    my $output_fn;
+
+    my $obj = Docker::CLI::Wrapper::Container->new(
+        { container => "rinutils--deb--test-build", sys => "debian:sid", } );
+
+    GetOptions( "output|o=s" => \$output_fn, )
+        or die "errror in cmdline args: $!";
+
+    if ( !defined($output_fn) )
+    {
+        die "Output filename not specified! Use the -o|--output flag!";
+    }
+    $obj->do_system( { cmd => [ "git", "clone", "-b", $BRANCH, $URL, ] } );
+
+    exit(0);
+}
+
+run();
+
+1;
+
+__END__
+
+=encoding UTF-8
+
+=head1 NAME
+
+XML::Grammar::Screenplay::App::FromProto
+
+=head1 VERSION
+
+version v0.16.0
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is Copyright (c) 2007 by Shlomi Fish.
+
+This is free software, licensed under:
+
+  The MIT (X11) License
+
+=cut

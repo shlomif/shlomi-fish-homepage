7aec64a9659c5c374060581b96d778a906c22c07
Shlomi Fish <shlomif@shlomifish.org>
2023-07-01 10:08:49 +0300

    try2fix gh actions

--- bin/my-tt-processor.pl
+++ bin/my-tt-processor.pl
@@ -12,10 +12,11 @@ use warnings;
 use 5.014;
 use autodie;
 
-use Carp                                   qw/ confess /;
-use Getopt::Long                           qw/ GetOptions /;
-use Path::Tiny                             qw/ cwd path tempdir tempfile /;
-use Docker::CLI::Wrapper::Container v0.0.4 ();
+use Carp         qw/ confess /;
+use Getopt::Long qw/ GetOptions /;
+use Path::Tiny   qw/ cwd path tempdir tempfile /;
+
+# use Docker::CLI::Wrapper::Container v0.0.4 ();
 
 use lib "./lib";
 

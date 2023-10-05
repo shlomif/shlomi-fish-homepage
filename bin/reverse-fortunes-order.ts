964bad52d1ddda469422a0d28f297298a65e5de3
Shlomi Fish <shlomif@shlomifish.org>
2023-10-05 16:15:29 +0300

    Extract a method or a function.
    This is Refactoring / code cleanup.
    
    See:
    
    * https://refactoring.com/catalog/extractMethod.html
    
    * https://en.wikipedia.org/wiki/Code_refactoring
    
    * https://www.refactoring.com/
    
    * https://www.joelonsoftware.com/2002/01/23/rub-a-dub-dub/
    
    Some small optimisations may have slipped in as well.

--- bin/reverse-fortunes-order.ts
+++ bin/reverse-fortunes-order.ts
@@ -13,4 +13,9 @@ function jq_reverse(parent_sel: string, child_sel: string): void {
     );
     return;
 }
-jq_reverse(".fortunes_list", "div.fortune");
+
+function fortunes_reverse(): void {
+    jq_reverse(".fortunes_list", "div.fortune");
+    jq_reverse("section.h3 > ul", "> li");
+    return;
+}

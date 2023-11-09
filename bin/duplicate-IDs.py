bd22438c04ec6db876c5144dd959d83d61a12e7f
Shlomi Fish <shlomif@shlomifish.org>
2023-11-09 15:42:32 +0200

    UI/UX

--- bin/duplicate-IDs.py
+++ bin/duplicate-IDs.py
@@ -14,15 +14,20 @@ class MyTests:
         import sys
         for input_fn in sys.argv[1:]:
             # doc = self.doc(input_fn)
-            doc = html_unit_test.HtmlTestsDoc(self, input_fn)
+            doc = html_unit_test.HtmlTestsDoc(None, input_fn)
             results = doc.xpath('//*/@id').xpath_results
             found = set()
+            dups = set()
             for x in results:
                 if x in found:
-                    print("{}: [ {} ]".format(input_fn, x))
-                    raise BaseException(x)
-                found.add(x)
-            print("{}:".format(input_fn), sorted(list(results)))
+                    # print("{}: [ {} ]".format(input_fn, x))
+                    dups.add(x)
+                else:
+                    found.add(x)
+            if len(dups) > 0:
+                print("DUPLICATES! {}:".format(input_fn), sorted(list(dups)))
+                # raise BaseException(x)
+            # print("{}:".format(input_fn), sorted(list(results)))
 
 
 if __name__ == '__main__':

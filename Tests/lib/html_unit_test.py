aae6fef420164e2ab38ed8b389e9dcbf8b9327e1
Shlomi Fish <shlomif@shlomifish.org>
2023-11-13 17:30:18 +0200

    Extract a common module/library/header.
    This is Refactoring / code cleanup.
    
    See:
    
    * https://refactoring.com/catalog/extractMethod.html
    
    * https://en.wikipedia.org/wiki/Code_refactoring
    
    * https://www.refactoring.com/
    
    * https://www.joelonsoftware.com/2002/01/23/rub-a-dub-dub/
    
    Some small optimisations may have slipped in as well.

--- Tests/lib/html_unit_test.py
+++ Tests/lib/html_unit_test.py
@@ -10,6 +10,8 @@
 
 """
 
+import os
+from os.path import join
 import unittest
 
 from lxml import html
@@ -57,3 +59,51 @@ class HtmlTestsDoc:
 class TestCase(unittest.TestCase):
     def doc(self, path):
         return HtmlTestsDoc(self, path)
+
+
+def _find_htmls(root):
+    """docstring for find_htmls"""
+    for dirpath, _, fns in os.walk(root):
+        for fn in fns:
+            if fn.endswith(('.html', '.xhtml')):
+                path = join(dirpath, fn)
+                yield path
+
+
+def _test_finder():
+    for x in _find_htmls(root="dest/post-incs/t2/"):
+        print(x)
+
+
+# _test_finder()
+
+
+class HtmlCheckDuplicateIDs:
+    def main(self, root):
+        """docstring for main"""
+        return self.check_files(
+            paths_list=_find_htmls(root=root)
+        )
+
+    def check_files(self, paths_list):
+        offending_files = {}
+        for input_fn in paths_list:
+            # doc = self.doc(input_fn)
+            doc = HtmlTestsDoc(None, input_fn)
+            ids_list = doc.xpath('//*/@id').xpath_results
+            found = set()
+            dups = set()
+            for x in ids_list:
+                if x in found:
+                    # print("{}: [ {} ]".format(input_fn, x))
+                    dups.add(x)
+                else:
+                    found.add(x)
+            if len(dups) > 0:
+                print("DUPLICATES! {}:".format(input_fn), sorted(list(dups)))
+                offending_files[input_fn] = dups
+                # raise BaseException(x)
+            # print("{}:".format(input_fn), sorted(list(ids_list)))
+        ret = (len(offending_files) == 0)
+        assert ret
+        return ret, offending_files
--- bin/duplicate-IDs.py
+++ bin/duplicate-IDs.py
@@ -6,57 +6,7 @@
 #
 # Distributed under terms of the MIT license.
 
-import html_unit_test
-import os
-from os.path import join
-
-
-def _find_htmls(root):
-    """docstring for find_htmls"""
-    for dirpath, _, fns in os.walk(root):
-        for fn in fns:
-            if fn.endswith(('.html', '.xhtml')):
-                path = join(dirpath, fn)
-                yield path
-
-
-def _test_finder():
-    for x in _find_htmls(root="dest/post-incs/t2/"):
-        print(x)
-
-
-# _test_finder()
-
-
-class HtmlCheckDuplicateIDs:
-    def main(self, root):
-        """docstring for main"""
-        return self.check_files(
-            paths_list=_find_htmls(root=root)
-        )
-
-    def check_files(self, paths_list):
-        offending_files = {}
-        for input_fn in paths_list:
-            # doc = self.doc(input_fn)
-            doc = html_unit_test.HtmlTestsDoc(None, input_fn)
-            ids_list = doc.xpath('//*/@id').xpath_results
-            found = set()
-            dups = set()
-            for x in ids_list:
-                if x in found:
-                    # print("{}: [ {} ]".format(input_fn, x))
-                    dups.add(x)
-                else:
-                    found.add(x)
-            if len(dups) > 0:
-                print("DUPLICATES! {}:".format(input_fn), sorted(list(dups)))
-                offending_files[input_fn] = dups
-                # raise BaseException(x)
-            # print("{}:".format(input_fn), sorted(list(ids_list)))
-        ret = (len(offending_files) == 0)
-        assert ret
-        return ret, offending_files
+from html_unit_test import HtmlCheckDuplicateIDs
 
 
 if __name__ == '__main__':

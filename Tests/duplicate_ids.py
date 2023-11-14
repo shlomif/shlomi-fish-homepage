47b0631f915065730d97ef181e7db7746133954a
Shlomi Fish <shlomif@shlomifish.org>
2023-11-14 10:42:22 +0200

    duplicate IDs test

---
+++ Tests/duplicate_ids.py
@@ -0,0 +1,26 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2019 Shlomi Fish <shlomif@cpan.org>
+#
+# Distributed under terms of the MIT license.
+
+import unittest
+
+import html_unit_test
+from html_unit_test import HtmlCheckDuplicateIDs
+
+
+class MyTests(html_unit_test.TestCase):
+    def test_main(self):
+        ret, dups = HtmlCheckDuplicateIDs().check_dir_tree(
+            root="dest/post-incs/t2/"
+        )
+        self.assertTrue(ret, "HtmlCheckDuplicateIDs")
+
+
+if __name__ == '__main__':
+    from pycotap import TAPTestRunner
+    suite = unittest.TestLoader().loadTestsFromTestCase(MyTests)
+    TAPTestRunner().run(suite)

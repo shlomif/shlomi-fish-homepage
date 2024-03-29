36fc841d98fe172880b7c6a18cf85e9e592bc9fd
Shlomi Fish <shlomif@shlomifish.org>
2024-03-29 16:53:29 +0300

    update to new path

--- bin/tot.bash
+++ bin/tot.bash
@@ -5,9 +5,9 @@ adest="$dir/have.xhtml"
 bdest="$dir/want.xhtml"
 rm -f "$adest" "$bdest"
 # for a in "dest/post-incs/t2/MathVentures/3d-outof-4d-mathml.xhtml"
-for a in "dest/post-incs/t2/humour/Selina-Mandrake/cast.html"
+for a in "dest/post-incs/t2/humour/All-in-an-Atypical-Day-Work/index.xhtml"
 do
-b=/home/shlomif/Backup/Arcs/post-$a
+b=/home/shlomif/Arcs/temp/shlomif-homepage/post-$a
 f()
 {
     perl -lapE 's/</\n</g'

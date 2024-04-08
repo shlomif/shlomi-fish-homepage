3788813f87ce07acb45854e0712a9327a1edf89d
Shlomi Fish <shlomif@shlomifish.org>
2024-04-08 05:10:28 +0300

    add iframes-fix.ts

---
+++ bin/iframes-fix.ts
@@ -0,0 +1,7 @@
+// One can run the code below (or similar) in order to improve the
+// dimensions of the embedded YouTube videos on
+// https://www.shlomifish.org/philosophy/culture/case-for-commercial-fan-fiction/
+// . It can be input into the JS console ( Tools → Web Developer Tools → Console ; Ctrl+Shift+K )
+//
+// License is https://en.wikipedia.org/wiki/MIT_License .
+$("iframe").attr("width", "600").attr("height", "400");

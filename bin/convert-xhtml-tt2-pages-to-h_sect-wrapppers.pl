fa9d21e592e4f4df0ddc058e21a604d786dc88ff
Shlomi Fish <shlomif@shlomifish.org>
2024-07-27 10:17:07 +0300

    start writing a program to massage <h2> to h2_sect / etc.

---
+++ bin/convert-xhtml-tt2-pages-to-h_sect-wrapppers.pl
@@ -0,0 +1,81 @@
+#! /usr/bin/env perl
+#
+# Short description for convert-xhtml-tt2-pages-to-h_sect-wrapppers.pl
+#
+# Version 0.0.1
+# Copyright (C) 2024 Shlomi Fish < https://www.shlomifish.org/ >
+#
+# Licensed under the terms of the MIT license.
+
+use strict;
+use warnings;
+use 5.014;
+use autodie;
+
+use Carp                                   qw/ confess /;
+use Getopt::Long                           qw/ GetOptions /;
+use Path::Tiny                             qw/ cwd path tempdir tempfile /;
+use Docker::CLI::Wrapper::Container v0.0.4 ();
+
+sub run
+{
+    my $output_fn;
+
+    my $in_fh = \*ARGV;
+    my $out   = "";
+
+    while ( my $line = <$in_fh> )
+    {
+        if ( my ($level) = $line =~ m@\A\<h([0-9]+)@ms )
+        {
+            if ( $level !~ m#\A[1-6]\z#ms )
+            {
+                confess "Out of range level $level !";
+            }
+        }
+        elsif ( my ($tag) = $line =~ m@(</h[0-9]+)@ms )
+        {
+            confess "Out of place end tag “$tag” !";
+        }
+    }
+
+    print $out;
+    return;
+
+    my $obj = Docker::CLI::Wrapper::Container->new(
+        { container => "rinutils--deb--test-build", sys => "debian:sid", } );
+
+    GetOptions( "output|o=s" => \$output_fn, )
+        or die "errror in cmdline args: $!";
+
+    if ( !defined($output_fn) )
+    {
+        die "Output filename not specified! Use the -o|--output flag!";
+    }
+
+    # $obj->do_system( { cmd => [ "git", "clone", "-b", $BRANCH, $URL, ] } );
+
+    exit(0);
+}
+
+run();
+
+1;
+
+__END__
+
+=encoding UTF-8
+
+=head1 NAME
+
+=head1 VERSION
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is Copyright (c) 2007 by Shlomi Fish.
+
+This is free software, licensed under:
+
+  The MIT (X11) License
+
+=cut

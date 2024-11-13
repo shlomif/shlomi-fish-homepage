3d057de0af1ca91542ab0b11f72dfad0d6bb517f
Shlomi Fish <shlomif@shlomifish.org>
2024-11-13 15:35:03 +0200

    [WiP] fortunes previous-arrow traversal

--- src/ts/previous-and-next-fortunes.ts
+++ src/ts/previous-and-next-fortunes.ts
@@ -13,6 +13,14 @@ function fortunes_addlinks(): void {
         return fort(idx).find("> .head > h3").attr("id");
     };
     for (let i = 0; i < fortunes.length; i++) {
+        if (i > 0) {
+            const thisid = getid(i);
+            const previd = getid(i - 1);
+            fort(i)
+                .find("> .head")
+                // .after('<a href="#' + nextid + '">[ Next ]</a>');
+                .append('<a href="#' + previd + '">[ Prev ]</a>');
+        }
         if (i < fortunes.length - 1) {
             const thisid = getid(i);
             const nextid = getid(i + 1);

fd04dbc80f63b711736c7f910e3c758bf70f5a7b
Shlomi Fish <shlomif@shlomifish.org>
2024-11-14 03:28:29 +0200

    Refactoring / code cleanup.
    See:
    
    * https://en.wikipedia.org/wiki/Code_refactoring
    
    * https://www.refactoring.com/
    
    * https://www.joelonsoftware.com/2002/01/23/rub-a-dub-dub/
    
    Some small optimisations may have slipped in as well.

--- src/ts/previous-and-next-fortunes.ts
+++ src/ts/previous-and-next-fortunes.ts
@@ -12,21 +12,21 @@ function fortunes_addlinks(): void {
     const getid = (idx) => {
         return fort(idx).find("> .head > h3").attr("id");
     };
+    const addlink = (idx, cls, label) => {
+        const link_end_id = getid(idx);
+        const ret =
+            ' <a class="' +
+            cls +
+            '" href="#' +
+            link_end_id +
+            '">' +
+            label +
+            "</a>";
+        return ret;
+    };
 
     for (let i = 0; i < fortunes.length; i++) {
         let html: string = "";
-        const addlink = (idx, cls, label) => {
-            const previd = getid(idx);
-            return (
-                ' <a class="' +
-                cls +
-                '" href="#' +
-                previd +
-                '">' +
-                label +
-                "</a>"
-            );
-        };
 
         if (i > 0) {
             html += addlink(i - 1, "previous", "Previous");

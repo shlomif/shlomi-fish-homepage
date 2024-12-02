7f0a5a8a7930998e1e59a65a849a2466ba5efa32
Shlomi Fish <shlomif@shlomifish.org>
2024-12-02 07:06:04 +0200

    update the wget script

--- bin/Call_Local_Wget.bash
+++ bin/Call_Local_Wget.bash
@@ -1,2 +1,23 @@
-#!/bin/bash
-wget --mirror --convert-links 'http://localhost/shlomif/homepage-local'
+#! /usr/bin/env bash
+#
+# mirror.bash
+# Copyright (C) 2024 Shlomi Fish < https://www.shlomifish.org/ >
+#
+# Distributed under the terms of the MIT license.
+#
+
+# wget --mirror --convert-links 'http://localhost/shlomif/homepage-local'
+
+set -e -x
+
+siteurl="http://127.0.0.1:2400/sites/hp/"
+excludes=()
+prod="true"
+
+if test "${prod}" = "true"
+then
+    siteurl="https://www.shlomifish.org/"
+    excludes=("--exclude-directories=/Files")
+fi
+
+wget2 -r -p --no-parent --default-page="index.xhtml" ${excludes[@]} "${siteurl}"

b4c129c8c1b769cb64e230b66ec65fc20773cf5b
Shlomi Fish <shlomif@shlomifish.org>
2025-02-19 11:20:11 +0200

    add lynx-beta xhtml issue proof tweak

---
+++ bin/xhtml-tweak-for-lynx.py
@@ -0,0 +1,45 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2025 Shlomi Fish < https://www.shlomifish.org/ >
+#
+# Licensed under the terms of the MIT license.
+
+"""
+
+"""
+import os
+import re
+
+
+def process_file(fn):
+    with open(fn, 'rt') as infh:
+        src = infh.read()
+
+    for bef in [True]:
+        for mid in [False]:
+            for aft in [False]:
+                def repl(m):
+                    return " " * bef + "/" + " " * mid + ">" + " " * aft
+                newtxt = re.sub("\\s*/\\s*>\\s*", repl, src)
+
+                with open(fn, 'wt') as o:
+                    o.write(newtxt)
+
+
+def calc_fn(dirpath):
+    total_fns = []
+    from os.path import join
+    for root, dirs, files in os.walk(dirpath):
+        for name in files:
+            fn = join(root, name)
+            if fn.endswith(".html") or fn.endswith(".xhtml"):
+                total_fns.append(fn)
+    return total_fns
+
+
+dirpath = 'dest/post-incs/t2/philosophy/philosophy/'
+total_fns = calc_fn(dirpath)
+for fn in total_fns:
+    process_file(fn)

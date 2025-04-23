211b4511acf831ede4137cb3f0feaab848f2253f
Shlomi Fish <shlomif@shlomifish.org>
2025-04-23 16:45:13 +0300

    update factoids-links script for new DOM

--- src/ts/factoids-links.ts
+++ src/ts/factoids-links.ts
@@ -6,43 +6,34 @@
 
 function factoids_addlinks({ clearMarkup }: { clearMarkup: boolean }): void {
     return;
-    const fortunes = $(".fortunes_list > div.fortune");
+    const fortunes = $(".main_facts_list > ul > li.fact");
     const fort = (idx) => {
         return fortunes.slice(idx, idx + 1);
     };
     const getid = (idx) => {
+        return "shlomif-fact-chuck-" + (idx + 1);
         return fort(idx).find("> .head > h3").attr("id");
     };
     const container_class: string = "factoids_addlinks";
     const getcontainer = (idx) => {
-        return fort(idx).find("> .head > h3 > span." + container_class);
+        return fort(idx).find("> blockquote > span." + container_class);
     };
     const addlink = (idx, cls, label) => {
         const link_end_id = getid(idx);
+        const link_end = "/humour/fortunes/show.cgi?id=" + link_end_id;
         const ret =
-            ' <a class="' +
-            cls +
-            '" href="#' +
-            link_end_id +
-            '">' +
-            label +
-            "</a>";
+            ' <a class="' + cls + '" href="' + link_end + '">' + label + "</a>";
         return ret;
     };
 
     for (let i = 0; i < fortunes.length; i++) {
         let html: string = "";
 
-        if (i > 0) {
-            html += addlink(i - 1, "previous", "Previous");
-        }
-        if (i < fortunes.length - 1) {
-            html += addlink(i + 1, "next", "Next");
-        }
+        html += addlink(i, "fact_node", "[ Node ]");
         let container = getcontainer(i);
         if (!container.length) {
             fort(i)
-                .find("> .head > h3")
+                .find("> blockquote")
                 .append('<span class="' + container_class + '"></span>');
             container = getcontainer(i);
         }

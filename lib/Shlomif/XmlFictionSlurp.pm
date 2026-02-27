7b6bb8658d3f55a0b9438667dbb61b7edf98bab4
Shlomi Fish <shlomif@shlomifish.org>
2026-02-27 15:55:22 +0200

    Fix markup / code validation.
    See https://www.shlomifish.org/philosophy/computers/web/validate-your-html/ .

--- lib/Shlomif/XmlFictionSlurp.pm
+++ lib/Shlomif/XmlFictionSlurp.pm
@@ -12,6 +12,8 @@ sub my_calc
     my ( $self, $args ) = @_;
 
     my $fn       = $args->{fn};
+    my $h_delta  = ( $args->{h_delta}  // 0 );
+    my $h1_delta = ( $args->{h1_delta} // 0 );
     my $index_id = $args->{index_id};
 
     my $text = path($fn)->slurp_utf8;
@@ -19,6 +21,8 @@ sub my_calc
     $text =~ s{\bindex\b}{$index_id}g;
     $text =~ s# xml:space="preserve"([ >]|/>)#$1#g;
     $text =~ s#^[ \t]+(<)#$1#gms;
+    $text =~
+s{\bh([1-6])\b}{my $n = $1; "h" . (($n eq "1") ? ($h1_delta + $n) : ($h_delta + $n))}egms;
 
     return $text;
 }

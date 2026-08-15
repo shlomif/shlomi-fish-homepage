b9d7c55cf5fc54c50fa3f644a3bda25d24bdc29e
Shlomi Fish <shlomif@shlomifish.org>
2026-08-15 15:09:29 +0300

    upgrade to typescript v6

--- src/ts/sub_menu.ts
+++ src/ts/sub_menu.ts
@@ -2,10 +2,10 @@ $(function (): void {
     $(".sub_menu ul.nav_links li a img")
         .mouseover(function (): void {
             const e = $(this);
-            e.attr("src", e.attr("src").replace(/\.svg$/, "-pressed.svg"));
+            e!.attr("src", e!.attr("src")!.replace(/\.svg$/, "-pressed.svg"));
         })
         .mouseout(function (): void {
             const e = $(this);
-            e.attr("src", e.attr("src").replace("-pressed.svg", ".svg"));
+            e!.attr("src", e!.attr("src")!.replace("-pressed.svg", ".svg"));
         });
 });
